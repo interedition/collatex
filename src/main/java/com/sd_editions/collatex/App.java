23c52f1ea168f77477466e87fac04cf733c7c20f
erebus25 <>
2007-07-04 18:28:21 +0000

    Initial Import

---
+++ src/main/java/com/sd_editions/collatex/App.java
@@ -0,0 +1,13 @@
+package com.sd_editions.collatex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
