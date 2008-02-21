13a8d1f89d505de1c12f9c3cf00b43751d277e3d
rhdekker <>
2008-02-21 10:36:50 +0000

    [BB] removed duplication in tests

--- src/test/java/com/sd_editions/collatex/Collate/TextAlignmentVisitorTest.java
+++ src/test/java/com/sd_editions/collatex/Collate/TextAlignmentVisitorTest.java
@@ -12,162 +12,120 @@ import com.sd_editions.collatex.InputPlugin.StringInputPlugin;
 
 public class TextAlignmentVisitorTest extends TestCase {
   public void testAlignment() {
-    Word base = new Word("cat");
-    Word variant = new Word("cat");
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    visitor.visitWord(base);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = wordAlignmentTable("cat", "cat");
     assertEquals("identical: cat", table.get(1, 2).toString());
   }
-  
+
   public void testAlignmentVariant() {
-    Word base = new Word("cat");
-    Word variant = new Word("mat");
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    visitor.visitWord(base);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = wordAlignmentTable("cat", "mat");
     assertEquals("variant-align: cat / mat", table.get(1, 2).toString());
   }
 
   public void testNonAlignment() {
-    Word base = new Word("cat");
-    Word variant = new Word("boat");
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    visitor.visitWord(base);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = wordAlignmentTable("cat", "boat");
     assertEquals("non-alignment: cat, boat", table.get(1, 2).toString());
   }
 
   public void testCapital() {
-    Word base = new Word("the");
-    Word variant = new Word("The");
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    visitor.visitWord(base);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = wordAlignmentTable("the", "The");
     assertEquals("identical: the", table.get(1, 2).toString());
   }
 
   public void testSentence() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-    BlockStructure base = new StringInputPlugin("a black cat").readFile();
-    BlockStructure variant = new StringInputPlugin("a black cat").readFile();
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    base.accept(visitor);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = alignmentTable("a black cat", "a black cat");
     assertEquals("identical: a", table.get(1, 2).toString());
     assertEquals("identical: black", table.get(1, 4).toString());
     assertEquals("identical: cat", table.get(1, 6).toString());
   }
 
   public void testPunctuation() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-    BlockStructure base = new StringInputPlugin("the black cat").readFile();
-    BlockStructure variant = new StringInputPlugin("The, black cat").readFile();
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    base.accept(visitor);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = alignmentTable("the black cat", "The, black cat");
     assertEquals("identical: the", table.get(1, 2).toString());
     assertEquals("identical: black", table.get(1, 4).toString());
     assertEquals("identical: cat", table.get(1, 6).toString());
   }
 
   public void testVariant() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-    BlockStructure base = new StringInputPlugin("a white cat").readFile();
-    BlockStructure variant = new StringInputPlugin("a black cat").readFile();
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    base.accept(visitor);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = alignmentTable("a white cat", "a black cat");
     assertEquals("identical: a", table.get(1, 2).toString());
     assertEquals("variant-align: white / black", table.get(1, 4).toString());
     assertEquals("identical: cat", table.get(1, 6).toString());
   }
 
   public void testOmission_InTheMiddle() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-    BlockStructure base = new StringInputPlugin("a white horse").readFile();
-    BlockStructure variant = new StringInputPlugin("a horse").readFile();
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    base.accept(visitor);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = alignmentTable("a white horse", "a horse");
     assertEquals("identical: a", table.get(1, 2).toString());
     assertEquals("omission: white", table.get(1, 4).toString());
     assertEquals("identical: horse", table.get(1, 6).toString());
   }
 
   public void testOmission_AtTheStart() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-    BlockStructure base = new StringInputPlugin("a certain death").readFile();
-    BlockStructure variant = new StringInputPlugin("certain death").readFile();
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    base.accept(visitor);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = alignmentTable("a certain death", "certain death");
     assertEquals("omission: a", table.get(1, 2).toString());
     assertEquals("identical: certain", table.get(1, 4).toString());
     assertEquals("identical: death", table.get(1, 6).toString());
   }
 
-    public void testOmission_AtTheEnd() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-    BlockStructure base = new StringInputPlugin("a calico cat").readFile();
-    BlockStructure variant = new StringInputPlugin("a calico").readFile();
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    base.accept(visitor);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+  public void testOmission_AtTheEnd() throws FileNotFoundException, IOException, BlockStructureCascadeException {
+    Table table = alignmentTable("a calico cat", "a calico");
     assertEquals("identical: a", table.get(1, 2).toString());
     assertEquals("identical: calico", table.get(1, 4).toString());
     assertEquals("omission: cat", table.get(1, 6).toString());
   }
 
   public void testAddition_InTheMiddle() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-    BlockStructure base = new StringInputPlugin("a cat").readFile();
-    BlockStructure variant = new StringInputPlugin("a calico cat").readFile();
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    base.accept(visitor);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = alignmentTable("a cat", "a calico cat");
     assertEquals("identical: a", table.get(1, 2).toString());
     assertEquals("addition: calico", table.get(1, 3).toString());
     assertEquals("identical: cat", table.get(1, 4).toString());
   }
 
   public void testAddition_AtTheEnd() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-    BlockStructure base = new StringInputPlugin("to be").readFile();
-    BlockStructure variant = new StringInputPlugin("to be lost").readFile();
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    base.accept(visitor);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = alignmentTable("to be", "to be lost");
     assertEquals("identical: to", table.get(1, 2).toString());
     assertEquals("identical: be", table.get(1, 4).toString());
     assertEquals("addition: lost", table.get(1, 5).toString());
   }
 
   public void testAddition_AtTheStart() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-    BlockStructure base = new StringInputPlugin("to be").readFile();
-    BlockStructure variant = new StringInputPlugin("not to be").readFile();
-    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-    base.accept(visitor);
-    BlockStructure alignmentInformation = visitor.getResult();
-    Table table = (Table) alignmentInformation.getRootBlock();
+    Table table = alignmentTable("to be", "not to be");
     assertEquals("addition: not", table.get(1, 1).toString());
     assertEquals("identical: to", table.get(1, 2).toString());
     assertEquals("identical: be", table.get(1, 4).toString());
   }
-  
+
+  //  public void testPhraseAlignment_AtTheStart() throws FileNotFoundException, IOException, BlockStructureCascadeException {
+  //    Table table = alignmentTable("i saw cloverfield yesterday", "i saw the moon get eclipsed yesterday");
+  //    assertEquals("identical: i", table.get(1, 2).toString());
+  //    assertEquals("identical: saw", table.get(1, 4).toString());
+  //    assertEquals("replacement: cloverfield, the moon get eclipsed", table.get(1, 5).toString());
+  //    assertEquals("identical: yesterday", table.get(1, 6).toString());
+  //  }
+
+  private Table alignmentTable(String baseString, String witnessString) throws FileNotFoundException, IOException, BlockStructureCascadeException {
+    BlockStructure base = new StringInputPlugin(baseString).readFile();
+    BlockStructure variant = new StringInputPlugin(witnessString).readFile();
+    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
+    base.accept(visitor);
+    BlockStructure alignmentInformation = visitor.getResult();
+    Table table = (Table) alignmentInformation.getRootBlock();
+    return table;
+  }
+
+  private Table wordAlignmentTable(final String string, final String string2) {
+    Word base = new Word(string);
+    Word variant = new Word(string2);
+    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
+    visitor.visitWord(base);
+    BlockStructure alignmentInformation = visitor.getResult();
+    Table table = (Table) alignmentInformation.getRootBlock();
+    return table;
+  }
+
 //  public void testMultipleAdditions() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-////  	black 	  	  	cat
-////  	the 	black 	and 	white 	cat
-//    BlockStructure base = new StringInputPlugin("black cat").readFile();
-//    BlockStructure variant = new StringInputPlugin("the black and white cat").readFile();
-//    TextAlignmentVisitor visitor = new TextAlignmentVisitor(variant);
-//    base.accept(visitor);
-//    BlockStructure alignmentInformation = visitor.getResult();
-//    Table table = (Table) alignmentInformation.getRootBlock();
+//    //  	black 	  	  	cat
+//    //  	the 	black 	and 	white 	cat
+//    Table table = alignmentTable("black cat", "the black and white cat");
 //    assertEquals("addition: the", table.get(1, 1).toString());
 //    assertEquals("identical: black", table.get(1, 2).toString());
 //    assertEquals("addition: and white", table.get(1, 3).toString());
