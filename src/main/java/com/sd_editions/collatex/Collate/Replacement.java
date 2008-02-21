6f968051fc3fc5ec13b0f634f5c180a1e3a479ea
rhdekker <>
2008-02-21 16:18:15 +0000

    [BB/RHD] Activated phrase alignment tests and made them work

--- src/main/java/com/sd_editions/collatex/Collate/Replacement.java
+++ src/main/java/com/sd_editions/collatex/Collate/Replacement.java
@@ -1,16 +1,23 @@
 package com.sd_editions.collatex.Collate;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 import com.sd_editions.collatex.Block.Word;
 
 public class Replacement extends Cell {
 
 	private final Word baseWord;
-	private final Word replacementWord;
+  private List<Word> replacementWords;
 
-	public Replacement(Word baseWord, Word replacementWord) {
-		this.baseWord = baseWord;
-		this.replacementWord = replacementWord;
-	}
+  public Replacement(Word baseWord, Word replacementWord) {
+    this(baseWord,Collections.singletonList(replacementWord));  }
+
+  public Replacement(Word baseWord, List<Word> replacementWords) {
+    this.baseWord = baseWord;
+    this.replacementWords = replacementWords;
+  }
 
 	@Override
 	public String toHTML() {
@@ -20,7 +27,17 @@ public class Replacement extends Cell {
 	
 	@Override
 	public String toString() {
-		return "replacement: "+baseWord.getContent()+" / "+replacementWord.getContent();
+		return "replacement: "+baseWord.getContent()+" / "+ replacementsAsString();
 	}
+
+  private String replacementsAsString() {
+    String replacementString="";
+    String divider="";
+    for (Word replacement : replacementWords) {
+      replacementString += divider + replacement.getContent();
+      divider=" ";
+    }
+    return replacementString;
+  }
 
 }
--- src/main/java/com/sd_editions/collatex/Collate/TupleToTable.java
+++ src/main/java/com/sd_editions/collatex/Collate/TupleToTable.java
@@ -1,122 +1,146 @@
 package com.sd_editions.collatex.Collate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.sd_editions.collatex.Block.BlockStructure;
 import com.sd_editions.collatex.Block.Line;
 import com.sd_editions.collatex.Block.Word;
 
 public class TupleToTable {
 
-	private Table table;
-	private final Line base;
-	private final Line witness;
-	private final Tuple[] tuples;
-	private int variant = 1;
-	private int column;
+  private Table table;
+  private final Line base;
+  private final Line witness;
+  private final Tuple[] tuples;
+  private int variant = 1;
+  private int column;
 
-	public TupleToTable(BlockStructure base, BlockStructure variant,
-			Tuple[] tuples) {
-		this.tuples = tuples;
-		this.base = (Line) base.getRootBlock();
-		this.witness = (Line) variant.getRootBlock();
-		this.table = new Table(this.base.size());
-		fillTable();
-	}
+  public TupleToTable(BlockStructure base, BlockStructure variant, Tuple[] tuples) {
+    this.tuples = tuples;
+    this.base = (Line) base.getRootBlock();
+    this.witness = (Line) variant.getRootBlock();
+    this.table = new Table(this.base.size());
+    fillTable();
+  }
 
-	private void fillTable() {
-		int baseIndex = 0;
-		int witnessIndex = 0;
-		for (int i = 0; i < tuples.length; i++) {
-			System.out.println("i: " + i + "; baseIndex: " + baseIndex
-					+ "; witnessIndex: " + witnessIndex);
-			Tuple tuple = tuples[i];
-			int difBaseIndex = tuple.baseIndex - baseIndex;
-			int difWitnessIndex = tuple.witnessIndex - witnessIndex;
-			if (difBaseIndex == 1 && difWitnessIndex == 1) {
-				baseIndex = tuple.baseIndex;
-				witnessIndex = tuple.witnessIndex;
-				column = baseIndex * 2 - 2;
-				addIdenticalToTable(base.get(baseIndex), witness.get(witnessIndex));
-			} else if (difBaseIndex > 1 && difWitnessIndex > 1) {
-				baseIndex = baseIndex + 1;
-				witnessIndex = witnessIndex + 1;
-				column = baseIndex * 2 - 2;
-				addReplacementToTable(base.get(baseIndex), witness.get(witnessIndex));
-				baseIndex = tuple.baseIndex;
-				witnessIndex = tuple.witnessIndex;
-				column = baseIndex * 2 - 2;
-				addIdenticalToTable(base.get(tuple.baseIndex), witness
-						.get(tuple.witnessIndex));
-			} else if (difBaseIndex > 1 && difWitnessIndex == 1) {
-				baseIndex = baseIndex + 1;
-				column = baseIndex * 2 - 2;
-				addOmissionToTable(base.get(baseIndex));
-				baseIndex = tuple.baseIndex;
-				witnessIndex = tuple.witnessIndex;
-				column = baseIndex * 2 - 2;
-				addIdenticalToTable(base.get(tuple.baseIndex), witness
-						.get(tuple.witnessIndex));
-			} else if (difBaseIndex == 1 && difWitnessIndex > 1) {
-				baseIndex = baseIndex + 1;
-				witnessIndex++;
-				column = baseIndex * 2 - 2;
-				addAdditionToTable(witness.get(witnessIndex));
-				baseIndex = tuple.baseIndex;
-				witnessIndex = tuple.witnessIndex;
-				column = baseIndex * 2 - 2;
-				addIdenticalToTable(base.get(tuple.baseIndex), witness
-						.get(tuple.witnessIndex));
-			}
-		}
-		int difBaseIndex = base.size() - baseIndex;
-		int difWitnessIndex = witness.size() - witnessIndex;
-		if (difBaseIndex > 0 && difWitnessIndex > 0) {
-			baseIndex = baseIndex + 1;
-			witnessIndex = witnessIndex + 1;
-			column = baseIndex * 2 - 2;
-			addReplacementToTable(base.get(baseIndex), witness.get(witnessIndex));
-		} else if (difBaseIndex > 0 && difWitnessIndex == 0) {
-			baseIndex++;
-			column = baseIndex * 2 - 2;
-			addOmissionToTable(base.get(baseIndex));
-		} else if (difBaseIndex == 0 && difWitnessIndex > 0) {
-			witnessIndex++;
-			column = baseIndex * 2;
-			addAdditionToTable(witness.get(witnessIndex));
-		}
+  private void fillTable() {
+    int baseIndex = 0;
+    int witnessIndex = 0;
+    for (int i = 0; i < tuples.length; i++) {
+      System.out.println("i: " + i + "; baseIndex: " + baseIndex + "; witnessIndex: " + witnessIndex);
+      Tuple tuple = tuples[i];
+      int difBaseIndex = tuple.baseIndex - baseIndex;
+      int difWitnessIndex = tuple.witnessIndex - witnessIndex;
+      if (difBaseIndex == 1 && difWitnessIndex == 1) {
+        baseIndex = tuple.baseIndex;
+        witnessIndex = tuple.witnessIndex;
+        column = baseIndex * 2 - 2;
+        addIdenticalToTable(base.get(baseIndex), witness.get(witnessIndex));
+      } else if (difBaseIndex > 1 && difWitnessIndex > 1) {
+        baseIndex++;
+        witnessIndex++;
+        column = baseIndex * 2 - 2;
+        List<Word> replacementWords = new ArrayList<Word>() {
+        };
+        for (int j = witnessIndex; j < tuple.witnessIndex; j++) {
+          replacementWords.add(witness.get(j));
+        }
+        addReplacementToTable(base.get(baseIndex), replacementWords);
+        baseIndex = tuple.baseIndex;
+        witnessIndex = tuple.witnessIndex;
+        column = baseIndex * 2 - 2;
+        addIdenticalToTable(base.get(tuple.baseIndex), witness.get(tuple.witnessIndex));
+      } else if (difBaseIndex > 1 && difWitnessIndex == 1) {
+        baseIndex++;
+        column = baseIndex * 2 - 2;
+        addOmissionToTable(base.get(baseIndex));
+        baseIndex = tuple.baseIndex;
+        witnessIndex = tuple.witnessIndex;
+        column = baseIndex * 2 - 2;
+        addIdenticalToTable(base.get(tuple.baseIndex), witness.get(tuple.witnessIndex));
+      } else if (difBaseIndex == 1 && difWitnessIndex > 1) {
+        baseIndex++;
+        witnessIndex++;
+        column = baseIndex * 2 - 2;
+        List<Word> additionalWords = new ArrayList<Word>() {
+        };
+        for (int j = witnessIndex; j < tuple.witnessIndex; j++) {
+          additionalWords.add(witness.get(j));
+        }
+        addAdditionToTable(additionalWords);
+        baseIndex = tuple.baseIndex;
+        witnessIndex = tuple.witnessIndex;
+        column = baseIndex * 2 - 2;
+        addIdenticalToTable(base.get(tuple.baseIndex), witness.get(tuple.witnessIndex));
+      }
+    }
+    int difBaseIndex = base.size() - baseIndex;
+    int difWitnessIndex = witness.size() - witnessIndex;
+    if (difBaseIndex > 0 && difWitnessIndex > 0) {
+      baseIndex = baseIndex + 1;
+      witnessIndex = witnessIndex + 1;
+      column = baseIndex * 2 - 2;
+      addReplacementToTable(base.get(baseIndex), witness.get(witnessIndex));
+    } else if (difBaseIndex > 0 && difWitnessIndex == 0) {
+      baseIndex++;
+      column = baseIndex * 2 - 2;
+      addOmissionToTable(base.get(baseIndex));
+    } else if (difBaseIndex == 0 && difWitnessIndex > 0) {
+      witnessIndex++;
+      column = baseIndex * 2;
+      String additionWord = "";
+      String divider = "";
+      for (int j = witnessIndex; j <= witness.size(); j++) {
+        additionWord += divider + witness.get(j).getContent();
+        divider = " ";
+      }
+      addAdditionToTable(new Word(additionWord));
+    }
 
-	}
+  }
 
-	private void addOmissionToTable(Word baseWord) {
-		Cell omission = new Omission(baseWord);
-		addAlignmentInformationToResult(2, omission);
-	}
+  private void addOmissionToTable(Word baseWord) {
+    Cell omission = new Omission(baseWord);
+    addAlignmentInformationToResult(2, omission);
+  }
 
-	private void addAdditionToTable(Word witnessWord) {
-		Cell addition = new Addition(witnessWord);
-		addAlignmentInformationToResult(1, addition);
-	}
+  private void addAdditionToTable(Word witnessWord) {
+    Cell addition = new Addition(witnessWord);
+    addAlignmentInformationToResult(1, addition);
+  }
 
-	private void addReplacementToTable(Word baseWord, Word replacementWord) {
-		Cell replacement = new Replacement(baseWord, replacementWord);
-		addAlignmentInformationToResult(2, replacement);
-	}
+  private void addAdditionToTable(List<Word> witnessWords) {
+    Cell addition = new Addition(witnessWords);
+    addAlignmentInformationToResult(1, addition);
+  }
 
-	private void addIdenticalToTable(Word baseWord, Word witnessWord) {
-		Cell alignment;
-		if (baseWord.alignmentFactor(witnessWord) == 0) {
-			alignment = new AlignmentIdentical(baseWord, witnessWord);
-		} else {
-			alignment = new AlignmentVariant(baseWord, witnessWord);
-		}
-		addAlignmentInformationToResult(2, alignment);
-	}
+  private void addReplacementToTable(Word baseWord, Word replacementWord) {
+    Cell replacement = new Replacement(baseWord, replacementWord);
+    addAlignmentInformationToResult(2, replacement);
+  }
 
-	private void addAlignmentInformationToResult(int offset, Cell alignment) {
-		table.setCell(variant, column + offset, alignment);
-	}
+  private void addReplacementToTable(Word baseWord, List<Word> replacementWords) {
+    Cell replacement = new Replacement(baseWord, replacementWords);
+    addAlignmentInformationToResult(2, replacement);
+  }
 
-	public Table getTable() {
-		return table;
-	}
+  private void addIdenticalToTable(Word baseWord, Word witnessWord) {
+    Cell alignment;
+    if (baseWord.alignmentFactor(witnessWord) == 0) {
+      alignment = new AlignmentIdentical(baseWord, witnessWord);
+    } else {
+      alignment = new AlignmentVariant(baseWord, witnessWord);
+    }
+    addAlignmentInformationToResult(2, alignment);
+  }
+
+  private void addAlignmentInformationToResult(int offset, Cell alignment) {
+    table.setCell(variant, column + offset, alignment);
+  }
+
+  public Table getTable() {
+    return table;
+  }
 
 }
