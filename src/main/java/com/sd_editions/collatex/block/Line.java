451e48caa3493e2eb2b1434e7492448efead5d67
rhdekker <>
2008-02-22 15:35:42 +0000

    [BB] unused imports

--- src/main/java/com/sd_editions/collatex/block/Line.java
+++ src/main/java/com/sd_editions/collatex/block/Line.java
@@ -1,7 +1,6 @@
 package com.sd_editions.collatex.Block;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,55 +17,55 @@ public class Line extends Block {
   int lineNumber;
 
   public Line(int lineNumber) {
-	this.lineNumber = lineNumber;
+    this.lineNumber = lineNumber;
   }
 
   public void setLineNumber(int lineNumber) {
-	this.lineNumber = lineNumber;
+    this.lineNumber = lineNumber;
   }
 
   public int getLineNumber() {
-	return this.lineNumber;
+    return this.lineNumber;
   }
 
   public String toString() {
-	return "<l number=\"" + this.getLineNumber()+ "\">";
-  }
-  
-  public void accept(IntBlockVisitor visitor) {
-	  visitor.visitLine(this);
+    return "<l number=\"" + this.getLineNumber() + "\">";
   }
 
-	public Word get(int i) {
-		Block child = this.getFirstChild();
-		for (int j = 1; j<i; j++) {
-			child = child.getNextSibling();
-		}
-		return (Word) child;
-	}
-	
-	public int size() {
-		if (!this.hasFirstChild()) {
-			return 0;
-		}
-		int i =1;
-		Block child = this.getFirstChild();
-		while(child.hasNextSibling()) {
-			i++;
-			child = child.getNextSibling();
-		}
-		return i;
-	}
-	
-	public List<Word> getPhrase(int i, int j) {
-	  List<Word> result = new ArrayList<Word>();
-	  Word word = get(i);
-	  result.add(word);
-	  for (int k = i; k < j; k++) {
+  public void accept(IntBlockVisitor visitor) {
+    visitor.visitLine(this);
+  }
+
+  public Word get(int i) {
+    Block child = this.getFirstChild();
+    for (int j = 1; j < i; j++) {
+      child = child.getNextSibling();
+    }
+    return (Word) child;
+  }
+
+  public int size() {
+    if (!this.hasFirstChild()) {
+      return 0;
+    }
+    int i = 1;
+    Block child = this.getFirstChild();
+    while (child.hasNextSibling()) {
+      i++;
+      child = child.getNextSibling();
+    }
+    return i;
+  }
+
+  public List<Word> getPhrase(int i, int j) {
+    List<Word> result = new ArrayList<Word>();
+    Word word = get(i);
+    result.add(word);
+    for (int k = i; k < j; k++) {
       word = (Word) word.getNextSibling();
       result.add(word);
     }
-	  return result;
-	}
+    return result;
+  }
 
 }
