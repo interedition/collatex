570ab87c91ed313fc5436c5fd385f79c4ff46718
rhdekker <>
2008-02-27 14:54:31 +0000

    [BB/ML] formatting, expanded test

--- src/test/java/com/sd_editions/collatex/InputPlugin/XMLInputPluginTest.java
+++ src/test/java/com/sd_editions/collatex/InputPlugin/XMLInputPluginTest.java
@@ -12,16 +12,20 @@ import com.sd_editions.collatex.Block.BlockStructureCascadeException;
 import com.sd_editions.collatex.Block.BlockStructureListIterator;
 
 public class XMLInputPluginTest extends TestCase {
-	public void test_simple_xml_input() throws FileNotFoundException, IOException, BlockStructureCascadeException {
-		IntInputPlugin plugin = new XMLInputPlugin(new File("examples/inputfiles/simple_xml.xml"));
-		BlockStructure document = plugin.readFile();
-		assertEquals(16, document.getNumberOfBlocks());
-		BlockStructureListIterator<? extends Block> listIterator = document.listIterator();
-		assertEquals("<l number=\"1\">", listIterator.next().toString());
-		assertEquals("<w>The</w>", listIterator.next().toString());
-		assertEquals("<w>prologe</w>", listIterator.next().toString());
-		assertEquals("<w>of</w>", listIterator.next().toString());
-		assertEquals("<w>the</w>", listIterator.next().toString());
-	}
-	
+  public void test_simple_xml_input() throws FileNotFoundException, IOException, BlockStructureCascadeException {
+    final IntInputPlugin plugin = new XMLInputPlugin(new File("examples/inputfiles/simple_xml.xml"));
+    final BlockStructure document = plugin.readFile();
+    assertEquals(16, document.getNumberOfBlocks());
+    final BlockStructureListIterator<? extends Block> listIterator = document.listIterator();
+    assertEquals("<l number=\"1\">", listIterator.next().toString());
+    assertEquals("<w>The</w>", listIterator.next().toString());
+    assertEquals("<w>prologe</w>", listIterator.next().toString());
+    assertEquals("<w>of</w>", listIterator.next().toString());
+    assertEquals("<w>the</w>", listIterator.next().toString());
+    assertEquals("<w>Milleres</w>", listIterator.next().toString());
+    assertEquals("<w>tale</w>", listIterator.next().toString());
+    //    assertEquals("<l number=\"2\">", listIterator.next().toString());
+    //    assertEquals("<w>Whan</w>", listIterator.next().toString());
+  }
+
 }
