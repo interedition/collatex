aec785ab765911b7fe32a79f96a886b28c37ee0d
rhdekker <>
2008-03-06 16:26:12 +0000

    [BB] changed port for webserver

--- src/main/java/com/sd_editions/collatex/Web/CollateXServer.java
+++ src/main/java/com/sd_editions/collatex/Web/CollateXServer.java
@@ -8,7 +8,7 @@ import org.mortbay.jetty.servlet.ServletHolder;
 public class CollateXServer {
   public static void main(String[] args) throws Exception {
     System.setProperty("org.mortbay.xml.XmlParser.NotValidating", "true");
-    Server server = new Server(8080);
+    Server server = new Server(2000);
     Context root = new Context(server, "/", Context.SESSIONS);
     ServletHolder holder = new ServletHolder(new WicketServlet());
     holder.setInitParameter("applicationClassName", "com.sd_editions.collatex.Web.CollateXApplication");
