e10639a2b596d0e7bd764278b4e608f4890a070c
michaelleuk <>
2008-04-09 16:48:47 +0000

    [ML] added new methods

--- src/main/java/com/sd_editions/collatex/Collate/Tuple.java
+++ src/main/java/com/sd_editions/collatex/Collate/Tuple.java
@@ -1,12 +1,33 @@
 package com.sd_editions.collatex.Collate;
 
-public class Tuple {
+public class Tuple implements Comparable {
   int baseIndex;
   int witnessIndex;
 
   public Tuple(int baseIndex, int witnessIndex) {
     this.baseIndex = baseIndex;
     this.witnessIndex = witnessIndex;
+  }
+
+  public int getBaseIndex() {
+    return baseIndex;
+  }
+
+  public int getWitnessIndex() {
+    return witnessIndex;
+  }
+
+  @Override
+  public int compareTo(Object obj) {
+    Tuple tmp = (Tuple) obj;
+    double newer = tmp.baseIndex + (double) tmp.witnessIndex / 10;
+    double me = this.baseIndex + (double) this.witnessIndex / 10;
+    if (me < newer) {
+      return -1;
+    } else if (me > newer) {
+      return 1;
+    }
+    return 0;
   }
 
   @Override
