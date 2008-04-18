57b726e3e1a43bc6b729c6bdbcdf203de9ceb791
michaelleuk <>
2008-04-18 10:59:26 +0000

    [ML] a small change

--- src/main/java/com/sd_editions/collatex/Collate/LCS.java
+++ src/main/java/com/sd_editions/collatex/Collate/LCS.java
@@ -5,25 +5,25 @@ import java.util.List;
 import com.sd_editions.collatex.Block.BlockStructure;
 
 public class LCS {
-  
+
   private Tuple[] LCS;
   private DotMatrix dotM;
-  
+
   public LCS(BlockStructure base, List<BlockStructure> witnesses) {
     IndexTable iTabbase = new IndexTable(base);
     IndexTable iTabwitness = new IndexTable(witnesses.get(0));
     iTabbase.makeSortedMap();
     iTabwitness.makeSortedMap();
-    LevTable levTab = new LevTable(iTabbase.getIndexTableSorted(),iTabwitness.getIndexTableSorted());
+    LevTable levTab = new LevTable(iTabbase.getIndexTableSorted(), iTabwitness.getIndexTableSorted());
     levTab.fillArrayCells();
-    this.dotM = new DotMatrix(levTab.getLevTuples(),levTab.getMaxH(), levTab.getMaxV());
-    this.LCS=dotM.getLCS();
+    this.dotM = new DotMatrix(levTab.getLevTuples(), levTab.getMaxH(), levTab.getMaxV());
+    this.LCS = dotM.getLCS();
   }
-  
+
   public Tuple[] getLCS() {
+    dotM.toString();
     return this.LCS;
   }
-  
 
   @Override
   public String toString() {
@@ -34,5 +34,5 @@ public class LCS {
     System.out.println();
     return "";
   }
-  
+
 }
