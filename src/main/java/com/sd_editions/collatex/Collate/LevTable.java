5c2de6278ce9ad7c63899dda86971e6bdadabc66
michaelleuk <>
2008-04-18 09:34:24 +0000

    [ML] some changes

--- src/main/java/com/sd_editions/collatex/Collate/LevTable.java
+++ src/main/java/com/sd_editions/collatex/Collate/LevTable.java
@@ -1,6 +1,7 @@
 package com.sd_editions.collatex.Collate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -8,51 +9,86 @@ import com.sd_editions.collatex.Block.Word;
 
 public class LevTable {
   private Object[][] array;
-  private Tuple[] tuples; //TODO Array of tuples       
-  private int anz;
+  private Tuple[] tuples;
+  private int anz, maxH, maxV;
   private Map<String, ArrayList<Integer>> base;
-  private Map<String, ArrayList<Integer>> witness; //TODO: Array of witnesses
+  private Map<String, ArrayList<Integer>> witness;
 
+  @SuppressWarnings("hiding")
   public LevTable(Map<String, ArrayList<Integer>> base, Map<String, ArrayList<Integer>> witness) {
     this.base = base;
     this.witness = witness;
     this.array = new Object[this.witness.size() + 1][this.base.size() + 1];
   }
 
+  public int getMaxH() {
+    return maxH;
+  }
+
+  public int getMaxV() {
+    return maxV;
+  }
+
+  @SuppressWarnings("boxing")
   public Integer getLevDist(String base, String wit) {
     Word bas = new Word(base);
     Word witn = new Word(wit);
     return bas.alignmentFactor(witn);
   }
 
-  public Tuple[] getTuples() {
-    tuples = new Tuple[this.anz * 4];
-    tableToTuple();
+  public Tuple[] getLevTuples() {
+    ArrayList<Tuple> arrL = tableToTuple();
+    tuples = new Tuple[this.anz];
+    Iterator<Tuple> it = arrL.iterator();
+    int i = 0;
+
+    while (it.hasNext()) {
+      this.tuples[i] = it.next();
+      if (tuples[i].baseIndex > this.maxH) {
+        this.maxH = tuples[i].baseIndex;
+      }
+      if (tuples[i].witnessIndex > this.maxV) {
+        this.maxV = tuples[i].witnessIndex;
+      }
+      //System.out.println(this.maxH+"---"+this.maxV+""+tuples[i]);
+      i++;
+    }
+    /* Sort array */
+    Arrays.sort(tuples);
+    showLevTable();
     return tuples;
   }
 
-  public void saveNewTuple(ArrayList<Tuple> resultList, ArrayList<Integer> arrLBaseValues, ArrayList<Integer> arrLWitnValues) {
-    Iterator<Integer> itBase = arrLBaseValues.iterator();
-    Iterator<Integer> itWitn = arrLWitnValues.iterator();
-    while (itBase.hasNext() && itWitn.hasNext()) {
-      resultList.add(new Tuple(itBase.next(), itWitn.next()));
+  public void showLevTable() {
+    System.out.print("Lev: ");
+    for (Tuple element : tuples) {
+      System.out.print(element.toString());
     }
   }
 
-  public void tableToTuple() {
+  @SuppressWarnings("boxing")
+  public void saveNewTuple(ArrayList<Tuple> resultList, ArrayList<Integer> arrLBaseValues, ArrayList<Integer> arrLWitnValues) {
+    for (int i = 0; i < arrLBaseValues.size(); i++) {
+      for (int j = 0; j < arrLWitnValues.size(); j++) {
+        resultList.add(new Tuple(arrLBaseValues.get(i), arrLWitnValues.get(j)));
+        this.anz++;
+      }
+    }
+  }
+
+  @SuppressWarnings("boxing")
+  public ArrayList<Tuple> tableToTuple() {
     int pos_h = 0;
     int pos_v = 0;
     boolean merk_1 = false;
     ArrayList<Tuple> arrL = new ArrayList<Tuple>();
-    ArrayList<Integer> arrLBaseValues = new ArrayList<Integer>();
-    ArrayList<Integer> arrLWitnValues = new ArrayList<Integer>();
     //look for matches
     for (int h = 1; h < this.base.size() + 1; h++) {
       for (int v = 1; v < this.witness.size() + 1; v++) {
         if ((Integer) array[v][h] == 0) {
           saveNewTuple(arrL, this.base.get(array[0][h]), this.witness.get(array[v][0]));
           //if match, go to the next column  
-          break;
+          //break;
         } else if ((Integer) array[v][h] == 1 && !merk_1) {
           //note just the first 1-match in a column
           pos_h = h;
@@ -65,14 +101,10 @@ public class LevTable {
         saveNewTuple(arrL, this.base.get(array[0][pos_h]), this.witness.get(array[pos_v][0]));
       }
     }
-    Iterator<Tuple> it = arrL.iterator();
-    int i = 0;
-    while (it.hasNext()) {
-      this.tuples[i] = it.next();
-      i++;
-    }
+    return arrL;
   }
 
+  @SuppressWarnings("boxing")
   public String[] mapToStringArray(Map<String, ArrayList<Integer>> source, Integer length) {
     String[] sArray = new String[length + 1];
     Iterator<String> itKey = source.keySet().iterator();
@@ -86,6 +118,7 @@ public class LevTable {
     return sArray;
   }
 
+  @SuppressWarnings("boxing")
   public void fillArrayCells() {
     String[] base = mapToStringArray(this.base, this.base.size());
     String[] witn = mapToStringArray(this.witness, this.witness.size());
@@ -99,10 +132,10 @@ public class LevTable {
           array[v][h] = witn[v];
         } else {
           if (getLevDist(base[h], witn[v]) == 0) {
-            this.anz++;
+            //this.anz++;
             //break;
           } else if (getLevDist(base[h], witn[v]) == 1) {
-            this.anz++;
+            //this.anz++;
           } else {}
           array[v][h] = getLevDist(base[h], witn[v]);
         }
@@ -120,4 +153,5 @@ public class LevTable {
     }
     return "done";
   }
+
 }
