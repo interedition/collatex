c231ab13aacb50259f06d64482cc76c3fb68646e
michaelleuk <>
2008-04-30 16:13:42 +0000

    [ML] some changes

--- src/main/java/com/sd_editions/collatex/Collate/Tuple.java
+++ src/main/java/com/sd_editions/collatex/Collate/Tuple.java
@@ -19,6 +19,29 @@ public class Tuple implements Comparable {
 
   public int compareTo(Object obj) {
     Tuple tmp = (Tuple) obj;
+    int newer = Math.abs(tmp.baseIndex - tmp.witnessIndex);
+    int me = Math.abs(this.baseIndex - this.witnessIndex);
+    if (me < newer) {
+      return -1;
+    } else if (me > newer) {
+      return 1;
+    } else if (me == newer) {
+      int newer2 = tmp.witnessIndex;
+      int me2 = this.witnessIndex;
+      if (me2 == newer2) {
+        return 0;
+      } else if (me2 < newer2) {
+        return -1;
+      } else if (me2 > newer2) {
+        return 1;
+
+      }
+    }
+    return 0;
+  }
+
+  public int compareTo_old(Object obj) {
+    Tuple tmp = (Tuple) obj;
     double newer = tmp.baseIndex + (double) tmp.witnessIndex / 10;
     double me = this.baseIndex + (double) this.witnessIndex / 10;
     if (me < newer) {
