1d567ddeb69e03cb1635aa1e943d4910b2554965
rhdekker <>
2008-08-01 11:55:15 +0000

    [RHD] Reordered use cases to the order of Peter's blog post

--- src/main/java/com/sd_editions/collatex/Web/UseCasePage.java
+++ src/main/java/com/sd_editions/collatex/Web/UseCasePage.java
@@ -15,16 +15,17 @@ public class UseCasePage extends WebPage {
 
   public UseCasePage() {
     List<String[]> usecases = Lists.newArrayList();
+    usecases.add(new String[] { "The black cat", "The black and white cat", "The black and green cat" });
+    usecases.add(new String[] { "The black cat", "The black and white cat", "The black and green cat", "The black very special cat", "The black not very special cat" });
+    usecases.add(new String[] { "He was agast, so", "He was agast", "So he was agast", "He was so agast", "He was agast and feerd", "So was he agast" });
+
     usecases.add(new String[] { "a cat or dog", "a cat and dog and", "a cat and dog and" });
     usecases.add(new String[] { "Auch hier hab ich wieder ein Plätzchen", "Ich hab auch hier wieder ein Pläzchen", "Ich hab auch hier wieder ein Pläzchen" });
-    usecases.add(new String[] { "He was agast, so", "He was agast", "So he was agast", "He was so agast", "He was agast and feerd", "So was he agast" });
     usecases.add(new String[] { "the big bug had a big head", "the bug big had a big head", "the bug had a small head" });
     usecases.add(new String[] { "the big bug had a big head", "the bug had a small head", "the bug had a small head" });
     usecases.add(new String[] { "the black cat and the black mat", "the black dog and the black mat", "the black dog and the black mat" });
     usecases.add(new String[] { "the black cat on the table", "the black saw the black cat on the table", "the black saw the black cat on the table" });
     usecases.add(new String[] { "the black cat sat on the mat", "the cat sat on the black mat", "the cat sat on the black mat" });
-    usecases.add(new String[] { "The black cat", "The black and white cat", "The black and green cat" });
-    usecases.add(new String[] { "The black cat", "The black and white cat", "The black and green cat", "The black very special cat", "The black not very special cat" });
     usecases.add(new String[] { "the black cat", "THE BLACK CAT", "The black cat", "The, black cat" });
     usecases.add(new String[] { "the bug big had a big head", "the bug had a small head", "the bug had a small head" });
     usecases.add(new String[] { "the drought of march hath perced to the root and is this the right", "the first march of drought pierced to the root and this is the ",
