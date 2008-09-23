00abf4def05858c30947fc31fd515b941092b7dd
michaelleuk <>
2008-09-23 14:24:47 +0000

    [ML]some modifications

--- src/main/java/com/sd_editions/collatex/Collate/TupleComparatorBI.java
+++ src/main/java/com/sd_editions/collatex/Collate/TupleComparatorBI.java
@@ -4,11 +4,19 @@ import java.util.Comparator;
 
 public class TupleComparatorBI implements Comparator<Tuple> {
   public int compare(Tuple tupA, Tuple tupB) {
-    if (tupA.baseIndex < tupB.baseIndex) {
-      return -1;
-    } else if (tupA.baseIndex > tupB.baseIndex) {
-      return 1;
-    }
+    //
+    double valTup_A = transformTuple(tupA);
+    double valTup_B = transformTuple(tupB);
+    if (valTup_A < valTup_B) return -1;
+    if (valTup_A > valTup_B) return 1;
+
     return 0;
+  }
+
+  private double transformTuple(Tuple r) {
+    Double str1 = new Double(Integer.toString(r.getBaseIndex()));
+    Double str2 = new Double(Integer.toString(r.getWitnessIndex()));
+
+    return str1 + (str2 * 0.1);
   }
 }
