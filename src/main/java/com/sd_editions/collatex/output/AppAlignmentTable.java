ed34e99d83433226e88b96608b4da46e84a60dee
Thorsten Vitt <vitt@linglit.tu-darmstadt.de>
2009-05-06 11:12:48 +0100

    [TV] Test and non-working skeleton class for alignment output

---
+++ src/main/java/com/sd_editions/collatex/output/AppAlignmentTable.java
@@ -0,0 +1,21 @@
+package com.sd_editions.collatex.output;
+
+import java.util.List;
+
+import com.sd_editions.collatex.permutations.MatchUnmatch;
+
+public class AppAlignmentTable {
+
+  private final List<MatchUnmatch> matchUnmatch;
+
+  public AppAlignmentTable(List<MatchUnmatch> _matchUnmatch) {
+    this.matchUnmatch = _matchUnmatch;
+  }
+
+  public String toXML() {
+    return null;
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ src/test/java/com/sd_editions/collatex/output/XMLAppOutputTest.java
@@ -0,0 +1,25 @@
+package com.sd_editions.collatex.output;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.sd_editions.collatex.permutations.CollateCore;
+import com.sd_editions.collatex.permutations.MatchUnmatch;
+
+public class XMLAppOutputTest {
+
+  /**
+   * The first example from #6 (http://arts-itsee.bham.ac.uk/trac/interedition/ticket/6) (without witness C for now)
+   */
+  @Test
+  public void testSimpleSubstitutionOutput() {
+    CollateCore collateCore = new CollateCore("the black cat and the black mat", "the black dog and the black mat");
+    List<MatchUnmatch> matchUnmatch = collateCore.doCompareWitnesses(collateCore.getWitness(1), collateCore.getWitness(2));
+    AppAlignmentTable alignmentTable = new AppAlignmentTable(matchUnmatch);
+    String xml = alignmentTable.toXML();
+    assertEquals("<collation>the black <app><rdg wit=\"#A\">cat</rdg><rdg wit=\"#B #C\">dog</rdg></app>  and the black mat</collation>", xml);
+  }
+}
