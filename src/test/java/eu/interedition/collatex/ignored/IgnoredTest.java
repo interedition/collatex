c1892b5237cb35c9f2bcc151e3ef1169f59ad1e2
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2009-06-24 14:56:27 +0200

    [RHD] Moved some unit tests

---
+++ src/test/java/eu/interedition/collatex/ignored/IgnoredTest.java
@@ -0,0 +1,110 @@
+package eu.interedition.collatex.ignored;
+
+import static junit.framework.Assert.assertEquals;
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.google.common.base.Join;
+import com.google.common.collect.Lists;
+
+import eu.interedition.collatex.collation.CollateCore;
+import eu.interedition.collatex.collation.Match;
+import eu.interedition.collatex.collation.MatchNonMatch;
+import eu.interedition.collatex.collation.NonMatch;
+import eu.interedition.collatex.collation.sequences.MatchSequence;
+import eu.interedition.collatex.input.WitnessBuilder;
+
+public class IgnoredTest {
+  private WitnessBuilder builder;
+
+  @Before
+  public void setUp() throws Exception {
+    builder = new WitnessBuilder();
+  }
+
+  @Test
+  @Ignore
+  public void testGetAllMatchNonMatchPermutations() {
+    CollateCore cc = new CollateCore(builder.buildWitnesses("The black car", "The dark car", "the dark day"));
+    List<List<MatchNonMatch>> allMatchNonMatchPermutations = cc.getAllMatchNonMatchPermutations();
+    // there should be 3 witness pairs
+    assertEquals(3, allMatchNonMatchPermutations.size());
+    List<MatchNonMatch> list = allMatchNonMatchPermutations.get(0);
+    assertEquals(1, list.size());
+    showAllMatchNonMatchPermutations(allMatchNonMatchPermutations);
+  }
+
+  @Test
+  @Ignore
+  public void testGetAllMatchNonMatchPermutations1() {
+    CollateCore cc = new CollateCore(builder.buildWitnesses("The cat chases the dog and mouse", "The dog chases the cat and mouse", "the dog and mouse chase the cat"));
+    List<List<MatchNonMatch>> allMatchNonMatchPermutations = cc.getAllMatchNonMatchPermutations();
+    showAllMatchNonMatchPermutations(allMatchNonMatchPermutations);
+    assertEquals(3, allMatchNonMatchPermutations.size());
+  }
+
+  void showAllMatchNonMatchPermutations(List<List<MatchNonMatch>> allMatchNonMatchPermutations) {
+    for (List<MatchNonMatch> list : allMatchNonMatchPermutations) {
+      println("witness pair " + allMatchNonMatchPermutations.indexOf(list));
+      for (MatchNonMatch matchNonMatch : list) {
+        println("  matchNonMatch " + list.indexOf(matchNonMatch));
+        List<MatchSequence> matchSequencesForBase = matchNonMatch.getMatchSequencesForBase();
+        List<NonMatch> nonMatches = matchNonMatch.getNonMatches();
+        for (MatchSequence matchSequence : matchSequencesForBase) {
+          println("    matchSequenceForBase " + matchSequencesForBase.indexOf(matchSequence));
+          List<Match> matches = matchSequence.getMatches();
+          List<String> baseWords = Lists.newArrayList();
+          List<String> witnessWords = Lists.newArrayList();
+          for (Match match : matches) {
+            baseWords.add(match.getBaseWord().original);
+            witnessWords.add(match.getWitnessWord().original);
+          }
+          println("      '" + Join.join(" ", baseWords) + "' -> '" + Join.join(" ", witnessWords) + "'");
+        }
+      }
+    }
+  }
+
+  private void print(String string) {
+    System.out.print(string);
+  }
+
+  private void println(String string) {
+    System.out.println(string);
+  }
+
+  @Test
+  @Ignore
+  public void testSymmetry1() {
+    symmetryTest("and then, the black cat and the black dog", "the black cat and the black dog");
+  }
+
+  @Test
+  @Ignore
+  public void testSymmetry2() {
+    symmetryTest("the black cat and the black dog, and then", "the black cat and the black dog");
+  }
+
+  @Test
+  @Ignore
+  public void testSymmetry3() {
+    symmetryTest("the black cat and the black dog", "the calico cat and the red dog");
+  }
+
+  private void symmetryTest(String string1, String string2) {
+    CollateCore colors = new CollateCore(builder.build(string1), builder.build(string2));
+    Assert.fail();
+    //    Util.p("ORIGINAL");
+    //    List<Modifications> permutations = colors.compareWitness(1, 2);
+    //    //    Util.p("MIRROR");
+    //    List<Modifications> mirrorPermutations = colors.compareWitness(2, 1);
+    //    assertEquals(permutations.size(), mirrorPermutations.size());
+  }
+
+}
