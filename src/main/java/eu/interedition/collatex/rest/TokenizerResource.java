b25fe16b2322ab79623976ac5e8db4da4799d086
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2009-09-23 18:00:48 +0100

    [RHD] Experiment with output generated by JsonLib

--- src/main/java/eu/interedition/collatex/rest/TokenizerResource.java
+++ src/main/java/eu/interedition/collatex/rest/TokenizerResource.java
@@ -1,14 +1,18 @@
 package eu.interedition.collatex.rest;
 
 import java.util.Arrays;
+import java.util.List;
+
+import net.sf.json.JSONObject;
 
 import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 import org.restlet.representation.Representation;
-import org.restlet.representation.StringRepresentation;
 import org.restlet.representation.Variant;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
+
+import com.google.common.collect.Lists;
 
 import eu.interedition.collatex.input.Witness;
 import eu.interedition.collatex.input.builders.WitnessBuilder;
@@ -39,9 +43,22 @@ public class TokenizerResource extends ServerResource {
 
     CharSequence text = visitor.getResult();
     //CharSequence text = "Welkom bij de tokenizer";
-    MediaType mediaType = MediaType.TEXT_PLAIN;
-    StringRepresentation result = new StringRepresentation(text, mediaType);
-    return result;
+    MediaType mediaType = MediaType.APPLICATION_JSON;
+
+    // TODO: make new visitor out of this!
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("ID", witness.id);
+    List<JSONObject> bla = Lists.newArrayList();
+    JSONObject w1 = new JSONObject();
+    w1.put("content", "first");
+    JSONObject w2 = new JSONObject();
+    w2.put("content", "second");
+    bla.add(w1);
+    bla.add(w2);
+    jsonObject.put("words", bla);
+    //    StringRepresentation result = new StringRepresentation(text, mediaType);
+    Representation representation = new JsonLibRepresentation(jsonObject);
+    return representation;
     //      try {
     //        String id = (String) getRequest().getAttributes().get("name");
     //        Database database = Database.getInstance();
