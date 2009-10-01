a7d1059db2c636f5e01c9e53853abfaf1df28aeb
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2009-10-01 17:07:58 +0200

    [RHD] Made demo work

--- src/main/java/eu/interedition/collatex/rest/DemoResource.java
+++ src/main/java/eu/interedition/collatex/rest/DemoResource.java
@@ -3,20 +3,33 @@ package eu.interedition.collatex.rest;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 import org.restlet.representation.Representation;
-import org.restlet.representation.StringRepresentation;
 import org.restlet.representation.Variant;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 
+import com.google.common.collect.Lists;
+
+import eu.interedition.collatex.alignment.multiple_witness.AlignmentTable2;
+import eu.interedition.collatex.alignment.multiple_witness.visitors.JSONObjectTableVisitor;
+import eu.interedition.collatex.input.Witness;
+import eu.interedition.collatex.input.WitnessSet;
+import eu.interedition.collatex.input.builders.WitnessBuilder;
+
 public class DemoResource extends ServerResource {
   private static final MediaType[] TYPES = { MediaType.TEXT_HTML, MediaType.TEXT_PLAIN };
   private String readFileToString;
+  private final WitnessSet set;
 
+  @SuppressWarnings("unchecked")
   public DemoResource() {
     getVariants().put(Method.GET, Arrays.asList(TYPES));
     File file = new File("docs/demodirk/json_input.txt");
@@ -27,20 +40,32 @@ public class DemoResource extends ServerResource {
       e.printStackTrace();
     }
 
+    List<Witness> witnesses = Lists.newArrayList();
+    WitnessBuilder builder = new WitnessBuilder();
+    try {
+      JSONObject jsonObject = new JSONObject(readFileToString);
+      Iterator<String> keys = jsonObject.keys();
+      while (keys.hasNext()) {
+        String key = keys.next();
+        String text = jsonObject.getString(key);
+        Witness witness = builder.build(key, text);
+        witnesses.add(witness);
+      }
+    } catch (JSONException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    set = new WitnessSet(witnesses);
   }
 
   @Override
   public Representation get(Variant variant) throws ResourceException {
-    Representation representation = new StringRepresentation(readFileToString);
-    //    // String witnessString = getQuery().getFirstValue("witness");
-    //    //    System.err.println("!!" + witnessString);
-    //    String[] witnessStrings = getQuery().getValuesArray("witness");
-    //    WitnessSet set = WitnessSet.createWitnessSet(witnessStrings);
-    //    AlignmentTable2 alignmentTable = set.createAlignmentTable();
-    //    JSONObjectTableVisitor visitor = new JSONObjectTableVisitor();
-    //    alignmentTable.accept(visitor);
-    //    JSONObject jsonObject = visitor.getJSONObject();
-    //    Representation representation = new JsonLibRepresentation(jsonObject);
+    //    Representation representation = new StringRepresentation(readFileToString);
+    AlignmentTable2 alignmentTable = set.createAlignmentTable();
+    JSONObjectTableVisitor visitor = new JSONObjectTableVisitor();
+    alignmentTable.accept(visitor);
+    net.sf.json.JSONObject jsonObject = visitor.getJSONObject();
+    Representation representation = new JsonLibRepresentation(jsonObject);
     return representation;
   }
   //    try {
