0176931a71e916856fe2f8757494920bf158664b
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2009-11-09 16:12:07 +0100

    [RHD] Added more tests to left to right matcher

--- src/test/java/eu/interedition/collatex/matching/LeftToRightMatchingTest.java
+++ src/test/java/eu/interedition/collatex/matching/LeftToRightMatchingTest.java
@@ -1,5 +1,6 @@
 package eu.interedition.collatex.matching;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import junit.framework.Assert;
@@ -57,6 +58,53 @@ public class LeftToRightMatchingTest {
     Assert.assertEquals(1, match.getBaseWord().getStartPosition());
     Assert.assertEquals(2, match.getWitnessWord().getStartPosition());
   }
+
+  @Test
+  public void testAdditionInTheMiddle() {
+    final Segment a = builder.build("a", "everything matches").getFirstSegment();
+    final Segment b = builder.build("b", "everything addition matches").getFirstSegment();
+    final SubsegmentExtractor sse = new SubsegmentExtractor(a, b);
+    sse.go();
+    final WitnessSegmentPhrases pa = sse.getWitnessSegmentPhrases("a");
+    final WitnessSegmentPhrases pb = sse.getWitnessSegmentPhrases("b");
+
+    Assert.assertEquals(2, pa.size());
+    Assert.assertEquals(3, pb.size());
+
+    final Set<Match<Phrase>> matches = LeftToRightMatcher.match(pa, pb);
+    Assert.assertEquals(2, matches.size());
+
+    final Iterator<Match<Phrase>> i = matches.iterator();
+    final Match<Phrase> match = i.next();
+    Assert.assertEquals(1, match.getBaseWord().getStartPosition());
+    Assert.assertEquals(1, match.getWitnessWord().getStartPosition());
+
+    final Match<Phrase> match2 = i.next();
+    Assert.assertEquals(2, match2.getBaseWord().getStartPosition());
+    Assert.assertEquals(3, match2.getWitnessWord().getStartPosition());
+  }
+
+  @Test
+  public void testAdditionAtTheEnd() {
+    final Segment a = builder.build("a", "everything matches").getFirstSegment();
+    final Segment b = builder.build("b", "everything matches addition").getFirstSegment();
+    final SubsegmentExtractor sse = new SubsegmentExtractor(a, b);
+    sse.go();
+    final WitnessSegmentPhrases pa = sse.getWitnessSegmentPhrases("a");
+    final WitnessSegmentPhrases pb = sse.getWitnessSegmentPhrases("b");
+
+    Assert.assertEquals(1, pa.size());
+    Assert.assertEquals(2, pb.size());
+
+    final Set<Match<Phrase>> matches = LeftToRightMatcher.match(pa, pb);
+    Assert.assertEquals(1, matches.size());
+    final Match<Phrase> match = matches.iterator().next();
+    Assert.assertEquals(1, match.getBaseWord().getStartPosition());
+    Assert.assertEquals(1, match.getWitnessWord().getStartPosition());
+  }
+
+  // TODO: add a transposition test!
+  // There I don't want the thing to be smart!
 
   //  @Test
   //  public void testOmittedInFront() {
