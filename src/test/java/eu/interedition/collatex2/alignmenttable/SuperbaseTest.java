9bf7eb1135c95eb9e640f7fb6096038cf036f47c
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-03-22 15:54:10 +0100

    [RHD] Made last Superbase unit test work.

--- src/test/java/eu/interedition/collatex2/alignmenttable/SuperbaseTest.java
+++ src/test/java/eu/interedition/collatex2/alignmenttable/SuperbaseTest.java
@@ -1,13 +1,18 @@
 package eu.interedition.collatex2.alignmenttable;
 
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.common.collect.Lists;
+
 import eu.interedition.collatex2.implementation.Factory;
 import eu.interedition.collatex2.implementation.alignmenttable.Column3;
 import eu.interedition.collatex2.implementation.alignmenttable.Superbase4;
+import eu.interedition.collatex2.interfaces.IAlignmentTable;
 import eu.interedition.collatex2.interfaces.IColumn;
 import eu.interedition.collatex2.interfaces.INormalizedToken;
 import eu.interedition.collatex2.interfaces.ISuperbase;
@@ -21,15 +26,14 @@ public class SuperbaseTest {
     factory = new Factory();
   }
 
-  //  
-  //  @Test
-  //  public void testCreateSuperBase() {
-  //    final Witness a = builder.build("A", "the first witness");
-  //    final AlignmentTable3 alignmentTable = new AlignmentTable3();
-  //    alignmentTable.addWitness(a);
-  //    final SegmentSuperbase superbase = SegmentSuperbase.create(alignmentTable);
-  //    assertEquals("the first witness", superbase.toString());
-  //  }
+  @Test
+  public void testCreateSuperBase() {
+    final IWitness a = factory.createWitness("A", "the first witness");
+    final List<IWitness> set = Lists.newArrayList(a);
+    final IAlignmentTable alignmentTable = factory.createNewAlignmentTable(set);
+    final ISuperbase superbase = Superbase4.create(alignmentTable);
+    Assert.assertEquals("Superbase: (the, first, witness)", superbase.toString());
+  }
 
   @Test
   public void testSuperbase1() {
