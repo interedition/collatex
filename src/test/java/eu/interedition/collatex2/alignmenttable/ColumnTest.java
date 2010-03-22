6a6ff9bd4087ca3a15e7bd462f0212d2c6d92d07
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-03-22 11:15:28 +0100

    [RHD] Moved tests involving the Superbase from ColumnTest to SuperbaseTest

--- src/test/java/eu/interedition/collatex2/alignmenttable/ColumnTest.java
+++ src/test/java/eu/interedition/collatex2/alignmenttable/ColumnTest.java
@@ -6,18 +6,14 @@ import java.util.NoSuchElementException;
 import junit.framework.Assert;
 
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import eu.interedition.collatex2.implementation.Factory;
 import eu.interedition.collatex2.implementation.alignmenttable.Column3;
-import eu.interedition.collatex2.implementation.alignmenttable.Superbase4;
 import eu.interedition.collatex2.interfaces.IColumn;
 import eu.interedition.collatex2.interfaces.INormalizedToken;
-import eu.interedition.collatex2.interfaces.ISuperbase;
 import eu.interedition.collatex2.interfaces.IWitness;
 
-//TODO: move superbase part to another test!
 //TODO: Add tests for the state of the Column
 //NOTE: this is an implementation test!
 //NOTE: constructors are called directly!
@@ -88,37 +84,4 @@ public class ColumnTest {
     Assert.assertTrue(column.containsWitness("C"));
   }
 
-  @Ignore
-  @Test
-  public void testSuperbase1() {
-    final IWitness witness = factory.createWitness("A", "first");
-    final IWitness witnessB = factory.createWitness("B", "second");
-    final IWitness witnessC = factory.createWitness("C", "third");
-    final INormalizedToken word = witness.getTokens().get(0);
-    final INormalizedToken wordB = witnessB.getTokens().get(0);
-    final INormalizedToken wordC = witnessC.getTokens().get(0);
-    final IColumn column = new Column3(word);
-    column.addVariant(wordB);
-    column.addVariant(wordC);
-    final ISuperbase superbase = new Superbase4();
-    column.addToSuperbase(superbase);
-    Assert.assertEquals("first second third", superbase.toString());
-  }
-
-  @Ignore
-  @Test
-  public void testSuperbase2() {
-    final IWitness witness = factory.createWitness("A", "first");
-    final IWitness witnessB = factory.createWitness("B", "match");
-    final IWitness witnessC = factory.createWitness("C", "match");
-    final INormalizedToken word = witness.getTokens().get(0);
-    final INormalizedToken wordB = witnessB.getTokens().get(0);
-    final INormalizedToken wordC = witnessC.getTokens().get(0);
-    final IColumn column = new Column3(word);
-    column.addVariant(wordB);
-    column.addMatch(wordC);
-    final ISuperbase superbase = new Superbase4();
-    column.addToSuperbase(superbase);
-    Assert.assertEquals("first match", superbase.toString());
-  }
 }
--- src/test/java/eu/interedition/collatex2/alignmenttable/SuperbaseTest.java
+++ src/test/java/eu/interedition/collatex2/alignmenttable/SuperbaseTest.java
@@ -1,15 +1,27 @@
 package eu.interedition.collatex2.alignmenttable;
 
-import eu.interedition.collatex.input.builders.WitnessBuilder;
+import junit.framework.Assert;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import eu.interedition.collatex2.implementation.Factory;
+import eu.interedition.collatex2.implementation.alignmenttable.Column3;
+import eu.interedition.collatex2.implementation.alignmenttable.Superbase4;
+import eu.interedition.collatex2.interfaces.IColumn;
+import eu.interedition.collatex2.interfaces.INormalizedToken;
+import eu.interedition.collatex2.interfaces.ISuperbase;
+import eu.interedition.collatex2.interfaces.IWitness;
 
 public class SuperbaseTest {
-  private static WitnessBuilder builder;
+  private static Factory factory;
 
-  //  @BeforeClass
-  //  public static void setUp() {
-  //    builder = new WitnessBuilder();
-  //  }
-  //
+  @BeforeClass
+  public static void setUp() {
+    factory = new Factory();
+  }
+
   //  
   //  @Test
   //  public void testCreateSuperBase() {
@@ -19,5 +31,39 @@ public class SuperbaseTest {
   //    final SegmentSuperbase superbase = SegmentSuperbase.create(alignmentTable);
   //    assertEquals("the first witness", superbase.toString());
   //  }
+
+  @Ignore
+  @Test
+  public void testSuperbase1() {
+    final IWitness witness = factory.createWitness("A", "first");
+    final IWitness witnessB = factory.createWitness("B", "second");
+    final IWitness witnessC = factory.createWitness("C", "third");
+    final INormalizedToken word = witness.getTokens().get(0);
+    final INormalizedToken wordB = witnessB.getTokens().get(0);
+    final INormalizedToken wordC = witnessC.getTokens().get(0);
+    final IColumn column = new Column3(word);
+    column.addVariant(wordB);
+    column.addVariant(wordC);
+    final ISuperbase superbase = new Superbase4();
+    column.addToSuperbase(superbase);
+    Assert.assertEquals("first second third", superbase.toString());
+  }
+
+  @Ignore
+  @Test
+  public void testSuperbase2() {
+    final IWitness witness = factory.createWitness("A", "first");
+    final IWitness witnessB = factory.createWitness("B", "match");
+    final IWitness witnessC = factory.createWitness("C", "match");
+    final INormalizedToken word = witness.getTokens().get(0);
+    final INormalizedToken wordB = witnessB.getTokens().get(0);
+    final INormalizedToken wordC = witnessC.getTokens().get(0);
+    final IColumn column = new Column3(word);
+    column.addVariant(wordB);
+    column.addMatch(wordC);
+    final ISuperbase superbase = new Superbase4();
+    column.addToSuperbase(superbase);
+    Assert.assertEquals("first match", superbase.toString());
+  }
 
 }
