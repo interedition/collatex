e8c6ea067ea549afb372696349f94e721d69f53f
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-04-07 17:57:57 +0200

    [RHD] moved another unit test from SuperBaseAlgorithmTest to new package (does not work yet because of missing parts!)

--- src/test/java/eu/interedition/collatex/alignmenttable/SuperbaseAlgorithmTest.java
+++ src/test/java/eu/interedition/collatex/alignmenttable/SuperbaseAlgorithmTest.java
@@ -15,30 +15,6 @@ public class SuperbaseAlgorithmTest {
     builder = new WitnessBuilder();
   }
 
-  // TODO make the tostring on the alignmenttable
-  // TODO work with multiple spaces for an empty cell
-  // TODO fix the gap bug for the last gap
-
-  // TODO add this for replacements in comb. with transposit.
-  //  // Note: this is with an unequal transposition sequence size!
-  //  @Test
-  //  public void testAdditionInCombinationWithTransposition() {
-  //    Witness w1 = builder.build("A", "the cat is black");
-  //    Witness w2 = builder.build("B", "black is the cat");
-  //    Witness w3 = builder.build("C", "black and white is the cat");
-  //    SuperbaseAlgorithm magic = new SuperbaseAlgorithm(w1, w2, w3);
-  //    AlignmentTable2 table = magic.createAlignmentTable();
-  //    String expected;
-  //    expected = "A: the|cat| |is|black| |\n";
-  //    expected += "B: black| | |is||the|cat\n";
-  //    expected += "C: black|and|white|is|the|cat\n";
-  //
-  //    assertEquals(expected, table.toString());
-  //  }
-
-  // TODO test a variant with multiple words
-  // TODO hint: more words than the original string it replaces
-
   // TODO test a variant where one words turns out
   // TODO to be a match later
   // this is an addition.. not the easiest test
