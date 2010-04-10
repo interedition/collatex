30747ca0d00aaa7838afa7b0a060bd3721c3188e
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-04-10 15:56:57 +0200

    [RHD] added parallel segmentation table and column

---
+++ src/main/java/eu/interedition/collatex2/implementation/parallel_segmentation/ParallelSegmentationTable.java
@@ -0,0 +1,48 @@
+package eu.interedition.collatex2.implementation.parallel_segmentation;
+
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+public class ParallelSegmentationTable {
+
+  private final List<SegmentColumn> segmentColumns;
+
+  public ParallelSegmentationTable(final List<SegmentColumn> segmentColumns) {
+    this.segmentColumns = segmentColumns;
+  }
+
+  private List<SegmentColumn> getColumns() {
+    return segmentColumns;
+  }
+
+  private List<String> getSigli() {
+    final Set<String> sigli = Sets.newLinkedHashSet();
+    for (final SegmentColumn column : segmentColumns) {
+      sigli.addAll(column.getSigli());
+    }
+    return Lists.newArrayList(sigli);
+  }
+
+  public static String tableToHTML(final ParallelSegmentationTable table) {
+    final StringBuilder tableHTML = new StringBuilder("<div id=\"alignment-table\"><h4>Alignment Table:</h4>\n<table border=\"1\" class=\"alignment\">\n");
+
+    for (final String witnessId : table.getSigli()) {
+      tableHTML.append("<tr>").append("<th>Witness ").append(witnessId).append(":</th>");
+      for (final SegmentColumn column : table.getColumns()) {
+        tableHTML.append("<td>");
+        if (column.containsWitness(witnessId)) {
+          tableHTML.append(column.getPhrase(witnessId).getContent()); // TODO add escaping!
+        }
+        tableHTML.append("</td>");
+      }
+      tableHTML.append("</tr>\n");
+    }
+    tableHTML.append("</table>\n</div>\n\n");
+    //    return alignmentTable.toString().replaceAll("\n", "<br/>") + "<br/>";
+    return tableHTML.toString();
+  }
+
+}
---
+++ src/main/java/eu/interedition/collatex2/implementation/parallel_segmentation/SegmentColumn.java
@@ -0,0 +1,38 @@
+package eu.interedition.collatex2.implementation.parallel_segmentation;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+
+import eu.interedition.collatex2.implementation.input.Phrase;
+import eu.interedition.collatex2.interfaces.INormalizedToken;
+import eu.interedition.collatex2.interfaces.IPhrase;
+
+public class SegmentColumn {
+
+  private final List<String> sigli;
+  private final Multimap<String, INormalizedToken> sigilToTokens;
+
+  public SegmentColumn(final List<String> sigli) {
+    this.sigli = sigli;
+    this.sigilToTokens = Multimaps.newLinkedHashMultimap();
+  }
+
+  public void addToken(final String sigil, final INormalizedToken token) {
+    sigilToTokens.put(sigil, token);
+  }
+
+  public boolean containsWitness(final String sigil) {
+    return sigilToTokens.containsKey(sigil);
+  }
+
+  public IPhrase getPhrase(final String witnessId) {
+    return new Phrase(Lists.newArrayList(sigilToTokens.get(witnessId)));
+  }
+
+  public List<String> getSigli() {
+    return sigli;
+  }
+}
