c43b269b534e488a2f899c20ff105656d8286e03
Gregor Middell <gregor@middell.net>
2010-04-13 15:43:29 +0200

    package collatex-web as a single runnable jar

--- collatex-web/src/main/java/eu/interedition/collatex2/rest/RestServer.java
+++ collatex-web/src/main/java/eu/interedition/collatex2/rest/RestServer.java
@@ -6,8 +6,18 @@ import org.restlet.data.Protocol;
 public class RestServer {
   public static void main(final String[] args) {
     try {
+      int serverPort = 8182;
+
+      if (args.length > 0) {
+        try {
+          serverPort = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+          e.printStackTrace();
+        }
+      }
+      System.out.println(String.format("Running CollateX server on port %d", serverPort));
       final Component component = new Component();
-      component.getServers().add(Protocol.HTTP, 8182);
+      component.getServers().add(Protocol.HTTP, serverPort);
       component.getDefaultHost().attach(new RestApplication());
       component.start();
     } catch (final Exception e) {
