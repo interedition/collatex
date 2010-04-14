dc5bdef997300d49df0587a23c76d1c86397c6d4
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-04-14 10:59:15 +0200

    [RHD] Moved additionView method from ColorsView over to StringCallback

--- collatex-legacy/src/main/java/com/sd_editions/collatex/Web/ColorsView.java
+++ collatex-legacy/src/main/java/com/sd_editions/collatex/Web/ColorsView.java
@@ -11,14 +11,11 @@ import eu.interedition.collatex.alignment.functions.Aligner;
 import eu.interedition.collatex.alignment.multiple_witness.AlignmentTable2;
 import eu.interedition.collatex.alignment.multiple_witness.AlignmentTableCreator;
 import eu.interedition.collatex.collation.CollateCore;
-import eu.interedition.collatex.experimental.ngrams.alignment.Addition;
 import eu.interedition.collatex.experimental.ngrams.alignment.Modification;
 import eu.interedition.collatex.experimental.ngrams.alignment.Omission;
 import eu.interedition.collatex.experimental.ngrams.alignment.Replacement;
-import eu.interedition.collatex.input.Segment;
 import eu.interedition.collatex.input.Witness;
 import eu.interedition.collatex.input.WitnessSet;
-import eu.interedition.collatex.input.Word;
 import eu.interedition.collatex.visualization.Modifications;
 import eu.interedition.collatex.visualization.Visualization;
 
@@ -127,8 +124,8 @@ public class ColorsView {
       for (Modification modification : modificationsL) {
         if (modification instanceof WordDistanceMatch) {
           html.append("<li>" + wordDistanceMatch((WordDistanceMatch) modification) + "</li>");
-        } else if (modification instanceof Addition) {
-          html.append("<li>" + additionView((Addition) modification, base) + "</li>");
+//        } else if (modification instanceof Addition) {
+//          html.append("<li>" + additionView((Addition) modification, base) + "</li>");
         } else if (modification instanceof Omission) {
           html.append("<li>" + removalView((Omission) modification) + "</li>");
         } else if (modification instanceof Transposition) {
@@ -161,18 +158,4 @@ public class ColorsView {
     return "<i>" + removal.getOmittedWords() + "</i> at position " + (position) + " removed ";
   }
 
-  private String additionView(Addition addition, int base) {
-    Segment baseIndex = colors.getWitness(base).getFirstSegment();
-    StringBuffer html = new StringBuffer("<i>" + addition.getAddedWords() + "</i> added ");
-    List<Word> baseWords = baseIndex.getWords();
-    int position = addition.getPosition();
-    if (position == 1) {
-      html.append("before <i>" + baseWords.get(0) + "</i>");
-    } else if (position > baseWords.size()) {
-      html.append(" after <i>" + baseWords.get(baseWords.size() - 1) + "</i>");
-    } else {
-      html.append("between <i>" + baseWords.get(position - 2) + "</i> and <i>" + baseWords.get(position - 1) + "</i>");
-    }
-    return html.toString();
-  }
 }
