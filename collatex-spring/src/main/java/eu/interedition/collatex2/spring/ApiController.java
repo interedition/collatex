b97d8f867c2e0e6c97aa76d420dd8862bba034ae
Gregor Middell <gregor@middell.net>
2010-04-14 12:34:00 +0200

    started adding REST API to spring module

--- collatex-spring/src/main/java/eu/interedition/collatex2/spring/ApiController.java
+++ collatex-spring/src/main/java/eu/interedition/collatex2/spring/ApiController.java
@@ -4,12 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.google.common.collect.Lists;
+
+import eu.interedition.collatex2.implementation.Factory;
+import eu.interedition.collatex2.implementation.parallel_segmentation.AlignmentTableSegmentator;
+import eu.interedition.collatex2.interfaces.IAlignmentTable;
+import eu.interedition.collatex2.interfaces.IWitness;
+
 @Controller
 @RequestMapping("/api/**")
 public class ApiController {
-
+  private static final String WITNESS_1 = "abcdefgh";
+  private static final String WITNESS_2 = "defgxyz";
+  
+  private Factory collateXEngine = new Factory();
+  
   @RequestMapping("collate")
   public void collate(ModelMap model) {
+    final IWitness witness1 = collateXEngine.createWitness("A", WITNESS_1);
+    final IWitness witness2 = collateXEngine.createWitness("B", WITNESS_2);
+    final IAlignmentTable alignmentTable = collateXEngine.createAlignmentTable(Lists.newArrayList(witness1, witness2));
+    AlignmentTableSegmentator.createParrallelSegmentationTable(alignmentTable);
     model.addAttribute("hello", "world");
   }
 }
