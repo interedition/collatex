5674b0aa84203c9b0e0267cf5316c5cf61c6d9d3
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-04-28 18:40:16 +0200

    [RHD] Added simple unit test for case Spencer Howe

---
+++ collatex/src/test/java/eu/interedition/collatex2/experimental/graph/VariantGraphSpencerHoweTest.java
@@ -0,0 +1,39 @@
+package eu.interedition.collatex2.experimental.graph;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import eu.interedition.collatex2.implementation.CollateXEngine;
+import eu.interedition.collatex2.interfaces.IWitness;
+
+public class VariantGraphSpencerHoweTest {
+  private static CollateXEngine engine;
+
+  @BeforeClass
+  public static void setup() {
+    engine = new CollateXEngine();
+  }
+
+  @Test
+  public void testSimpleSpencerHowe() {
+    IWitness w1 = engine.createWitness("A", "a");
+    IWitness w2 = engine.createWitness("B", "b");
+    IWitness w3 = engine.createWitness("C", "a b");
+    VariantGraph graph = VariantGraph.create();
+    graph.addWitness(w1);
+    graph.addWitness(w2);
+    graph.addWitness(w3);
+    assertEquals(3, graph.getNodes().size());
+    final List<IVariantGraphArc> arcs = graph.getArcs();
+    assertEquals(3, arcs.size());
+    assertEquals("# -> a: A, C", arcs.get(0).toString());
+    assertEquals("# -> b: B", arcs.get(1).toString());
+    assertEquals("a -> b: C", arcs.get(2).toString());
+  }
+
+
+}
