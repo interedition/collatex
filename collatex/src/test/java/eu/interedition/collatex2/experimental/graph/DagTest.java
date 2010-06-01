5f2a797d14f745c8a55698e6f1f10f76d3ad0f5a
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-06-01 17:22:05 +0200

    [RHD] Added JGraphT 0.7.3 to MVN dependencies; added DirectedAcyclicGraph class from 0.8.1

---
+++ collatex/src/test/java/eu/interedition/collatex2/experimental/graph/DagTest.java
@@ -0,0 +1,10 @@
+package eu.interedition.collatex2.experimental.graph;
+
+import org.jgrapht.experimental.dag.DirectedAcyclicGraph;
+
+public class DagTest {
+  public void testBla() {
+    DirectedAcyclicGraph<Object, Object> graph = new DirectedAcyclicGraph<Object, Object>(Object.class);
+
+  }
+}
