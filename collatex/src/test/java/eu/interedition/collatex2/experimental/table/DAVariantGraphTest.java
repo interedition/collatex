4e3dfcf92149b1bed564a6802b4d59bed9b86059
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-06-07 18:12:12 +0200

    [RHD] Added future testcase for longest path algorithm

---
+++ collatex/src/test/java/eu/interedition/collatex2/experimental/table/DAVariantGraphTest.java
@@ -0,0 +1,42 @@
+package eu.interedition.collatex2.experimental.table;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import eu.interedition.collatex2.experimental.graph.IVariantGraphArc;
+import eu.interedition.collatex2.experimental.graph.VariantGraph;
+import eu.interedition.collatex2.implementation.CollateXEngine;
+import eu.interedition.collatex2.interfaces.IWitness;
+
+public class DAVariantGraphTest {
+  private static CollateXEngine engine;
+
+  @BeforeClass
+  public static void setup() {
+    engine = new CollateXEngine();
+  }
+
+  // TODO!!
+  // Note: this only tests the Graph, not the table!
+  @Test
+  public void testLongestPath() {
+    IWitness w1 = engine.createWitness("A", "a");
+    IWitness w2 = engine.createWitness("B", "b");
+    IWitness w3 = engine.createWitness("C", "a b");
+    VariantGraph graph = VariantGraph.create();
+    graph.addWitness(w1);
+    graph.addWitness(w2);
+    graph.addWitness(w3);
+    assertEquals(3, graph.getNodes().size());
+    final List<IVariantGraphArc> arcs = graph.getArcs();
+    assertEquals(3, arcs.size());
+    assertEquals("# -> a: A, C", arcs.get(0).toString());
+    assertEquals("# -> b: B", arcs.get(1).toString());
+    assertEquals("a -> b: C", arcs.get(2).toString());
+  }
+
+}
