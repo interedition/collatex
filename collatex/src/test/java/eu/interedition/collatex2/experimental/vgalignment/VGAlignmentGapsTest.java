ca496271bdd86abeab0f8d58bba22b09cf48ea99
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-07-22 18:10:58 +0200

    [RHD] Moved last unit test from AlignmentTest to VGAlignmentTest; removed empty test case; removed empty test package

--- collatex/src/test/java/eu/interedition/collatex2/experimental/vgalignment/VGAlignmentGapsTest.java
+++ collatex/src/test/java/eu/interedition/collatex2/experimental/vgalignment/VGAlignmentGapsTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import eu.interedition.collatex2.alignment.AlignmentTest;
 import eu.interedition.collatex2.implementation.CollateXEngine;
 import eu.interedition.collatex2.implementation.PairwiseAlignmentHelper;
 import eu.interedition.collatex2.interfaces.IAddition;
@@ -21,7 +20,7 @@ import eu.interedition.collatex2.interfaces.IWitness;
 //TODO: these test have to be rewritten to work with the 
 //variant graph based alignment
 public class VGAlignmentGapsTest {
-  private static final Logger LOG = LoggerFactory.getLogger(AlignmentTest.class);
+  private static final Logger LOG = LoggerFactory.getLogger(VGAlignmentGapsTest.class);
   private CollateXEngine      factory;
 
   @Before
--- collatex/src/test/java/eu/interedition/collatex2/experimental/vgalignment/VGAlignmentTest.java
+++ collatex/src/test/java/eu/interedition/collatex2/experimental/vgalignment/VGAlignmentTest.java
@@ -9,7 +9,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import eu.interedition.collatex2.alignment.AlignmentTest;
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
+
 import eu.interedition.collatex2.experimental.graph.IVariantGraph;
 import eu.interedition.collatex2.experimental.graph.VariantGraph2;
 import eu.interedition.collatex2.experimental.vg_alignment.IAlignment2;
@@ -17,12 +20,15 @@ import eu.interedition.collatex2.experimental.vg_alignment.IMatch2;
 import eu.interedition.collatex2.experimental.vg_alignment.ITransposition2;
 import eu.interedition.collatex2.experimental.vg_alignment.VariantGraphAligner;
 import eu.interedition.collatex2.implementation.CollateXEngine;
+import eu.interedition.collatex2.implementation.PairwiseAlignmentHelper;
+import eu.interedition.collatex2.interfaces.IAlignment;
+import eu.interedition.collatex2.interfaces.ITransposition;
 import eu.interedition.collatex2.interfaces.IWitness;
 
 //NOTE: these unit tests replace the tests in AlignmentTest!
 //TODO: have to be rewritten to work with VariantGraph alignment!
 public class VGAlignmentTest {
-  private static final Logger LOG = LoggerFactory.getLogger(AlignmentTest.class);
+  private static final Logger LOG = LoggerFactory.getLogger(VGAlignmentTest.class);
   private CollateXEngine factory;
 
   @Before
@@ -147,6 +153,46 @@ public class VGAlignmentTest {
     assertEquals("d", transpositions.get(0).getMatchA().getNormalized());
     assertEquals("d", transpositions.get(1).getMatchB().getNormalized());
     assertEquals("a b", transpositions.get(1).getMatchA().getNormalized());
+  }
+
+  @Test
+  public void testTransposition2() {
+    final IWitness a = factory.createWitness("A", "d a b");
+    final IWitness b = factory.createWitness("B", "a c b d");
+    IVariantGraph graph = VariantGraph2.create(a);
+    VariantGraphAligner aligner = new VariantGraphAligner(graph);
+    IAlignment2 alignment = aligner.align(b);
+    final List<ITransposition2> transpositions = alignment.getTranspositions();
+    assertEquals(3, transpositions.size());
+    assertEquals("d", transpositions.get(0).getMatchA().getNormalized());
+    assertEquals("a", transpositions.get(0).getMatchB().getNormalized());
+    assertEquals("a", transpositions.get(1).getMatchA().getNormalized());
+    assertEquals("b", transpositions.get(1).getMatchB().getNormalized());
+    assertEquals("b", transpositions.get(2).getMatchA().getNormalized());
+    assertEquals("d", transpositions.get(2).getMatchB().getNormalized());
+  }
+
+  @Test
+  public void testTransposition3() {
+    final IWitness a = factory.createWitness("1", "a b x c d e");
+    final IWitness b = factory.createWitness("2", "c e y a d b");
+    final IAlignment align = PairwiseAlignmentHelper.align(factory, a, b);
+    final List<ITransposition> transpositions = align.getTranspositions();
+    LOG.debug("transpositions=[" + Joiner.on(", ").join(Iterables.transform(transpositions, new Function<ITransposition, String>() {
+      @Override
+      public String apply(final ITransposition from) {
+        return from.getMatchA().getNormalized() + "=>" + from.getMatchB().getNormalized();
+      }
+    })) + "]");
+    assertEquals(4, transpositions.size());
+    assertEquals("a", transpositions.get(0).getMatchA().getNormalized());
+    assertEquals("c", transpositions.get(0).getMatchB().getNormalized());
+    assertEquals("b", transpositions.get(1).getMatchA().getNormalized());
+    assertEquals("e", transpositions.get(1).getMatchB().getNormalized());
+    assertEquals("c", transpositions.get(2).getMatchA().getNormalized());
+    assertEquals("a", transpositions.get(2).getMatchB().getNormalized());
+    assertEquals("e", transpositions.get(3).getMatchA().getNormalized());
+    assertEquals("b", transpositions.get(3).getMatchB().getNormalized());
   }
 
 
