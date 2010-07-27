d4c5f5e7a3f35388844f9284298566e231f19811
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-07-27 10:33:45 +0200

    [RHD] Moved unit test from AlignmentTableIndex0Test to VariantGraphIndexTest

--- collatex/src/test/java/eu/interedition/collatex2/experimental/graph/indexing/VariantGraphIndexTest.java
+++ collatex/src/test/java/eu/interedition/collatex2/experimental/graph/indexing/VariantGraphIndexTest.java
@@ -1,13 +1,13 @@
 package eu.interedition.collatex2.experimental.graph.indexing;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import eu.interedition.collatex2.experimental.graph.IVariantGraph;
-import eu.interedition.collatex2.experimental.graph.indexing.IVariantGraphIndex;
-import eu.interedition.collatex2.experimental.graph.indexing.VariantGraphIndex;
 import eu.interedition.collatex2.implementation.CollateXEngine;
 import eu.interedition.collatex2.interfaces.IWitness;
 
@@ -26,6 +26,36 @@ public class VariantGraphIndexTest {
     final IVariantGraphIndex index = VariantGraphIndex.create(graph, graph.findRepeatingTokens());
     assertEquals("AlignmentGraphIndex: (the, first, witness)", index.toString());
   }
+  
+  @Ignore
+  @Test
+  public void test1() {
+    final IWitness a = factory.createWitness("A", "the big black cat and the big black rat");
+    final IVariantGraph graph = factory.graph(a);
+    final IVariantGraphIndex index = VariantGraphIndex.create(graph, graph.findRepeatingTokens());
+    //TODO: Note: duplicated down below!
+    assertTrue(index.contains("cat"));
+    assertTrue(index.contains("and"));
+    assertTrue(index.contains("rat"));
+    assertEquals(15, index.size());
+    assertTrue(index.contains("# the"));
+    assertTrue(index.contains("# the big"));
+    assertTrue(index.contains("# the big black"));
+    assertTrue(index.contains("the big black cat"));
+    assertTrue(index.contains("big black cat"));
+    assertTrue(index.contains("black cat"));
+    assertTrue(index.contains("cat"));
+    assertTrue(index.contains("and"));
+    assertTrue(index.contains("and the"));
+    assertTrue(index.contains("and the big"));
+    assertTrue(index.contains("and the big black"));
+    assertTrue(index.contains("the big black rat"));
+    assertTrue(index.contains("big black rat"));
+    assertTrue(index.contains("black rat"));
+    assertTrue(index.contains("rat"));
+    assertEquals(15, index.size());
+  }
+
 
 //  @Test
 //  public void testCreateAlignmentTableIndexWithVariation() {
