34f9827501ac9077cae42f341573300092ebda97
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-08-11 09:22:20 +0200

    [RHD] Moved unit from AlignmentTableIndexTest to VariantGraphIndexTest; Removed empty AlignmentTableIndexTest.

--- collatex/src/test/java/eu/interedition/collatex2/experimental/graph/indexing/VariantGraphIndexTest.java
+++ collatex/src/test/java/eu/interedition/collatex2/experimental/graph/indexing/VariantGraphIndexTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import eu.interedition.collatex2.experimental.graph.IVariantGraph;
 import eu.interedition.collatex2.implementation.CollateXEngine;
 import eu.interedition.collatex2.interfaces.IWitness;
+import eu.interedition.collatex2.interfaces.IWitnessIndex;
 
 public class VariantGraphIndexTest {
   private static CollateXEngine factory;
@@ -19,12 +20,14 @@ public class VariantGraphIndexTest {
   }
 
   @Test
-  public void testCreateAlignmentTableIndex() {
+  public void testCreateVariantGraphIndex() {
     final IWitness a = factory.createWitness("A", "the first witness");
     final IVariantGraph graph = factory.graph(a);
     final IVariantGraphIndex index = VariantGraphIndex.create(graph, graph.findRepeatingTokens());
     assertEquals("VariantGraphIndex: (the, first, witness)", index.toString());
   }
+  
+
   
   @Test
   public void test1() {
@@ -51,12 +54,34 @@ public class VariantGraphIndexTest {
 
 
   @Test
-  public void testCreateAlignmentTableIndexWithVariation() {
+  public void testCreateVariantGraphIndexWithVariation() {
     final IWitness a = factory.createWitness("A", "the first witness");
     final IWitness b = factory.createWitness("B", "the second witness");
     final IVariantGraph graph = factory.graph(a, b);
     final IVariantGraphIndex index = VariantGraphIndex.create(graph, graph.findRepeatingTokens());
     assertEquals("VariantGraphIndex: (the, first, witness, second)", index.toString());
   }
+  
+  @Test
+  public void testVariantGraphIndex() {
+    final IWitness a = factory.createWitness("A", "first");
+    final IWitness b = factory.createWitness("B", "second");
+    final IWitness c = factory.createWitness("C", "third");
+    final IVariantGraph graph = factory.graph(a, b, c);
+    final IVariantGraphIndex index = VariantGraphIndex.create(graph, graph.findRepeatingTokens());
+    assertEquals("VariantGraphIndex: (first, second, third)", index.toString());
+  }
+
+
+  @Test
+  public void testVariantGraphIndex2() {
+    final IWitness a = factory.createWitness("A", "first");
+    final IWitness b = factory.createWitness("B", "match");
+    final IWitness c = factory.createWitness("C", "match");
+    final IVariantGraph graph = factory.graph(a, b, c);
+    final IWitnessIndex index = VariantGraphIndex.create(graph, graph.findRepeatingTokens());
+    assertEquals("VariantGraphIndex: (first, match)", index.toString());
+  }
+
 
 }
