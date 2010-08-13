cd624366aed6877965f680d65a639d31e515929f
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-08-13 14:31:51 +0200

    [RHD] Moved over last unit tests from IndexMatcherTest to VariantGraphIndexMatcherTest; removed empty test suite

--- collatex/src/test/java/eu/interedition/collatex2/experimental/tokenmatching/VariantGraphIndexMatcherTest.java
+++ collatex/src/test/java/eu/interedition/collatex2/experimental/tokenmatching/VariantGraphIndexMatcherTest.java
@@ -69,12 +69,84 @@ public class VariantGraphIndexMatcherTest {
     final IVariantGraph graph = factory.graph(witnessA);
     VariantGraphIndexMatcher matcher = new VariantGraphIndexMatcher(graph);
     List<ITokenMatch> matches = matcher.getMatches(witnessB);
-    System.out.println(matches);
     assertEquals(3, matches.size());
     assertEquals("the: 1 -> 1", matches.get(0).toString());
     assertEquals("big: 2 -> 2", matches.get(1).toString());
     assertEquals("black: 3 -> 3", matches.get(2).toString());
   }
+
+  //Note: internally this gives # the big black and the big black cat as matches
+  @Test
+  public void testGetMatchesUsingWitnessIndexWithOverlapping() {
+    final IWitness witnessA = factory.createWitness("A", "the big black cat and the big black rat");
+    final IWitness witnessB = factory.createWitness("B", "the big black cat");
+    final IVariantGraph graph = factory.graph(witnessA);
+    VariantGraphIndexMatcher matcher = new VariantGraphIndexMatcher(graph);
+    List<ITokenMatch> matches = matcher.getMatches(witnessB);
+    assertEquals(4, matches.size());
+    assertEquals("the: 1 -> 1", matches.get(0).toString());
+    assertEquals("big: 2 -> 2", matches.get(1).toString());
+    assertEquals("black: 3 -> 3", matches.get(2).toString());
+    assertEquals("cat: 4 -> 4", matches.get(3).toString());
+  }
+
+  @Test
+  public void testOverlappingMatches2() {
+    final IWitness witnessA = factory.createWitness("A", "the black cat and the black mat");
+    final IWitness witnessB = factory.createWitness("B", "the black dog and the black mat");
+    final IVariantGraph graph = factory.graph(witnessA);
+    VariantGraphIndexMatcher matcher = new VariantGraphIndexMatcher(graph);
+    List<ITokenMatch> matches = matcher.getMatches(witnessB);
+    assertEquals(6, matches.size());
+    assertEquals("the: 1 -> 1", matches.get(0).toString());
+    assertEquals("black: 2 -> 2", matches.get(1).toString());
+    assertEquals("and: 4 -> 4", matches.get(2).toString());
+    assertEquals("the: 5 -> 5", matches.get(3).toString());
+    assertEquals("black: 6 -> 6", matches.get(4).toString());
+    assertEquals("mat: 7 -> 7", matches.get(5).toString());
+  }
+
+  @Test
+  public void testMatchesWithIndex() {
+    final IWitness witnessA = factory.createWitness("A", "The black cat");
+    final IWitness witnessB = factory.createWitness("B", "The black and white cat");
+    final IVariantGraph graph = factory.graph(witnessA);
+    VariantGraphIndexMatcher matcher = new VariantGraphIndexMatcher(graph);
+    List<ITokenMatch> matches = matcher.getMatches(witnessB);
+    assertEquals(3, matches.size());
+    assertEquals("the: 1 -> 1", matches.get(0).toString());
+    assertEquals("black: 2 -> 2", matches.get(1).toString());
+    assertEquals("cat: 5 -> 3", matches.get(2).toString());
+  }
+  
+  @Test
+  public void testTwoEqualPossibilities1() {
+    // test a -> a a
+    final IWitness witnessA = factory.createWitness("A", "a");
+    final IWitness witnessB = factory.createWitness("B", "a a");
+    final IVariantGraph graph = factory.graph(witnessA);
+    VariantGraphIndexMatcher matcher = new VariantGraphIndexMatcher(graph);
+    List<ITokenMatch> matches = matcher.getMatches(witnessB);
+    assertEquals(1, matches.size());
+    ITokenMatch match = matches.get(0);
+    assertEquals(1, match.getTableToken().getPosition());
+    assertEquals(1, match.getWitnessToken().getPosition());
+  }
+
+  @Test
+  public void testTwoEqualPossibilities2() {
+    // test a a -> a
+    final IWitness witnessA = factory.createWitness("A", "a a");
+    final IWitness witnessB = factory.createWitness("B", "a");
+    final IVariantGraph graph = factory.graph(witnessA);
+    VariantGraphIndexMatcher matcher = new VariantGraphIndexMatcher(graph);
+    List<ITokenMatch> matches = matcher.getMatches(witnessB);
+    assertEquals(1, matches.size());
+    ITokenMatch match = matches.get(0);
+    assertEquals(1, match.getTableToken().getPosition());
+    assertEquals(1, match.getWitnessToken().getPosition());
+  }
+
 
 
 
