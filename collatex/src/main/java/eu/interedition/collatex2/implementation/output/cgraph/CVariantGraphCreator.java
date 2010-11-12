1b7fc97120d3562a4d6183d6c0a3ddc5b0474e2e
Bram Buitendijk <bramb.g@gmail.com>
2010-11-12 13:26:30 +0100

    [BB] greatly simplified the acyclic variantgraph -> cyclic variantgraph converter, failing test now passes

--- collatex/src/main/java/eu/interedition/collatex2/implementation/output/cgraph/CVariantGraphCreator.java
+++ collatex/src/main/java/eu/interedition/collatex2/implementation/output/cgraph/CVariantGraphCreator.java
@@ -2,10 +2,6 @@ package eu.interedition.collatex2.implementation.output.cgraph;
 
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Maps;
 
@@ -18,7 +14,7 @@ import eu.interedition.collatex2.interfaces.IVariantGraphVertex;
 import eu.interedition.collatex2.interfaces.IWitness;
 
 public class CVariantGraphCreator {
-  private static final Logger LOG = LoggerFactory.getLogger(CVariantGraphCreator.class);
+  //  private static final Logger LOG = LoggerFactory.getLogger(CVariantGraphCreator.class);
   private static IVariantGraph cyclicGraph;
   private static Map<INormalizedToken, IVariantGraphVertex> keyToken2Vertex;
   private static Map<IVariantGraphVertex, IVariantGraphVertex> a2cVertexMap;
@@ -27,63 +23,43 @@ public class CVariantGraphCreator {
     keyToken2Vertex = Maps.newHashMap();
     a2cVertexMap = Maps.newHashMap();
     cyclicGraph = CyclicVariantGraph.create();
-    IVariantGraphVertex aStartVertex = acyclicGraph.getStartVertex();
-    IVariantGraphVertex cStartVertex = cyclicGraph.getStartVertex();
-    a2cVertexMap.put(aStartVertex, cStartVertex);
+
+    a2cVertexMap.put(acyclicGraph.getStartVertex(), cyclicGraph.getStartVertex());
     a2cVertexMap.put(acyclicGraph.getEndVertex(), cyclicGraph.getEndVertex());
-    processVertex(aStartVertex, acyclicGraph, cStartVertex);
+
+    for (IVariantGraphVertex avgVertex : acyclicGraph.vertexSet()) {
+      if (!a2cVertexMap.containsKey(avgVertex)) {
+        INormalizedToken vertexKey = avgVertex.getVertexKey();
+        IVariantGraphVertex cvgVertex;
+        if (keyToken2Vertex.containsKey(vertexKey)) {
+          cvgVertex = keyToken2Vertex.get(vertexKey);
+        } else {
+          cvgVertex = new VariantGraphVertex(avgVertex.getNormalized(), avgVertex.getVertexKey());
+          keyToken2Vertex.put(vertexKey, cvgVertex);
+          cyclicGraph.addVertex(cvgVertex);
+        }
+        a2cVertexMap.put(avgVertex, cvgVertex);
+      }
+    }
+
+    for (IVariantGraphEdge avgEdge : acyclicGraph.edgeSet()) {
+      IVariantGraphVertex cvgStart = a2cVertexMap.get(avgEdge.getBeginVertex());
+      IVariantGraphVertex cvgEnd = a2cVertexMap.get(avgEdge.getEndVertex());
+
+      Iterator<IWitness> witnessIterator = avgEdge.getWitnesses().iterator();
+      IVariantGraphEdge cvgEdge;
+      if (cyclicGraph.containsEdge(cvgStart, cvgEnd)) {
+        cvgEdge = cyclicGraph.getEdge(cvgStart, cvgEnd);
+      } else {
+        cvgEdge = new VariantGraphEdge(cvgStart, cvgEnd, witnessIterator.next());
+        cyclicGraph.addEdge(a2cVertexMap.get(avgEdge.getBeginVertex()), a2cVertexMap.get(avgEdge.getEndVertex()), cvgEdge);
+      }
+      while (witnessIterator.hasNext()) {
+        cvgEdge.addWitness(witnessIterator.next());
+      }
+    }
+
     return cyclicGraph;
   }
 
-  private static void processVertex(IVariantGraphVertex avgVertex, final IVariantGraph acyclicGraph, IVariantGraphVertex cvgVertex) {
-    LOG.info("processVertex(avgVertex=({}),cvgVertex=({}))", new Object[] { avgVertex, cvgVertex });
-    Set<IVariantGraphEdge> outgoingEdges = acyclicGraph.outgoingEdgesOf(avgVertex);
-    for (IVariantGraphEdge avgEdge : outgoingEdges) {
-      processEdge(avgEdge, acyclicGraph, cvgVertex);
-    }
-  }
-
-  private static void processEdge(IVariantGraphEdge avgEdge, final IVariantGraph acyclicGraph, IVariantGraphVertex cvgLastVertex) {
-    LOG.info("processEdge(avgEdge=({}),cvgLastVertex=({}))", new Object[] { avgEdge, cvgLastVertex });
-    IVariantGraphVertex avgTargetVertex = acyclicGraph.getEdgeTarget(avgEdge);
-    boolean avgTargetVertexIsNew = !a2cVertexMap.containsKey(avgTargetVertex);
-    INormalizedToken vertexKey = avgTargetVertex.getVertexKey();
-    //    LOG.info("vertexKey={}", vertexKey);
-    IVariantGraphVertex cvgTargetVertex;
-    if (vertexKey == null) {
-      cvgTargetVertex = a2cVertexMap.get(avgTargetVertex);
-    } else {
-      if (keyToken2Vertex.containsKey(vertexKey)) {
-        cvgTargetVertex = keyToken2Vertex.get(vertexKey);
-      } else {
-        cvgTargetVertex = new VariantGraphVertex(avgTargetVertex.getNormalized(), avgTargetVertex.getVertexKey());
-        keyToken2Vertex.put(vertexKey, cvgTargetVertex);
-        cyclicGraph.addVertex(cvgTargetVertex);
-        a2cVertexMap.put(avgTargetVertex, cvgTargetVertex);
-      }
-    }
-    VariantGraphEdge cvgEdge = convertEdge(avgEdge);
-    //    LOG.info("cvgLastVertex={}, cvgTargetVertex={}", cvgLastVertex, cvgTargetVertex);
-    cyclicGraph.addEdge(cvgLastVertex, cvgTargetVertex, cvgEdge);
-    if (avgTargetVertexIsNew) {
-      processVertex(avgTargetVertex, acyclicGraph, cvgTargetVertex);
-    }
-  }
-
-  private static VariantGraphEdge convertEdge(IVariantGraphEdge avgEdge) {
-    IVariantGraphVertex cvgStart = a2cVertexMap.get(avgEdge.getBeginVertex());
-    IVariantGraphVertex cvgEnd = a2cVertexMap.get(avgEdge.getEndVertex());
-    Iterator<IWitness> witnessIterator = avgEdge.getWitnesses().iterator();
-    if (cvgStart == null) {
-      LOG.error("start=null for {}", avgEdge);
-    }
-    if (cvgEnd == null) {
-      LOG.error("end=null for {}", avgEdge);
-    }
-    VariantGraphEdge cvgEdge = new VariantGraphEdge(cvgStart, cvgEnd, witnessIterator.next());
-    while (witnessIterator.hasNext()) {
-      cvgEdge.addWitness(witnessIterator.next());
-    }
-    return cvgEdge;
-  }
 }
