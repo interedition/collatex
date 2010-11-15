122b436d57b8419e0819c7aa9a5d9d21edb2678a
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2010-11-15 18:07:38 +0100

    [RHD] Enabled Spencer and Howe unit test

--- collatex/src/test/java/eu/interedition/collatex2/output/alignmenttable/SpencerHoweTest.java
+++ collatex/src/test/java/eu/interedition/collatex2/output/alignmenttable/SpencerHoweTest.java
@@ -22,16 +22,10 @@ package eu.interedition.collatex2.output.alignmenttable;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import eu.interedition.collatex2.implementation.CollateXEngine;
-import eu.interedition.collatex2.implementation.containers.graph.VariantGraph2Creator;
-import eu.interedition.collatex2.implementation.output.table.VariantGraphBasedAlignmentTable;
 import eu.interedition.collatex2.interfaces.IAlignmentTable;
-import eu.interedition.collatex2.interfaces.IVariantGraph;
 import eu.interedition.collatex2.interfaces.IWitness;
 
 /**
@@ -46,19 +40,16 @@ import eu.interedition.collatex2.interfaces.IWitness;
  * 
  */
 public class SpencerHoweTest {
-  private static Logger logger = LoggerFactory.getLogger(SpencerHoweTest.class);
-
   private CollateXEngine engine = new CollateXEngine();
 
-  //TODO: need variant graph based alignment for this test to work!
+  //NOTE: Currently the AT visualization aligns variation to the
+  //left of the table: see the 'C' element
   @Test
-  @Ignore
   public void testATSpencerHowe() {
     final IWitness w1 = engine.createWitness("V", "a b c d e f ");
     final IWitness w2 = engine.createWitness("W", "x y z d e");
     final IWitness w3 = engine.createWitness("X", "a b x y z");
-    IVariantGraph graph = VariantGraph2Creator.create(w1, w2, w3);
-    IAlignmentTable table = new VariantGraphBasedAlignmentTable(graph);
+    IAlignmentTable table = engine.align(w1, w2, w3);
     assertEquals("V: |a|b|c| | |d|e|f|", table.getRow(w1).rowToString());
     assertEquals("W: | | |x|y|z|d|e| |", table.getRow(w2).rowToString());
     assertEquals("X: |a|b|x|y|z| | | |", table.getRow(w3).rowToString());
