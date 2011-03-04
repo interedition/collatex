66af950b772b897a84882a0e3edc9a3ff9cd5401
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2011-03-04 11:07:02 +0100

    [RHD] Removed examples cases from modern literature

--- collatex/src/test/java/eu/interedition/collatex2/usecases/dirk/HighFrequencyWordsTest.java
+++ collatex/src/test/java/eu/interedition/collatex2/usecases/dirk/HighFrequencyWordsTest.java
@@ -1,25 +1,22 @@
-package eu.interedition.collatex2.usecases.beckett;
+package eu.interedition.collatex2.usecases.dirk;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Iterator;
 import java.util.List;
 
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import eu.interedition.collatex2.implementation.CollateXEngine;
 import eu.interedition.collatex2.implementation.vg_alignment.IAlignment2;
-import eu.interedition.collatex2.implementation.vg_analysis.IAnalysis;
-import eu.interedition.collatex2.implementation.vg_analysis.ISequence;
 import eu.interedition.collatex2.interfaces.INormalizedToken;
 import eu.interedition.collatex2.interfaces.ITokenMatch;
 import eu.interedition.collatex2.interfaces.IVariantGraph;
 import eu.interedition.collatex2.interfaces.IWitness;
-public class BeckettTest {
+public class HighFrequencyWordsTest {
   private static CollateXEngine factory;
   
   @BeforeClass
@@ -68,21 +65,4 @@ public class BeckettTest {
     assertEquals(3, tokenMatches.size());
   }
   
-  //TODO: This test only works with the
-  //TODO: AlternativeTokenIndexMatcher and the AlternativeWitnessIndex
-  //TODO: and the yet to be made AlternativeVariantGraphIndex!
-  @Ignore
-  @Test
-  public void testAnalysisBeckettLotsOfRepetition() {
-    final IWitness a = factory.createWitness("A", "from the days & nights when day followed on night & night on day.");
-    final IWitness b = factory.createWitness("B", "from the days and nights when day followed fast on night and night on day.");
-    IVariantGraph graph = factory.graph(a);
-    IAnalysis analysis = factory.analyse(graph, b);
-    List<ISequence> sequences = analysis.getSequences();
-    assertEquals("from the days", sequences.get(0).getNormalized());
-    assertEquals("nights when day followed", sequences.get(1).getNormalized());
-    assertEquals("on night", sequences.get(2).getNormalized());
-    assertEquals("night on day", sequences.get(3).getNormalized());
-    assertEquals(4, sequences.size());
-  }
 }
