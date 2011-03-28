7c896935148e96b5fe723b3f8f31384f0ad1c2ff
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2011-03-28 14:42:42 +0200

    [RHD] Refactored List return type of matches into ListMultimap

--- collatex/src/main/java/eu/interedition/collatex2/experimental/DeTestDirkVincent.java
+++ collatex/src/main/java/eu/interedition/collatex2/experimental/DeTestDirkVincent.java
@@ -6,8 +6,10 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.google.common.collect.ListMultimap;
+
 import eu.interedition.collatex2.implementation.CollateXEngine;
-import eu.interedition.collatex2.interfaces.ITokenMatch;
+import eu.interedition.collatex2.interfaces.INormalizedToken;
 import eu.interedition.collatex2.interfaces.IWitness;
 
 public class DeTestDirkVincent {
@@ -18,9 +20,13 @@ public class DeTestDirkVincent {
     IWitness a = factory.createWitness("01b", "Its soft light neither daylight nor moonlight nor starlight nor any light he could remember from the days & nights when day followed night & vice versa.");
     IWitness b = factory.createWitness("10a", "Its soft changeless light unlike any light he could remember from the days and nights when day followed hard on night and vice versa.");
     MyNewMatcher matcher = new MyNewMatcher();
-    List<ITokenMatch> matches = matcher.match(a, b);
-    ITokenMatch m = matches.get(0);
-    assertEquals("Its", m.getBaseToken().getContent());
-    assertEquals("Its", m.getWitnessToken().getContent());
+    ListMultimap<INormalizedToken, INormalizedToken> matches = matcher.match(a, b);
+    INormalizedToken its = a.getTokens().get(0);
+    INormalizedToken light = a.getTokens().get(2);
+    List<INormalizedToken> matchedTokens;
+    matchedTokens = matches.get(its);
+    assertEquals("Its", matchedTokens.get(0).getContent());
+    matchedTokens = matches.get(light);
+    assertEquals(2, matchedTokens.size());
   }
 }
--- collatex/src/main/java/eu/interedition/collatex2/experimental/MyNewMatcher.java
+++ collatex/src/main/java/eu/interedition/collatex2/experimental/MyNewMatcher.java
@@ -1,23 +1,19 @@
 package eu.interedition.collatex2.experimental;
 
-import java.util.List;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
 
-import com.google.common.collect.Lists;
-
-import eu.interedition.collatex2.implementation.vg_alignment.TokenMatch;
 import eu.interedition.collatex2.interfaces.INormalizedToken;
-import eu.interedition.collatex2.interfaces.ITokenMatch;
 import eu.interedition.collatex2.interfaces.IWitness;
 
 public class MyNewMatcher {
 
-  public List<ITokenMatch> match(IWitness a, IWitness b) {
-    List<ITokenMatch> matches = Lists.newArrayList();
+  public ListMultimap<INormalizedToken, INormalizedToken> match(IWitness a, IWitness b) {
+    ListMultimap<INormalizedToken, INormalizedToken> matches = ArrayListMultimap.create();
     for (INormalizedToken tokenA : a.getTokens()) {
       for (INormalizedToken tokenB : b.getTokens()) {
         if (tokenA.getNormalized().equals(tokenB.getNormalized())) {
-          TokenMatch match = new TokenMatch(tokenA, tokenB);
-          matches.add(match);
+          matches.put(tokenA, tokenB);
         }
       }
     }
