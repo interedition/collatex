e94f5da6f0f6fa76df678bb2e6ba757688c15093
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2011-03-31 09:48:39 +0200

    [RHD] Added class WitnessAfgeleide, which contains only the matching tokens, not  the tokens that are omitted, added or replaced.

--- collatex/src/main/java/eu/interedition/collatex2/experimental/DeTestDirkVincent.java
+++ collatex/src/main/java/eu/interedition/collatex2/experimental/DeTestDirkVincent.java
@@ -2,6 +2,7 @@ package eu.interedition.collatex2.experimental;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
@@ -54,7 +55,28 @@ public class DeTestDirkVincent {
   }
 
   @Test
-  public void testDirkVincent3() {
+  public void testVincentDirk3() {
+    CollateXEngine factory = new CollateXEngine();
+    IWitness a = factory.createWitness("01b", "Its soft light neither daylight nor moonlight nor starlight nor any light he could remember from the days & nights when day followed night & vice versa.");
+    IWitness b = factory.createWitness("10a", "Its soft changeless light unlike any light he could remember from the days and nights when day followed hard on night and vice versa.");
+    MyNewMatcher matcher = new MyNewMatcher();
+    ListMultimap<INormalizedToken, INormalizedToken> matches = matcher.match(a, b);
+    WitnessAfgeleide afgeleider = new WitnessAfgeleide();
+    IWitness afgeleideWitness = afgeleider.calculateAfgeleide(b, matches);
+    Iterator<INormalizedToken> tokenIterator = afgeleideWitness.tokenIterator();
+    assertEquals("Its", tokenIterator.next().getContent());
+    assertEquals("soft", tokenIterator.next().getContent());
+    assertEquals("light", tokenIterator.next().getContent());
+    assertEquals("any", tokenIterator.next().getContent());
+    assertEquals("light", tokenIterator.next().getContent());
+    assertEquals("he", tokenIterator.next().getContent());
+    assertEquals("could", tokenIterator.next().getContent());
+  }
+  
+  
+  //TODO: make return type map
+  @Test
+  public void testDirkVincent4() {
     CollateXEngine factory = new CollateXEngine();
     IWitness a = factory.createWitness("01b", "Its soft light neither daylight nor moonlight nor starlight nor any light he could remember from the days & nights when day followed night & vice versa.");
     IWitness b = factory.createWitness("10a", "Its soft changeless light unlike any light he could remember from the days and nights when day followed hard on night and vice versa.");
