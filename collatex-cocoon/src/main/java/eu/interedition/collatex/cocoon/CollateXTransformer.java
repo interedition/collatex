a08037de1ca2f0f96d233c908fbcdaa041677d30
Gregor Middell <gregor@middell.net>
2011-06-07 16:48:46 +0100

    generate TEI-P5 critapp via Cocoon transformer

--- collatex-cocoon/src/main/java/eu/interedition/collatex/cocoon/CollateXTransformer.java
+++ collatex-cocoon/src/main/java/eu/interedition/collatex/cocoon/CollateXTransformer.java
@@ -1,39 +1,54 @@
 package eu.interedition.collatex.cocoon;
 
-import com.google.common.collect.Lists;
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.*;
+import com.sun.jdi.event.EventIterator;
 import eu.interedition.collatex2.implementation.CollateXEngine;
-import eu.interedition.collatex2.interfaces.IAlignmentTable;
-import eu.interedition.collatex2.interfaces.ICell;
-import eu.interedition.collatex2.interfaces.IRow;
-import eu.interedition.collatex2.interfaces.IWitness;
+import eu.interedition.collatex2.interfaces.*;
 import org.apache.cocoon.ProcessingException;
 import org.apache.cocoon.transformation.AbstractSAXTransformer;
 import org.apache.cocoon.xml.AttributesImpl;
+import org.w3c.dom.Element;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author <a href="http://gregor.middell.net/" title="Homepage">Gregor Middell</a>
  */
 public class CollateXTransformer extends AbstractSAXTransformer {
 
+    private static final String TEI_NS = "http://www.tei-c.org/ns/1.0";
+    public static final String COLLATEX_NS = "http://interedition.eu/collatex/ns/1.0";
+
+    private enum OutputType {
+        ALIGNMENT_TABLE, TEI_APPARATUS
+    }
+
     private CollateXEngine engine = new CollateXEngine();
+    private OutputType outputType = OutputType.ALIGNMENT_TABLE;
     private List<IWitness> witnesses = Lists.newArrayList();
     private String sigil;
 
     public CollateXTransformer() {
         super();
-        this.defaultNamespaceURI = "http://interedition.eu/collatex/ns/1.0";
+        this.defaultNamespaceURI = COLLATEX_NS;
     }
 
     public void startTransformingElement(String uri, String name, String raw, Attributes attr) throws ProcessingException,
             IOException, SAXException {
         if ("collation".equals(name)) {
-            witnesses.clear();
+            final String outputType = attr.getValue(defaultNamespaceURI, "outputType");
+            if (outputType != null && "tei".equalsIgnoreCase(outputType.trim())) {
+                this.outputType = OutputType.TEI_APPARATUS;
+            } else {
+                this.outputType = OutputType.ALIGNMENT_TABLE;
+            }
             sigil = null;
+            witnesses.clear();
         } else if ("witness".equals(name)) {
             sigil = attr.getValue("sigil");
             if (sigil == null) {
@@ -47,30 +62,102 @@ public class CollateXTransformer extends AbstractSAXTransformer {
     public void endTransformingElement(String uri, String name, String raw) throws ProcessingException, IOException, SAXException {
         if ("collation".equals(name)) {
             ignoreHooksCount++;
-            sendStartPrefixMapping();
-            sendStartElementEventNS("alignment", EMPTY_ATTRIBUTES);
-            if (!witnesses.isEmpty()) {
-                final IAlignmentTable alignmentTable = engine.align(witnesses.toArray(new IWitness[witnesses.size()]));
-                for (IRow row : alignmentTable.getRows()) {
-                    final AttributesImpl rowAttrs = new AttributesImpl();
-                    rowAttrs.addAttribute(namespaceURI, "sigil", "sigil", "CDATA", row.getSigil());
-                    sendStartElementEventNS("row", rowAttrs);
-                    for (ICell cell: row) {
-                        sendStartElementEventNS("cell", EMPTY_ATTRIBUTES);
-                        if (!cell.isEmpty()) {
-                            sendTextEvent(cell.getToken().getContent());
-                        }
-                        sendEndElementEventNS("cell");
-
-                    }
-                    sendEndElementEventNS("row");
-                }
+            switch (outputType) {
+                case TEI_APPARATUS:
+                    sendTeiApparatus();
+                    break;
+                default:
+                    sendAlignmentTable();
+                    break;
             }
-            sendEndElementEventNS("alignment");
-            sendEndPrefixMapping();
             ignoreHooksCount--;
         } else if ("witness".equals(name)) {
             witnesses.add(engine.createWitness(sigil, endTextRecording()));
         }
     }
+
+    private void sendAlignmentTable() throws SAXException {
+        sendStartElementEventNS("alignment", EMPTY_ATTRIBUTES);
+        if (!witnesses.isEmpty()) {
+            final IAlignmentTable alignmentTable = engine.align(witnesses.toArray(new IWitness[witnesses.size()]));
+            for (IRow row : alignmentTable.getRows()) {
+                final AttributesImpl rowAttrs = new AttributesImpl();
+                rowAttrs.addAttribute(namespaceURI, "sigil", "sigil", "CDATA", row.getSigil());
+                sendStartElementEventNS("row", rowAttrs);
+                for (ICell cell : row) {
+                    sendStartElementEventNS("cell", EMPTY_ATTRIBUTES);
+                    if (!cell.isEmpty()) {
+                        sendTextEvent(cell.getToken().getContent());
+                    }
+                    sendEndElementEventNS("cell");
+
+                }
+                sendEndElementEventNS("row");
+            }
+        }
+        sendEndElementEventNS("alignment");
+    }
+
+    private void sendTeiApparatus() throws SAXException {
+        final IApparatus apparatus = engine.createApparatus(engine.graph(witnesses.toArray(new IWitness[witnesses.size()])));
+
+        sendStartElementEventNS("apparatus", EMPTY_ATTRIBUTES);
+        startPrefixMapping("tei", TEI_NS);
+        // FIXME: this should be dealt with on the tokenizer level!
+        final String separator = " ";
+        for (Iterator<IApparatusEntry> entryIt = apparatus.getEntries().iterator(); entryIt.hasNext(); ) {
+            final IApparatusEntry entry = entryIt.next();
+            // group together similar phrases
+            final Multimap<String, String> content2WitMap = ArrayListMultimap.create();
+            for (IWitness witness : entry.getWitnesses()) {
+                content2WitMap.put(entry.getPhrase(witness).getContent(), witness.getSigil());
+            }
+
+            if ((content2WitMap.keySet().size() == 1) && !entry.hasEmptyCells()) {
+                // common content, there is no apparatus tag needed, just output the
+                // segment
+                sendTextEvent(content2WitMap.keys().iterator().next());
+            } else {
+                // convert the multimap to a normal map indexed by segment content and
+                // containing a sorted set of witness identifiers
+                Map<String, SortedSet<String>> readings = Maps.newHashMap();
+                for (final String content : content2WitMap.keySet()) {
+                    readings.put(content, Sets.newTreeSet(content2WitMap.get(content)));
+                }
+
+                SortedMap<String, String> readingMap = Maps.newTreeMap();
+                for (Map.Entry<String, SortedSet<String>> reading : readings.entrySet()) {
+                    readingMap.put(Joiner.on(" ").join(Iterables.transform(reading.getValue(), WIT_TO_XML_ID)), reading.getKey());
+                }
+
+                startElement(TEI_NS, "app", "tei:app", EMPTY_ATTRIBUTES);
+                for (Map.Entry<String, String> reading : readingMap.entrySet()) {
+                    final AttributesImpl attributes = new AttributesImpl();
+                    attributes.addCDATAAttribute("wit", reading.getKey());
+                    startElement(TEI_NS, "rdg", "tei:rdg", attributes);
+                    String content = reading.getValue();
+                    if (!content.isEmpty()) {
+                        sendTextEvent(content);
+                    }
+                    endElement(TEI_NS, "rdg", "tei:rdg");
+                }
+                endElement(TEI_NS, "app", "tei:app");
+            }
+            if (entryIt.hasNext()) {
+                sendTextEvent(separator);
+            }
+        }
+
+        endPrefixMapping("tei");
+        sendEndElementEventNS("apparatus");
+    }
+
+    private static final Function<String, String> WIT_TO_XML_ID = new Function<String, String>() {
+
+      @Override
+      public String apply(String from) {
+        return (from.startsWith("#") ? from : ("#" + from));
+      }
+    };
+
 }
