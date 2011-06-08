2be5ed8ad33c47e40b672afda7aff8eb8e8058a4
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2011-06-08 17:23:50 +0100

    [RHD] Moved unit tests from TokenIndexMatcher to Linker class. Only one unit test left to fix.

--- collatex/src/test/java/eu/interedition/collatex2/experimental/LinkerTest.java
+++ collatex/src/test/java/eu/interedition/collatex2/experimental/LinkerTest.java
@@ -7,13 +7,16 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
 
 import eu.interedition.collatex2.implementation.CollateXEngine;
 import eu.interedition.collatex2.implementation.containers.graph.VariantGraph2;
+import eu.interedition.collatex2.implementation.vg_alignment.TokenMatch;
 import eu.interedition.collatex2.interfaces.INormalizedToken;
+import eu.interedition.collatex2.interfaces.ITokenMatch;
 import eu.interedition.collatex2.interfaces.IVariantGraph;
 import eu.interedition.collatex2.interfaces.IWitness;
 
@@ -23,6 +26,20 @@ public class LinkerTest {
   @BeforeClass
   public static void setup() {
     engine = new MyNewCollateXEngine();
+  }
+
+  //convenience method
+  private List<ITokenMatch> createTokenMatches2(final IVariantGraph graph, final IWitness witnessB) {
+    IWitness superbase = new SuperbaseCreator().create(graph);
+    MyNewLinker linker = new MyNewLinker();
+    Map<INormalizedToken, INormalizedToken> tokens = linker.link2(superbase, witnessB);
+    List<ITokenMatch> matches = Lists.newArrayList();
+    for (INormalizedToken witnessToken : tokens.keySet()) {
+      INormalizedToken baseToken = tokens.get(witnessToken);
+      ITokenMatch match = new TokenMatch(baseToken, witnessToken);
+      matches.add(match);
+    }
+    return matches;
   }
 
   @Test
@@ -137,5 +154,110 @@ public class LinkerTest {
     assertEquals(expectedBaseToken, tokens.get(witnessToken));
   }
 
+  @Test
+  public void testEverythingIsUnique() {
+    final IWitness witnessA = engine.createWitness("A", "everything is unique should be no problem");
+    final IWitness witnessB = engine.createWitness("B", "everything is unique");
+    final IVariantGraph graph = engine.graph(witnessA);
+    List<ITokenMatch> matches = createTokenMatches2(graph, witnessB);
+    assertEquals(3, matches.size());
+    assertEquals("everything: 1 -> [everything]", matches.get(0).toString());
+    assertEquals("is: 2 -> [is]", matches.get(1).toString());
+    assertEquals("unique: 3 -> [unique]", matches.get(2).toString());
+  }
 
+
+  @Test
+  public void testEverythingIsUniqueTwoWitnesses() {
+    final IWitness witnessA = engine.createWitness("A", "everything is unique should be no problem");
+    final IWitness witnessB = engine.createWitness("B", "this one very different");
+    final IWitness witnessC = engine.createWitness("C", "everything is different");
+    IVariantGraph graph = engine.graph(witnessA, witnessB);
+    List<ITokenMatch> matches = createTokenMatches2(graph, witnessC);
+    assertEquals(3, matches.size());
+    assertEquals("everything", matches.get(0).getNormalized());
+    assertEquals("is", matches.get(1).getNormalized());
+    assertEquals("different", matches.get(2).getNormalized());
+  }
+
+  @Test
+  public void testOverlappingMatches() {
+    final IWitness witnessA = engine.createWitness("A", "everything is unique should be no problem");
+    final IWitness witnessB = engine.createWitness("B", "this one is different");
+    final IWitness witnessC = engine.createWitness("C", "everything is different");
+    IVariantGraph graph = engine.graph(witnessA, witnessB);
+    List<ITokenMatch> matches = createTokenMatches2(graph, witnessC);
+    assertEquals(3, matches.size());
+    assertEquals("everything", matches.get(0).getNormalized());
+    assertEquals("is", matches.get(1).getNormalized());
+    assertEquals("different", matches.get(2).getNormalized());
+  }
+  
+  @Test
+  public void testGetMatchesUsingWitnessIndex() {
+    final IWitness witnessA = engine.createWitness("A", "The big black cat and the big black rat");
+    final IWitness witnessB = engine.createWitness("B", "The big black");
+    final IVariantGraph graph = engine.graph(witnessA);
+    List<ITokenMatch> matches = createTokenMatches2(graph, witnessB);
+    assertEquals(3, matches.size());
+    assertEquals("the: 1 -> [the]", matches.get(0).toString());
+    assertEquals("big: 2 -> [big]", matches.get(1).toString());
+    assertEquals("black: 3 -> [black]", matches.get(2).toString());
+  }
+
+  //Note: internally this gives # the big black and the big black cat as matches
+  @Test
+  public void testGetMatchesUsingWitnessIndexWithOverlapping() {
+    final IWitness witnessA = engine.createWitness("A", "the big black cat and the big black rat");
+    final IWitness witnessB = engine.createWitness("B", "the big black cat");
+    final IVariantGraph graph = engine.graph(witnessA);
+    List<ITokenMatch> matches = createTokenMatches2(graph, witnessB);
+    assertEquals(4, matches.size());
+    assertEquals("cat: 4 -> [cat]", matches.get(0).toString());
+    assertEquals("the: 1 -> [the]", matches.get(1).toString());
+    assertEquals("big: 2 -> [big]", matches.get(2).toString());
+    assertEquals("black: 3 -> [black]", matches.get(3).toString());
+  }
+
+
+  @Test
+  public void testOverlappingMatches2() {
+    final IWitness witnessA = engine.createWitness("A", "the black cat and the black mat");
+    final IWitness witnessB = engine.createWitness("B", "the black dog and the black mat");
+    final IVariantGraph graph = engine.graph(witnessA);
+    List<ITokenMatch> matches = createTokenMatches2(graph, witnessB);
+    assertEquals(6, matches.size());
+    assertEquals("and: 4 -> [and]", matches.get(0).toString());
+    assertEquals("mat: 7 -> [mat]", matches.get(1).toString());
+    assertEquals("the: 1 -> [the]", matches.get(2).toString());
+    assertEquals("black: 2 -> [black]", matches.get(3).toString());
+    assertEquals("the: 5 -> [the]", matches.get(4).toString());
+    assertEquals("black: 6 -> [black]", matches.get(5).toString());
+  }
+
+  @Test
+  public void testMatchesWithIndex() {
+    final IWitness witnessA = engine.createWitness("A", "The black cat");
+    final IWitness witnessB = engine.createWitness("B", "The black and white cat");
+    final IVariantGraph graph = engine.graph(witnessA);
+    List<ITokenMatch> matches = createTokenMatches2(graph, witnessB);
+    assertEquals(3, matches.size());
+    assertEquals("the: 1 -> [the]", matches.get(0).toString());
+    assertEquals("black: 2 -> [black]", matches.get(1).toString());
+    assertEquals("cat: 5 -> [cat]", matches.get(2).toString());
+  }
+
+  @Ignore
+  @Test
+  public void testTwoEqualPossibilities2() {
+    // test a a -> a
+    final IWitness witnessA = engine.createWitness("A", "a a");
+    final IWitness witnessB = engine.createWitness("B", "a");
+    final IVariantGraph graph = engine.graph(witnessA);
+    List<ITokenMatch> matches = createTokenMatches2(graph, witnessB);
+    assertEquals(1, matches.size());
+    ITokenMatch match = matches.get(0);
+    assertEquals(graph.getTokens(witnessA).get(0), match.getBaseToken());
+    assertEquals(witnessB.getTokens().get(0), match.getWitnessToken());
+  }
 }
