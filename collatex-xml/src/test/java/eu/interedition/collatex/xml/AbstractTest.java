c5a20fc0c9ed663cb72b943bd223a0fb27f00ce6
Gregor Middell <gregor@middell.net>
2011-06-08 15:57:56 +0100

    setup test database for XML witness support

---
+++ collatex-xml/src/test/java/eu/interedition/collatex/xml/AbstractTest.java
@@ -0,0 +1,13 @@
+package eu.interedition.collatex.xml;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author <a href="http://gregor.middell.net/" title="Homepage">Gregor Middell</a>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/testContext.xml")
+public abstract class AbstractTest {
+}
