b8acdf898e0162d16207f84f34f3762715047694
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2011-06-19 11:05:31 +0200

    [RHD] Added start and end vertices to DecisionGraph.

--- collatex/src/main/java/eu/interedition/collatex2/implementation/decision_graph/DecisionGraph.java
+++ collatex/src/main/java/eu/interedition/collatex2/implementation/decision_graph/DecisionGraph.java
@@ -2,11 +2,22 @@ package eu.interedition.collatex2.implementation.decision_graph;
 
 import org.jgrapht.experimental.dag.DirectedAcyclicGraph;
 
+import eu.interedition.collatex2.implementation.vg_alignment.EndToken;
+import eu.interedition.collatex2.implementation.vg_alignment.StartToken;
+
 @SuppressWarnings("serial")
 public class DecisionGraph extends DirectedAcyclicGraph<DGVertex, DGEdge> {
 
+  private final DGVertex v1;
+  private final DGVertex end;
+
   public DecisionGraph() {
     super(DGEdge.class);
+    v1 = new DGVertex(new StartToken());
+    //TODO: that eight there is not handy!
+    //TODO: the end vertex is unique by itself...
+    //TODO: override the equals!
+    end = new DGVertex(new EndToken(8));
   }
 
   public void add(DGVertex... vertices) {
@@ -19,6 +30,14 @@ public class DecisionGraph extends DirectedAcyclicGraph<DGVertex, DGEdge> {
     for (DGEdge e : edges) {
       addEdge(e.getBeginVertex(), e.getEndVertex(), e);
     }
+  }
+
+  public DGVertex getStartVertex() {
+    return v1;
+  }
+
+  public DGVertex getEndVertex() {
+    return end;
   }
 
 }
--- collatex/src/test/java/eu/interedition/collatex2/decision_graph/DecisionGraphTest.java
+++ collatex/src/test/java/eu/interedition/collatex2/decision_graph/DecisionGraphTest.java
@@ -13,8 +13,6 @@ import eu.interedition.collatex2.implementation.CollateXEngine;
 import eu.interedition.collatex2.implementation.decision_graph.DGEdge;
 import eu.interedition.collatex2.implementation.decision_graph.DGVertex;
 import eu.interedition.collatex2.implementation.decision_graph.DecisionGraph;
-import eu.interedition.collatex2.implementation.vg_alignment.EndToken;
-import eu.interedition.collatex2.implementation.vg_alignment.StartToken;
 import eu.interedition.collatex2.interfaces.IWitness;
 
 public class DecisionGraphTest {
@@ -38,9 +36,12 @@ public class DecisionGraphTest {
     // dan zou het mogelijk moeten zijn om meerdere shortest paths 
     // te reconstrueren
     
+
+    // we use a weighted DAG to make alignment decisions
+    
     // eerst de vertices
     DecisionGraph graph = new DecisionGraph();
-    DGVertex v1 = new DGVertex(new StartToken());
+    DGVertex v1 = graph.getStartVertex();
     DGVertex vThe1 = new DGVertex(a.getTokens().get(0));
     DGVertex vThe2 = new DGVertex(a.getTokens().get(4));
     DGEdge e1 = new DGEdge(v1, vThe1, 0); // 0 = no gap -> ENumeration?
@@ -58,12 +59,43 @@ public class DecisionGraphTest {
     DGEdge e6 = new DGEdge(vB, vC2, 0);
     graph.add(vC1, vC2);
     graph.add(e5, e6);
-    DGVertex end = new DGVertex(new EndToken(8));
+    DGVertex end = graph.getEndVertex();
     DGEdge e7 = new DGEdge(vC1, end, 1);
     DGEdge e8 = new DGEdge(vC2, end, 0);
     graph.add(end);
     graph.add(e7, e8);
     
+    Map<DGVertex, Integer> vertexToMinWeight = determineMinWeightForEachVertex(graph);
+    
+    
+    // we moeten de weight van de end vertex hebben om de rest te filteren..
+    DGVertex endVertex = graph.getEndVertex();
+    int minGaps = vertexToMinWeight.get(endVertex);
+    System.out.println("Mininum number of gaps in the alignment: "+minGaps);
+    
+    // ik kan nu een nieuwe graph maken waarbij ik alle vertices en edges die niet kleiner 
+    // of gelijk de minimum weight zijn deleten
+    // maar of dat echt nodig is
+    // is nog maar de vraag
+    
+    
+    
+    // we moeten bijhouden welk pad we gelopen hebben in de vorm van edges
+    // ook moeten we bijhouden welke stappen we nog moeten zetten
+    // daar twijfel ik tussen de vertices en de edges
+    // aangezien je wil recursen bij meerdere vertices..
+    // en dan dus een bepaalde edge meegeven om te doen...
+    // laten we initialisen met een bepaalde edge
+//    DGVertex start = graph.getStartVertex();
+//    Set<DGEdge> outgoingEdges = graph.outgoingEdgesOf(start);
+//    for (DGEdge edge : outgoingEdges) {
+//      edge.getEndVertex();
+//      if (vertexToMinWeight )
+//    }
+    
+  }
+
+  private Map<DGVertex, Integer> determineMinWeightForEachVertex(DecisionGraph graph) {
     // bepalen minimaal aantal gaps in de decision graph
     Map<DGVertex, Integer> vertexToMinWeight = Maps.newLinkedHashMap();
     Iterator<DGVertex> iterator = graph.iterator();
@@ -82,7 +114,17 @@ public class DecisionGraphTest {
       vertexToMinWeight.put(next, min);
     }
     System.out.println(vertexToMinWeight);
+    return vertexToMinWeight;
   }
+  
+  // we kunnen natuurlijk de graph in een tree converten
+  // door strategies de vertex te dupliceren
+  // dan kun je alle paden vinden door de leaf nodes af te lopen
+  // een dag zou meerdere start nodes kunne hebben
+  // daar is er geen algoritme voor
+  // maar mijn dag heeft maar 1 start node..
+  // aargh
+  
   
   // het moet wel met een graph want anders wordt het nix
   // in een nromale decision tree schuif je dan die ene optie in de bij de andere
