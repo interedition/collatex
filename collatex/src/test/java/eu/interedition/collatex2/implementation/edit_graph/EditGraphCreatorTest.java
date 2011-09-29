7556b77ae39e2f8c40e16a12f73f86b8a0c36e8e
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2011-09-29 18:12:17 +0200

    [RHD] Partial sync with personal repository: Updated EditGraphCreatorTest to use mock objects instead of asserts.

--- collatex/src/test/java/eu/interedition/collatex2/implementation/edit_graph/EditGraphCreatorTest.java
+++ collatex/src/test/java/eu/interedition/collatex2/implementation/edit_graph/EditGraphCreatorTest.java
@@ -1,28 +1,110 @@
 package eu.interedition.collatex2.implementation.edit_graph;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import java.util.Iterator;
 
 import org.junit.Test;
 
 import eu.interedition.collatex2.implementation.CollateXEngine;
-import eu.interedition.collatex2.implementation.edit_graph.EditGraphEdge;
-import eu.interedition.collatex2.implementation.edit_graph.EditGraphVertex;
-import eu.interedition.collatex2.implementation.edit_graph.EditGraph;
-import eu.interedition.collatex2.implementation.edit_graph.EditGraphCreator;
+import eu.interedition.collatex2.implementation.containers.witness.FakeWitness;
 import eu.interedition.collatex2.implementation.matching.VariantGraphMatcher;
+import eu.interedition.collatex2.interfaces.INormalizedToken;
 import eu.interedition.collatex2.interfaces.IVariantGraph;
 import eu.interedition.collatex2.interfaces.IWitness;
 
 public class EditGraphCreatorTest {
 
+  @Test
+  public void testExample1Mockito() {
+//    CollateXEngine engine = new CollateXEngine();
+//    IWitness a = engine.createWitness("a", "The red cat and the black cat");
+//    IWitness b = engine.createWitness("b", "The red cat and the black cat");
+//    IVariantGraph vGraph = engine.graph(a);
+//    VariantGraphMatcher matcher = new VariantGraphMatcher();
+    
+    //setup witnesses
+    FakeWitness base = new FakeWitness();
+    INormalizedToken bThe = base.add("The");
+    INormalizedToken bRed = base.add("red");
+    INormalizedToken bCat = base.add("cat");
+    INormalizedToken bAnd = base.add("and");
+    INormalizedToken bThe2 = base.add("the");
+    INormalizedToken bBlack = base.add("black");
+    INormalizedToken bCat2 = base.add("cat");
+    
+    FakeWitness witness = new FakeWitness();
+    INormalizedToken wThe = witness.add("The");
+    INormalizedToken wRed = witness.add("red");
+    INormalizedToken wCat = witness.add("cat");
+    INormalizedToken wAnd = witness.add("and");
+    INormalizedToken wThe2 = witness.add("the");
+    INormalizedToken wBlack = witness.add("black");
+    INormalizedToken wCat2 = witness.add("cat");
+ 
+    //setup vertices
+    EditGraphVertex startVertex = new EditGraphVertex(null, null); // vGraph.getStartVertex());
+    EditGraphVertex vertex1 = new EditGraphVertex(wThe, bThe);
+    EditGraphVertex vertex2 = new EditGraphVertex(wThe, bThe2);
+    EditGraphVertex vertex3 = new EditGraphVertex(wRed, bRed);
+    EditGraphVertex vertex4 = new EditGraphVertex(wCat, bCat);
+    EditGraphVertex vertex5 = new EditGraphVertex(wCat, bCat2);
+    EditGraphVertex vertex6 = new EditGraphVertex(wAnd, bAnd);
+    EditGraphVertex vertex7 = new EditGraphVertex(wThe2, bThe);
+    EditGraphVertex vertex8 = new EditGraphVertex(wThe2, bThe2);
+    EditGraphVertex vertex9 = new EditGraphVertex(wBlack, bBlack);
+    EditGraphVertex vertex10 = new EditGraphVertex(wCat2, bCat);
+    EditGraphVertex vertex11 = new EditGraphVertex(wCat2, bCat2);
+    
+    
+    //mock
+    EditGraph editGraph = mock(EditGraph.class);
+    when(editGraph.getStartVertex()).thenReturn(startVertex);
+    
+    //run
+    EditGraphCreator creator = new EditGraphCreator(editGraph, null, null, null); // matcher, vGraph, b);
+    creator.buildEditGraph(base, witness);
+    
+    //verify vertices
+    verify(editGraph).getStartVertex();
+    verify(editGraph).add(vertex1);
+    verify(editGraph).add(vertex2);
+    verify(editGraph).add(vertex3);
+    verify(editGraph).add(vertex4);
+    verify(editGraph).add(vertex5);
+    verify(editGraph).add(vertex6);
+    verify(editGraph).add(vertex7); // transposition!
+    verify(editGraph).add(vertex8);
+    verify(editGraph).add(vertex9);
+    verify(editGraph).add(vertex10); // transposition!
+    verify(editGraph).add(vertex11);
+    
+    //verify edges
+    //TODO: add checks for edges!
+    //verify(editGraph).add(new EditGraphEdge(startVertex, vertex1, 0));
+    
+    verifyNoMoreInteractions(editGraph);
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   //TODO: add a test where there is sometimes no match for a given token
   
   // All the witness are equal
   // There are choices to be made however, since there is duplication of tokens
   @Test
-  public void testBuildingDGEverythingEqual() {
+  public void testBuildingEditGraphEverythingEqual() {
     CollateXEngine engine = new CollateXEngine();
     IWitness a = engine.createWitness("a", "The red cat and the black cat");
     IWitness b = engine.createWitness("b", "The red cat and the black cat");
@@ -86,7 +168,7 @@ public class EditGraphCreatorTest {
   }
 
   @Test
-  public void testDecisionGraphOmission() {
+  public void testEditGraphOmission() {
     CollateXEngine engine = new CollateXEngine();
     IWitness a = engine.createWitness("a", "The red cat and the black cat");
     IWitness b = engine.createWitness("b", "the black cat");
