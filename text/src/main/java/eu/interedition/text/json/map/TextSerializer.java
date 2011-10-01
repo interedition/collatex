4ab672d1393145b7b6b6526fee4a5f6faa2d4734
Gregor Middell <gregor@middell.net>
2011-10-01 09:31:07 +0200

    JSON serializer for text entities

---
+++ text/src/main/java/eu/interedition/text/json/map/TextSerializer.java
@@ -0,0 +1,35 @@
+package eu.interedition.text.json.map;
+
+import eu.interedition.text.Text;
+import eu.interedition.text.rdbms.RelationalText;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.JsonSerializer;
+import org.codehaus.jackson.map.SerializerProvider;
+
+import java.io.IOException;
+
+/**
+ * @author <a href="http://gregor.middell.net/" title="Homepage">Gregor Middell</a>
+ */
+public class TextSerializer extends JsonSerializer<Text> {
+  private static final String TYPE_FIELD = "type";
+  private static final String LENGTH_FIELD = "len";
+  private static final String ID_FIELD = "id";
+
+  @Override
+  public Class<Text> handledType() {
+    return Text.class;
+  }
+
+  @Override
+  public void serialize(Text value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+    jgen.writeStartObject();
+    jgen.writeStringField(TYPE_FIELD, value.getType().name().toLowerCase());
+    jgen.writeNumberField(LENGTH_FIELD, value.getLength());
+    if (value instanceof RelationalText) {
+      jgen.writeNumberField(ID_FIELD, ((RelationalText) value).getId());
+    }
+    jgen.writeEndObject();
+  }
+}
--- text/src/main/java/eu/interedition/text/json/map/TextSerializerModule.java
+++ text/src/main/java/eu/interedition/text/json/map/TextSerializerModule.java
@@ -1,6 +1,5 @@
 package eu.interedition.text.json.map;
 
-import com.sun.org.apache.xml.internal.serialize.TextSerializer;
 import org.codehaus.jackson.Version;
 import org.codehaus.jackson.map.module.SimpleModule;
 
@@ -11,6 +10,7 @@ public class TextSerializerModule extends SimpleModule {
 
   public TextSerializerModule() {
     super(TextSerializerModule.class.getPackage().getName(), new Version(1, 0, 0, ""));
+    addSerializer(new TextSerializer());
     addSerializer(new QNameSerializer());
     addSerializer(new RangeSerializer());
     addSerializer(new AnnotationSerializer());
