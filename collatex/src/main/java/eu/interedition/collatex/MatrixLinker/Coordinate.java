8f08a8aef670ec4e4944838afb236b659e43d544
mkroese <meindert.kroese@huygens.knaw.nl>
2012-01-19 15:13:06 +0100

    [MK] Added class Coordinate

---
+++ collatex/src/main/java/eu/interedition/collatex/MatrixLinker/Coordinate.java
@@ -0,0 +1,36 @@
+package eu.interedition.collatex.MatrixLinker;
+
+public class Coordinate {
+	int row;
+	int col;
+	
+	Coordinate(int row, int col) {
+	  this.row = row;
+	  this.col = col;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+
+	public String toString() {
+		return "("+row+","+col+")";
+	}
+	
+	public boolean equals(Object o) {
+		if(!o.getClass().equals(Coordinate.class))
+			return false;
+		Coordinate c = (Coordinate) o;
+		return this.row==c.getRow() && this.col==c.getCol();
+	}
+
+	public boolean borders(Coordinate c) {
+    boolean res = Math.abs(this.row - c.getRow())==1;
+    res &= Math.abs(this.col - c.getCol())==1;
+		return res;
+  }
+}
