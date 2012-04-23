f47f9e730445d858ca39144b5872957b228825a8
Gregor Middell <gregor@middell.net>
2012-04-23 18:26:49 +0100

    add license

--- image-transform/src/main/java/eu/interedition/image/ImageFile.java
+++ image-transform/src/main/java/eu/interedition/image/ImageFile.java
@@ -1,24 +1,24 @@
-/* ImageInput-- digilib image input interface.
-
-  Digital Image Library servlet components
-
-  Copyright (C) 2010 Robert Casties (robcast@mail.berlios.de)
-
-  This program is free software; you can redistribute  it and/or modify it
-  under  the terms of  the GNU General  Public License as published by the
-  Free Software Foundation;  either version 2 of the  License, or (at your
-  option) any later version.
-   
-  Please read license.txt for the full details. A copy of the GPL
-  may be found at http://www.gnu.org/copyleft/lgpl.html
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
- * Created on 20.12.2010
+/*
+ * #%L
+ * Image Transformation: Utilities for transforming digital images.
+ * %%
+ * Copyright (C) 2010 - 2012 The Interedition Development Group
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
  */
-
 package eu.interedition.image;
 
 import com.google.common.base.Preconditions;
