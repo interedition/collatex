e663d01d18a1264b93e4959e028213d9e8c296c4
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2012-06-26 17:05:01 +0200

    [RHD] added extra unit test for creating of match table

--- collatex/src/test/java/eu/interedition/collatex/dekker/matrix/MatchTableTest.java
+++ collatex/src/test/java/eu/interedition/collatex/dekker/matrix/MatchTableTest.java
@@ -13,7 +13,7 @@ import eu.interedition.collatex.simple.SimpleWitness;
 public class MatchTableTest extends AbstractTest {
 
   @Test
-  public void testAbAcAbc() {
+  public void testTableCreationAbAcAbc() {
     SimpleWitness[] witnesses = createWitnesses("a b", "a c", "a b c");
     VariantGraph graph = collate(witnesses[0], witnesses[1]);
     MatchTable matrix = MatchTable.create(graph, witnesses[2]);
@@ -22,4 +22,18 @@ public class MatchTableTest extends AbstractTest {
     assertVertexEquals("b", table.at(1, 1));
     assertVertexEquals("c", table.at(2, 1));
   }
+  
+  @Test
+  public void testTableCreationAbcabCab() {
+    SimpleWitness[] witnesses = createWitnesses("a b c a b", "c a b");
+    VariantGraph graph = collate(witnesses[0]);
+    MatchTable matrix = MatchTable.create(graph, witnesses[1]);
+    ArrayTable<Token, Integer, VariantGraphVertex> table = matrix.getTable();
+    assertVertexEquals("a", table.at(1, 0));
+    assertVertexEquals("b", table.at(2, 1));
+    assertVertexEquals("c", table.at(0, 2));
+    assertVertexEquals("a", table.at(1, 3));
+    assertVertexEquals("b", table.at(2, 4));
+  }
+
 }
