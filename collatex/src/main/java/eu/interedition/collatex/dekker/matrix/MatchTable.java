1cc5fe4a6b7c7cabbb3218600b057f168d9e68de
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2012-07-04 10:31:33 +0200

    [RHD] Added extra test + fix for number of columns during table creation

--- collatex/src/main/java/eu/interedition/collatex/dekker/matrix/MatchTable.java
+++ collatex/src/main/java/eu/interedition/collatex/dekker/matrix/MatchTable.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import com.google.common.collect.ArrayTable;
 import com.google.common.collect.DiscreteDomains;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Range;
@@ -26,7 +25,12 @@ import eu.interedition.collatex.matching.Matches;
 
 //Note: this class is intended to replace the current MatchMatrix class
 //The current class is limited to pairwise collation
-//This one should not be
+//This one is not
+//TODO: The current implementation is based on an ArrayTable
+//TODO: A hashmap based table implementation would be more memory efficient for
+//TODO: a sparse table such as this.
+//TODO: However the hashmap based table has a different API, so making MatchTable
+//TODO: work with it requires work
 public class MatchTable {
   static Logger LOG = LoggerFactory.getLogger(MatchTable.class);
   private final ArrayTable<Token, Integer, VariantGraphVertex> table;
@@ -63,8 +67,8 @@ public class MatchTable {
 
   private static MatchTable createEmptyTable(VariantGraph graph, Iterable<Token> witness) {
     graph.rank();
-    // -3 === ignore the start and the end vertex
-    Range<Integer> range = Ranges.closed(0, Math.max(0, Iterables.size(graph.vertices())-3));
+    // -2 === ignore the start and the end vertex
+    Range<Integer> range = Ranges.closed(0, Math.max(0, graph.getEnd().getRank()-2));
     ImmutableList<Integer> set = range.asSet(DiscreteDomains.integers()).asList();
     return new MatchTable(witness, set);
   }
