5cb7735285f252c5c7ea2f1a6695aef6f74a8450
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2012-07-18 15:38:00 +0200

    [RHD] Added Seg type=word support and a numbers of paragraphs limit.

--- collatex/src/test/java/eu/interedition/collatex/simple/SimpleWitnessTeiBuilderTest.java
+++ collatex/src/test/java/eu/interedition/collatex/simple/SimpleWitnessTeiBuilderTest.java
@@ -15,11 +15,19 @@ public class SimpleWitnessTeiBuilderTest {
   @Ignore
   @Test
   public void testTei() throws IOException, XMLStreamException {
-    InputStream resourceAsStream = getClass().getResourceAsStream("/Tara.xml");
+    InputStream resourceAsStream = getClass().getResourceAsStream("/matenadaran_1731.xml");
     //System.out.println(resourceAsStream.available());
     SimpleWitnessTeiBuilder builder = new SimpleWitnessTeiBuilder();
     SimpleWitness w = builder.read(resourceAsStream);
     for (Token t : w) {
+      System.out.print(((SimpleToken)t).getContent()+" ");
+    }
+    System.out.println();
+    InputStream stream1767 = getClass().getResourceAsStream("/matenadaran_1767.xml");
+    //System.out.println(resourceAsStream.available());
+    builder = new SimpleWitnessTeiBuilder();
+    SimpleWitness w2 = builder.read(stream1767);
+    for (Token t : w2) {
       System.out.print(((SimpleToken)t).getContent()+" ");
     }
   }
