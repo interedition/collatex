dca977c342cb86233e9c49bc69ed685967e32a09
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2012-07-18 15:38:00 +0200

    [RHD] Added Seg type=word support and a numbers of paragraphs limit.

--- collatex/src/main/java/eu/interedition/collatex/simple/SimpleWitnessTeiBuilder.java
+++ collatex/src/main/java/eu/interedition/collatex/simple/SimpleWitnessTeiBuilder.java
@@ -11,14 +11,14 @@ import javax.xml.stream.events.XMLEvent;
 
 import com.google.common.collect.Lists;
 
-import eu.interedition.collatex.Witness;
-
 // we are going to use stax
 // previous version was build on DOM, which is not particularly well suited to parsing
 // large TEI files, with segments and expansions
 public class SimpleWitnessTeiBuilder {
 
   private static QName w = new QName("http://www.tei-c.org/ns/1.0", "w");
+  private static QName seg = new QName("http://www.tei-c.org/ns/1.0", "seg");
+  private static QName p = new QName("http://www.tei-c.org/ns/1.0", "p");
 
   public SimpleWitness read(InputStream input) throws XMLStreamException {
     SimpleWitness witness = new SimpleWitness("id");
@@ -26,10 +26,16 @@ public class SimpleWitnessTeiBuilder {
     XMLInputFactory factory = XMLInputFactory.newInstance();
     XMLEventReader reader = factory.createXMLEventReader(input);
     XMLEvent event = null;
-    while ((event = reader.peek()) != null) {
-//      System.out.println(event.toString());
+    int numberOfParagraphs = 0;
+    while ((event = reader.peek()) != null && numberOfParagraphs < 10) {
+      // System.out.println(event.toString());
       if (event.isStartElement() && event.asStartElement().getName().equals(w)) {
 	tokenContents.add(handleWElement(reader));
+      } else if (event.isStartElement() && event.asStartElement().getName().equals(seg)) {
+	tokenContents.add(handleSegElement(reader));
+      } else if (event.isStartElement() && event.asStartElement().getName().equals(p)) {
+	reader.next();
+	numberOfParagraphs++;
       } else {
 	reader.next();
       }
@@ -42,7 +48,7 @@ public class SimpleWitnessTeiBuilder {
     XMLEvent event = reader.nextEvent();
     // Do what you need to do with the start element, e.g. initialize
     // data structures
-    //System.out.println("W tag is triggered here!");
+    // System.out.println("W tag is triggered here!");
     StringBuffer textBuffer = new StringBuffer();
     while ((event = reader.peek()) != null) {
       if (event.isEndElement() && event.asEndElement().getName().equals(w)) {
@@ -55,10 +61,33 @@ public class SimpleWitnessTeiBuilder {
 	// dispatch to another handler function
 	event = reader.nextEvent();
 	textBuffer.append(event.toString());
-	//System.out.println("Text :"+event.toString());
+	// System.out.println("Text :"+event.toString());
       }
     }
     return textBuffer.toString();
   }
 
+  private static String handleSegElement(XMLEventReader reader) throws XMLStreamException {
+    XMLEvent event = reader.nextEvent();
+    // Do what you need to do with the start element, e.g. initialize
+    // data structures
+    // System.out.println("Seg tag is triggered here!");
+    StringBuffer textBuffer = new StringBuffer();
+    while ((event = reader.peek()) != null) {
+      if (event.isEndElement() && event.asEndElement().getName().equals(seg)) {
+	// Do what you need to do at the end, e.g. add data
+	// collected from sub elements, etc.
+	event = reader.nextEvent();
+	break;
+      } else {
+	// Do what you need to do for start or child elements, e.g.
+	// dispatch to another handler function
+	event = reader.nextEvent();
+	if (event.getEventType() == XMLEvent.CHARACTERS) {
+	  textBuffer.append(event.toString().trim());
+	}
+      }
+    }
+    return textBuffer.toString();
+  }
 }
