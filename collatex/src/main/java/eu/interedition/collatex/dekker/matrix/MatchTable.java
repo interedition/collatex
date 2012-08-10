9ea9440af1581a9d376aabf4ae49f5a69015f027
Ronald Haentjens Dekker <ronald.dekker@huygensinstituut.knaw.nl>
2012-08-10 14:44:17 +0200

    [RHD] Refactored the MatchTable class implementation into a HashBasedTable, which takes less memory

--- collatex/src/main/java/eu/interedition/collatex/dekker/matrix/MatchTable.java
+++ collatex/src/main/java/eu/interedition/collatex/dekker/matrix/MatchTable.java
@@ -8,10 +8,9 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.ArrayTable;
 import com.google.common.collect.DiscreteDomains;
+import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Range;
@@ -24,24 +23,19 @@ import eu.interedition.collatex.graph.VariantGraphVertex;
 import eu.interedition.collatex.matching.EqualityTokenComparator;
 import eu.interedition.collatex.matching.Matches;
 
-//Note: this class is intended to replace the current MatchMatrix class
-//The current class is limited to pairwise collation
-//This one is not
-//TODO: The current implementation is based on an ArrayTable
-//TODO: A hashmap based table implementation would be more memory efficient for
-//TODO: a sparse table such as this.
-//TODO: However the hashmap based table has a different API, so making MatchTable
-//TODO: work with it requires work
+// author: Ronald Haentjens Dekker
+//
+// This class represents a table of the matches
+// Since this table is sparse a Hashmap based implementation
+// is used rather than a Arraylist based one.
+// However the API of this class looks very much like an array based one
+// since you can use tokenAt(row, column) or vertexAt(row, column)
 public class MatchTable {
   static Logger LOG = LoggerFactory.getLogger(MatchTable.class);
-  private final ArrayTable<Integer, Integer, MatchTableCell> table;
+  private final HashBasedTable<Integer, Integer, MatchTableCell> table;
   private final Iterable<Token> witness;
+  private final List<Integer> ranks;
   
-  public MatchTable(Iterable<Token> tokens, Iterable<Integer> tokensInteger, Iterable<Integer> ranks) {
-    this.table = ArrayTable.create(tokensInteger, ranks);
-    this.witness = tokens;
-  }
-
   // assumes default token comparator
   public static MatchTable create(VariantGraph graph, Iterable<Token> witness) {
     Comparator<Token> comparator = new EqualityTokenComparator();
@@ -57,12 +51,12 @@ public class MatchTable {
   }
 
   public VariantGraphVertex vertexAt(int rowIndex, int columnIndex) {
-    MatchTableCell cell = table.at(rowIndex, columnIndex);
+    MatchTableCell cell = table.get(rowIndex, columnIndex);
     return cell==null ? null : cell.variantGraphVertex;
   }
   
   public Token tokenAt(int rowIndex, int columnIndex) {
-    MatchTableCell cell = table.at(rowIndex, columnIndex);
+    MatchTableCell cell = table.get(rowIndex, columnIndex);
     return cell==null ? null : cell.token;
   }
 
@@ -74,7 +68,32 @@ public class MatchTable {
   }
 
   public List<Integer> columnList() {
-    return table.columnKeyList();
+    return ranks;
+  }
+
+  // Since the coordinates in allMatches are ordered from upper left to lower right, 
+  // we don't need to check the lower right neighbor.
+  public Set<Island> getIslands() {
+    Map<Coordinate, Island> coordinateMapper = Maps.newHashMap();
+    List<Coordinate> allMatches = allMatches();
+    for (Coordinate c : allMatches) {
+      //      LOG.debug("coordinate {}", c);
+      addToIslands(coordinateMapper, c);
+    }
+    Set<Coordinate> smallestIslandsCoordinates = Sets.newHashSet(allMatches);
+    smallestIslandsCoordinates.removeAll(coordinateMapper.keySet());
+    for (Coordinate coordinate : smallestIslandsCoordinates) {
+      Island island = new Island();
+      island.add(coordinate);
+      coordinateMapper.put(coordinate, island);
+    }
+    return Sets.newHashSet(coordinateMapper.values());
+  }
+
+  private MatchTable(Iterable<Token> tokens, List<Integer> ranks) {
+    this.table = HashBasedTable.create();
+    this.witness = tokens;
+    this.ranks = ranks;
   }
 
   private static MatchTable createEmptyTable(VariantGraph graph, Iterable<Token> witness) {
@@ -82,9 +101,7 @@ public class MatchTable {
     // -2 === ignore the start and the end vertex
     Range<Integer> ranksRange = Ranges.closed(0, Math.max(0, graph.getEnd().getRank() - 2));
     ImmutableList<Integer> ranksSet = ranksRange.asSet(DiscreteDomains.integers()).asList();
-    Range<Integer> tokensRange = Ranges.closed(0, Iterables.size(witness)-1);
-    ImmutableList<Integer> tokensSet = tokensRange.asSet(DiscreteDomains.integers()).asList();
-    return new MatchTable(witness, tokensSet, ranksSet);
+    return new MatchTable(witness, ranksSet);
   }
 
   // move parameters into fields?
@@ -108,25 +125,6 @@ public class MatchTable {
     //    LOG.debug("putting: {}<->{}<->{}", new Object[] { token, columnIndex, variantGraphVertex });
     MatchTableCell cell = new MatchTableCell(token, variantGraphVertex);
     table.put(rowIndex, columnIndex, cell);
-  }
-
-  // Since the coordinates in allMatches are ordered from upper left to lower right, 
-  // we don't need to check the lower right neighbor.
-  public Set<Island> getIslands() {
-    Map<Coordinate, Island> coordinateMapper = Maps.newHashMap();
-    List<Coordinate> allMatches = allMatches();
-    for (Coordinate c : allMatches) {
-      //      LOG.debug("coordinate {}", c);
-      addToIslands(coordinateMapper, c);
-    }
-    Set<Coordinate> smallestIslandsCoordinates = Sets.newHashSet(allMatches);
-    smallestIslandsCoordinates.removeAll(coordinateMapper.keySet());
-    for (Coordinate coordinate : smallestIslandsCoordinates) {
-      Island island = new Island();
-      island.add(coordinate);
-      coordinateMapper.put(coordinate, island);
-    }
-    return Sets.newHashSet(coordinateMapper.values());
   }
 
   private void addToIslands(Map<Coordinate, Island> coordinateMapper, Coordinate c) {
@@ -154,16 +152,15 @@ public class MatchTable {
   }
 
   // Note: code taken from MatchMatrix class
-  // might be simpler to work from the cellSet
-  // problem there is that a token does not have to have a position
-  // but a Cell Object might have one? Upd: does not seem so
+  // TODO: might be simpler to work from the valueSet
+  // TODO: try remove the call to rowList / columnList
   List<Coordinate> allMatches() {
     List<Coordinate> pairs = Lists.newArrayList();
-    int rows = table.rowKeySet().size();
-    int cols = table.columnKeySet().size();
+    int rows = rowList().size();
+    int cols = columnList().size();
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
-        if (table.at(i, j) != null) pairs.add(new Coordinate(i, j));
+        if (vertexAt(i, j) != null) pairs.add(new Coordinate(i, j));
       }
     }
     return pairs;
