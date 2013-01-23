ec539c9f16ba02271cf895a93ef7dfecdb965abd
gregor <gregor@middell.net>
2013-01-23 14:55:13 +0000

    MEDITE adaptation: recursive alignment

--- collatex-core/src/main/java/eu/interedition/collatex/medite/MediteAlgorithm.java
+++ collatex-core/src/main/java/eu/interedition/collatex/medite/MediteAlgorithm.java
@@ -4,11 +4,11 @@ import com.google.common.base.Stopwatch;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Ranges;
 import com.google.common.collect.Sets;
 import eu.interedition.collatex.CollationAlgorithm;
 import eu.interedition.collatex.Token;
 import eu.interedition.collatex.VariantGraph;
-import eu.interedition.collatex.dekker.Match;
 import eu.interedition.collatex.dekker.Tuple;
 import eu.interedition.collatex.util.VariantGraphRanking;
 
@@ -24,7 +24,6 @@ import java.util.logging.Level;
  */
 public class MediteAlgorithm extends CollationAlgorithm.Base {
 
-
   private final Comparator<Token> comparator;
 
   public MediteAlgorithm(Comparator<Token> comparator) {
@@ -33,17 +32,17 @@ public class MediteAlgorithm extends CollationAlgorithm.Base {
 
   @Override
   public void collate(VariantGraph graph, Iterable<Token> witness) {
-    final Token[] tokens = Iterables.toArray(witness, Token.class);
+    final boolean logTimings = LOG.isLoggable(Level.FINE);
+    final Stopwatch stopwatch = (logTimings ? new Stopwatch() : null);
 
-    final Stopwatch stopwatch = new Stopwatch();
-
-    if (LOG.isLoggable(Level.FINE)) {
+    if (logTimings) {
       stopwatch.start();
     }
 
+    final Token[] tokens = Iterables.toArray(witness, Token.class);
     final SuffixTree<Token> suffixTree = SuffixTree.build(comparator, tokens);
 
-    if (LOG.isLoggable(Level.FINE)) {
+    if (logTimings) {
       stopwatch.stop();
       LOG.log(Level.FINE, "Built suffix tree of {0} token(s) in {1}", new Object[] { tokens.length, stopwatch });
       stopwatch.reset().start();
@@ -51,61 +50,68 @@ public class MediteAlgorithm extends CollationAlgorithm.Base {
 
     final VariantGraphRanking ranking = VariantGraphRanking.of(graph);
 
-    if (LOG.isLoggable(Level.FINE)) {
+    if (logTimings) {
       stopwatch.stop();
-      LOG.log(Level.FINE, "Ranked variant graph of {0} in {1}", new Object[] { graph, stopwatch });
+      LOG.log(Level.FINE, "Ranked variant graph {0} in {1}", new Object[] { graph, stopwatch });
       stopwatch.reset().start();
     }
 
 
     final Matches matcher = Matches.between(comparator, ranking, suffixTree);
 
-    if (LOG.isLoggable(Level.FINE)) {
+    if (logTimings) {
       stopwatch.stop();
-      LOG.log(Level.FINE, "Found {0} matching phrase(s) in {1}", new Object[] { matcher.size(), stopwatch });
+      LOG.log(Level.FINE, "Found a total of {0} matching phrase(s) in {1}", new Object[] { matcher.size(), stopwatch });
       stopwatch.reset().start();
     }
 
     final IndexRangeSet rankFilter = new IndexRangeSet();
     final IndexRangeSet tokenFilter = new IndexRangeSet();
 
-    final SortedSet<Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex>> phraseMatches = Sets.newTreeSet();
+    final SortedSet<Phrase<Match.WithTokenIndex>> alignments = Sets.newTreeSet();
     while (true) {
-      final SortedSet<Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex>> maximalUniqueMatches = matcher.removeMaximalUniqueMatches(rankFilter, tokenFilter);
+      final SortedSet<Phrase<Match.WithTokenIndex>> maximalUniqueMatches = matcher.removeMaximalUniqueMatches(rankFilter, tokenFilter);
       if (maximalUniqueMatches.isEmpty()) {
         break;
       }
-      phraseMatches.addAll(Aligner.align(maximalUniqueMatches));
-      break;
+      final SortedSet<Phrase<Match.WithTokenIndex>> aligned = Aligner.align(maximalUniqueMatches);
+      for (Phrase<Match.WithTokenIndex> phrase : aligned) {
+        final Match.WithTokenIndex firstMatch = phrase.first();
+        final Match.WithTokenIndex lastMatch = phrase.last();
+
+        rankFilter.add(Ranges.closed(firstMatch.vertexRank, lastMatch.vertexRank));
+        tokenFilter.add(Ranges.closed(firstMatch.token, lastMatch.token));
+      }
+      alignments.addAll(aligned);
     }
 
-    if (LOG.isLoggable(Level.FINE)) {
+    if (logTimings) {
       stopwatch.stop();
-      LOG.log(Level.FINE, "Selected {0} maximal unique matches in {1}", new Object[] { phraseMatches.size(), stopwatch });
+      LOG.log(Level.FINE, "Selected {0} maximal unique matches in {1}", new Object[] { alignments.size(), stopwatch });
       stopwatch.reset().start();
     }
 
-    final List<Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex>> transpositions = transpositions(phraseMatches);
+    final List<Phrase<Match.WithTokenIndex>> transpositions = transpositions(alignments);
 
-    if (LOG.isLoggable(Level.FINE)) {
+    if (logTimings) {
       stopwatch.stop();
-      LOG.log(Level.FINE, "Detected {0} transpositions in {1} maximal unique matches in {2}", new Object[] { transpositions.size(), phraseMatches.size(), stopwatch });
+      LOG.log(Level.FINE, "Detected {0} transpositions in {1} maximal unique matches in {2}", new Object[] { transpositions.size(), alignments.size(), stopwatch });
       stopwatch.reset().start();
     }
 
     final Map<Token, VariantGraph.Vertex> tokenMatches = Maps.newHashMap();
-    for (Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex> phraseMatch : phraseMatches) {
-      for (eu.interedition.collatex.medite.Match.WithTokenIndex tokenMatch : phraseMatch) {
+    for (Phrase<Match.WithTokenIndex> phraseMatch : alignments) {
+      for (Match.WithTokenIndex tokenMatch : phraseMatch) {
         tokenMatches.put(tokens[tokenMatch.token], tokenMatch.vertex);
       }
     }
 
-    final List<List<Match>> transpositionMatches = Lists.newLinkedList();
-    for (Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex> transposition : transpositions) {
-      final List<Match> transpositionMatch = Lists.newLinkedList();
-      for (eu.interedition.collatex.medite.Match.WithTokenIndex match : transposition) {
+    final List<List<eu.interedition.collatex.dekker.Match>> transpositionMatches = Lists.newLinkedList();
+    for (Phrase<Match.WithTokenIndex> transposition : transpositions) {
+      final List<eu.interedition.collatex.dekker.Match> transpositionMatch = Lists.newLinkedList();
+      for (Match.WithTokenIndex match : transposition) {
         tokenMatches.remove(tokens[match.token]);
-        transpositionMatch.add(new Match(match.vertex, tokens[match.token]));
+        transpositionMatch.add(new eu.interedition.collatex.dekker.Match(match.vertex, tokens[match.token]));
       }
       transpositionMatches.add(transpositionMatch);
     }
@@ -113,28 +119,28 @@ public class MediteAlgorithm extends CollationAlgorithm.Base {
     merge(graph, witness, tokenMatches);
     mergeTranspositions(graph, transpositionMatches);
 
-    if (LOG.isLoggable(Level.FINE)) {
+    if (logTimings) {
       stopwatch.stop();
       LOG.log(Level.FINE, "Merged {0} token matches and {1} transpositions in {2}", new Object[] { tokenMatches.size(), transpositions.size(), stopwatch });
     }
   }
 
-  List<Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex>> transpositions(SortedSet<Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex>> phraseMatches) {
+  List<Phrase<Match.WithTokenIndex>> transpositions(SortedSet<Phrase<Match.WithTokenIndex>> phraseMatches) {
 
     // gather matched tokens into a list ordered by their natural order
     final List<Integer> sortedMatchedTokens = Lists.newArrayList();
-    for (Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex> phraseMatch : phraseMatches) {
+    for (Phrase<Match.WithTokenIndex> phraseMatch : phraseMatches) {
       sortedMatchedTokens.add(phraseMatch.first().token);
     }
     Collections.sort(sortedMatchedTokens);
 
     // detect transpositions
-    final List<Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex>> transpositions = Lists.newArrayList();
+    final List<Phrase<Match.WithTokenIndex>> transpositions = Lists.newArrayList();
 
     int previousToken = 0;
     Tuple<Integer> previous = new Tuple<Integer>(0, 0);
 
-    for (Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex> phraseMatch : phraseMatches) {
+    for (Phrase<Match.WithTokenIndex> phraseMatch : phraseMatches) {
       int currentToken = phraseMatch.first().token;
       int expectedToken = sortedMatchedTokens.get(previousToken);
       Tuple<Integer> current = new Tuple<Integer>(expectedToken, currentToken);
@@ -151,10 +157,10 @@ public class MediteAlgorithm extends CollationAlgorithm.Base {
     return previousTuple.left.equals(tuple.right) && previousTuple.right.equals(tuple.left);
   }
 
-  String toString(Phrase<eu.interedition.collatex.medite.Match.WithTokenIndex> phrase, Token[] tokens) {
+  String toString(Phrase<Match.WithTokenIndex> phrase, Token[] tokens) {
     final List<VariantGraph.Vertex> phraseVertices = Lists.newArrayListWithExpectedSize(phrase.size());
     final List<Token> phraseTokens = Lists.newArrayListWithExpectedSize(phrase.size());
-    for (eu.interedition.collatex.medite.Match.WithTokenIndex tokenMatch : phrase) {
+    for (Match.WithTokenIndex tokenMatch : phrase) {
       phraseVertices.add(tokenMatch.vertex);
       phraseTokens.add(tokens[tokenMatch.token]);
     }
