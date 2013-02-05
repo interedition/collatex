7102a2029f443c309d463e65cd8cb0b9d76bd6dd
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2013-02-05 10:40:02 +0100

    [RHD] Issue #9: Made TokenLinker switchable between new Vector code and old Island code

--- collatex-core/src/main/java/eu/interedition/collatex/dekker/matrix/MatchTableLinker.java
+++ collatex-core/src/main/java/eu/interedition/collatex/dekker/matrix/MatchTableLinker.java
@@ -1,19 +1,24 @@
 package eu.interedition.collatex.dekker.matrix;
 
-import com.google.common.collect.Maps;
-import eu.interedition.collatex.Token;
-import eu.interedition.collatex.VariantGraph;
-import eu.interedition.collatex.dekker.TokenLinker;
-
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import eu.interedition.collatex.Token;
+import eu.interedition.collatex.VariantGraph;
+import eu.interedition.collatex.dekker.TokenLinker;
+import eu.interedition.collatex.dekker.matrix.VectorConflictResolver.Vector;
+
 //Note: This class is intended to be the replacement for the MatchMatrixLinker class
 public class MatchTableLinker implements TokenLinker {
-  static Logger LOG = Logger.getLogger(MatchTableLinker.class.getName());
+  private static final boolean USE_ARCHIPELAGO = true;
+	static Logger LOG = Logger.getLogger(MatchTableLinker.class.getName());
   private final int outlierTranspositionsSizeLimit;
 
   public MatchTableLinker(int outlierTranspositionsSizeLimit) {
@@ -31,29 +36,51 @@ public class MatchTableLinker implements TokenLinker {
     LOG.fine("getIslands()");
     Set<Island> islands = table.getIslands();
 
-    LOG.fine("create Archipelago data structure");
-    // create Archipelago data structure and fill it with all the islands
-    ArchipelagoWithVersions archipelago = new ArchipelagoWithVersions(table, outlierTranspositionsSizeLimit);
-    LOG.fine("fill it with all the islands");
-    for (Island isl : islands) {
-      archipelago.add(isl);
+    if (USE_ARCHIPELAGO==true) {
+	    LOG.fine("create Archipelago data structure");
+	    // create Archipelago data structure and fill it with all the islands
+	    ArchipelagoWithVersions archipelago = new ArchipelagoWithVersions(table, outlierTranspositionsSizeLimit);
+	    LOG.fine("fill it with all the islands");
+	    for (Island isl : islands) {
+	      archipelago.add(isl);
+	    }
+	
+	    // The archipelago with version createNonConflictingVersion() method
+	    // selects the optimal islands
+	    LOG.fine("select the optimal islands");
+	    Archipelago preferredIslands = archipelago.createNonConflictingVersion();
+	    if (LOG.isLoggable(Level.FINE)) {
+	      LOG.log(Level.FINE, "Number of preferred Islands: {0}", preferredIslands.size());
+	    }
+	
+	    // Here the result is put in a map
+	    Map<Token, VariantGraph.Vertex> map = Maps.newHashMap();
+	    for (Island island : preferredIslands.iterator()) {
+	      for (Coordinate c : island) {
+	        map.put(table.tokenAt(c.row, c.column), table.vertexAt(c.row, c.column));
+	      }
+	    }
+	    return map;
+    } else {
+      LOG.fine("create the vector space from the matches");
+	    // create Archipelago data structure and fill it with all the islands
+	    Set<Island> islands2 = table.getIslands();
+	    Set<Vector> vectors = Sets.newHashSet();
+	    for (Island i : islands2) {
+	    	Coordinate leftEnd = i.getLeftEnd();
+	    	Vector v = new Vector(leftEnd.column, leftEnd.row, i.size());
+	    	vectors.add(v);
+	    }
+	    VectorConflictResolver resolver = new VectorConflictResolver(vectors);
+	    List<Vector> resolved = resolver.resolveConflicts();
+	    // Here the result is put in a map
+	    Map<Token, VariantGraph.Vertex> map = Maps.newHashMap();
+	    for (Vector v2 : resolved) {
+	      for (int i=0; i < v2.length; i++) {
+	        map.put(table.tokenAt(v2.y+i, v2.x+i), table.vertexAt(v2.y+i, v2.x+i));
+	      }
+	    }
+	  	return map;
     }
-
-    // The archipelago with version createNonConflictingVersion() method
-    // selects the optimal islands
-    LOG.fine("select the optimal islands");
-    Archipelago preferredIslands = archipelago.createNonConflictingVersion();
-    if (LOG.isLoggable(Level.FINE)) {
-      LOG.log(Level.FINE, "Number of preferred Islands: {0}", preferredIslands.size());
-    }
-
-    // Here the result is put in a map
-    Map<Token, VariantGraph.Vertex> map = Maps.newHashMap();
-    for (Island island : preferredIslands.iterator()) {
-      for (Coordinate c : island) {
-        map.put(table.tokenAt(c.row, c.column), table.vertexAt(c.row, c.column));
-      }
-    }
-    return map;
   }
 }
