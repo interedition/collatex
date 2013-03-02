d96e0661df15f2905d9c3bbccf7e7275802c2f7e
gremid <gregor@middell.net>
2013-03-02 12:16:29 +0000

    added copyright headers

--- collatex-core/src/test/java/eu/interedition/collatex/dekker/AlignmentTest.java
+++ collatex-core/src/test/java/eu/interedition/collatex/dekker/AlignmentTest.java
@@ -1,21 +1,20 @@
-/**
- * CollateX - a Java library for collating textual sources, for example, to
- * produce an apparatus.
+/*
+ * Copyright (c) 2013 The Interedition Development Group.
  *
- * Copyright (C) 2010 ESF COST Action "Interedition".
+ * This file is part of CollateX.
  *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
+ * CollateX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * CollateX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with CollateX.  If not, see <http://www.gnu.org/licenses/>.
  */
 package eu.interedition.collatex.dekker;
 
--- collatex-tools/src/main/java/eu/interedition/collatex/http/VariantGraphDotMessageBodyWriter.java
+++ collatex-tools/src/main/java/eu/interedition/collatex/http/VariantGraphDotMessageBodyWriter.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2013 The Interedition Development Group.
+ *
+ * This file is part of CollateX.
+ *
+ * CollateX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CollateX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CollateX.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package eu.interedition.collatex.http;
 
 import com.google.common.io.Closeables;
