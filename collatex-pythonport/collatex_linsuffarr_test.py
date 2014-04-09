10120c065c84bb27dcb610111b2bdedc04c72439
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2014-04-09 10:54:05 +0200

    More work on finding the SuperMaximumRepeats of two witnesses.

--- collatex-pythonport/collatex_linsuffarr_test.py
+++ collatex-pythonport/collatex_linsuffarr_test.py
@@ -5,7 +5,8 @@ Created on Apr 7, 2014
 '''
 import unittest
 from linsuffarr import SuffixArray
-from collatex_simple import SuperMaximumRe
+from collatex_simple import SuperMaximumRe, Block
+from rangeset import RangeSet
 
 
 
@@ -19,11 +20,13 @@ from collatex_simple import SuperMaximumRe
 class Test(unittest.TestCase):
 
 
+    #TODO: write asserts
     def testLineairSuffixArray(self):
         sa = SuffixArray("The Quick Brown Fox Jumped Over")
         print(sa)
         pass
 
+    #TODO: write asserts
     def testSA2(self):
         # $ is meant to separate witnesses here
         sa = SuffixArray("a b c d F g h i ! K ! q r s t $ a b c d F g h i ! q r s t")
@@ -35,7 +38,13 @@ class Test(unittest.TestCase):
         sa = SuffixArray("a b c d F g h i ! K ! q r s t $ a b c d F g h i ! q r s t")
         smr = SuperMaximumRe()
         blocks = smr.find_blocks(sa)
-        print(blocks)
+        # we expect two blocks ("a b c d F g h i", "! q r s t")
+        #TODO: now the first block has size 9 instead of 8!
+        #NOTE: It at the moment finds the Maximum Repeats not the Super Maximal Repeats
+        block1 = Block(RangeSet(0,8).union(RangeSet(16,24)))
+        block2 = Block(RangeSet(10,14).union(RangeSet(24,28)))
+        #print(blocks)
+        self.assertEqual([block1, block2], blocks)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
--- collatex-pythonport/collatex_simple.py
+++ collatex-pythonport/collatex_simple.py
@@ -11,11 +11,48 @@ class Block(object):
     def __init__(self, ranges):
         self.ranges = ranges
     
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.__dict__ == other.__dict__
+        return False
+    
     def __str__(self):
         return "Block with occurrences "+self.ranges.__str__()
     
+    def __repr__(self):
+        return "wowie a block: "+self.ranges.__str__()
+    
 class SuperMaximumRe(object):
     
+    def find_blocks(self, sa):
+        lcp = sa._LCP_values
+        blocks = []
+        # I can not seem to make an empty RangeSet, nor can I add to a RangeSet
+        # so we have to reassign the variable each time
+        # TODO: instead of using an occupied range set it would be better
+        # to loop over the blocks and delegate this responsibility to them.
+        occupied = None
+        max_prefix = -1
+        while(max_prefix!=0):
+            max_position, max_prefix = self.find_max_prefix(lcp)
+            if (max_prefix!=0):
+                piece1 = sa.SA[max_position-1]
+                piece2 = sa.SA[max_position]
+                p1 = RangeSet(piece1, piece1+max_prefix-1)
+                p2 = RangeSet(piece2, piece2+max_prefix-1)
+                # NOTE: it might be that issubset is not good enough
+                # and that overlap != empty has to be used instead
+                if occupied == None: 
+                    occupied = RangeSet.mutual_union(p1, p2) 
+                    blocks.append(Block(RangeSet.mutual_union(p1, p2)))
+                else:
+                    if p1.issubset(occupied) == False & p2.issubset(occupied) == False:
+                        blocks.append(Block(RangeSet.mutual_union(p1, p2)))
+                        occupied = RangeSet.mutual_union(occupied, p1, p2)
+                # reset the lcp value to zero
+                # TODO: it is not nice to change the lcp value
+                lcp[max_position]=0
+        return blocks
 
     def find_max_prefix(self, lcp):
         max_prefix = 0
@@ -25,26 +62,8 @@ class SuperMaximumRe(object):
                 max_prefix = prefix
                 max_position = index
         
-        print(max_prefix, max_position)
+        #print(max_prefix, max_position)
         return max_position, max_prefix
-
-    def find_blocks(self, sa):
-        lcp = sa._LCP_values
-        blocks = []
-        
-        max_prefix = -1
-        while(max_prefix!=0):
-            max_position, max_prefix = self.find_max_prefix(lcp)
-            if (max_prefix!=0):
-                piece1 = sa.SA[max_position-1]
-                piece2 = sa.SA[max_position]
-                p1 = RangeSet(piece1, piece1+max_prefix-1)
-                p2 = RangeSet(piece2, piece2+max_prefix-1)
-                blocks.append(Block(RangeSet.mutual_union(p1, p2)))
-                # reset the lcp value to zero
-                # it is not nice to change the lcp value
-                lcp[max_position]=0
-        return blocks
 
 # not used
 # Tokenizer inside suffix array library is used
@@ -55,7 +74,7 @@ class Tokenizer(object):
         return contents.split()
 
 # not used
-# suffix library is used    
+# external suffix library is used    
 class Suffix(object):
     
     #generate suffixes from a list of tokens
