7459ed6fb14f48802de161520ba79d36e8d4c335
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2014-11-06 15:06:36 +0100

    Updated exposure of public API.

--- collatex-pythonport/collatex/__init__.py
+++ collatex-pythonport/collatex/__init__.py
@@ -3,11 +3,12 @@
 
 __author__ = 'Ronald Haentjens Dekker'
 __email__ = 'ronald.dekker@huygens.knaw.nl'
-__version__ = '2.0.0'
+__version__ = '2.0.0pre10'
 
 from collatex.collatex_dekker_algorithm import Collation
 from collatex.collatex_dekker_algorithm import collate
+from collatex.collatex_dekker_algorithm import collate_pretokenized_json
 
-__all__ = ["Collation", "collate"]
+__all__ = ["Collation", "collate", "collate_pretokenized_json"]
 
 
