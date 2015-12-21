64e21019c552971dac99f23017090578b6ea820b
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2015-12-21 21:46:10 +0100

    Removed caching_of_suffix_array_test. Reason: no longer relevant. Moved failing tests from test_extended_suffix_array to test_tokenindex and made them work again. Deleted now empty test_extended_suffixarray.

--- collatex-pythonport/tests/test_tokenindex.py
+++ collatex-pythonport/tests/test_tokenindex.py
@@ -112,3 +112,30 @@ class Test(unittest.TestCase):
         self.assertIntervalIn(0, 20, 4, split_intervals)
         self.assertIntervalIn(0, 4, 5, split_intervals)
         self.assertEqual(2, len(split_intervals), "More items: "+str(split_intervals))
+
+    def test_lcp_intervals_number_of_witnesses_Hermans_case(self):
+        collation = Collation()
+        collation.add_plain_witness("W1", "a b c d F g h i ! K ! q r s t")
+        collation.add_plain_witness("W2", "a b c d F g h i ! q r s t")
+        collation.add_plain_witness("W3", "a b c d E g h i ! q r s t")
+        token_index = TokenIndex(collation.witnesses)
+        token_index.prepare()
+        intervals = token_index.split_lcp_array_into_intervals()
+        potential_block = intervals[1] # ! q r s t
+        self.assertEqual(3, potential_block.number_of_witnesses)
+
+    def test_filter_potential_blocks(self):
+        collation = Collation()
+        collation.add_plain_witness("W1", "a a")
+        collation.add_plain_witness("w2", "a")
+        token_index = TokenIndex(collation.witnesses)
+        token_index.prepare()
+        intervals = token_index.split_lcp_array_into_intervals()
+        # expectations
+        # There is one interval with length 1, number of occurrences 3, number of witnesses: 2
+        a_interval = intervals[0] # a
+        self.assertEqual(2, a_interval.number_of_witnesses)
+        self.assertEqual(1, a_interval.length)
+        self.assertEqual(3, a_interval.number_of_occurrences)
+
+
