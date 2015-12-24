a7c17ed6c943f335097b7838990eb6f381ab99cd
David J Birnbaum <djbpitt@gmail.com>
2015-12-24 11:27:17 -0500

    Removed unused collation functions from init

--- collatex-pythonport/collatex/__init__.py
+++ collatex-pythonport/collatex/__init__.py
@@ -7,10 +7,6 @@ __version__ = '2.0.0rc16'
 
 from collatex.core_functions import Collation
 from collatex.core_functions import collate
-from collatex.core_functions import collate_pretokenized_json
-from collatex.core_functions import collate_nearMatch
-from collatex.core_functions import collate_pretokenized_json_nearMatch
 
-__all__ = ["Collation", "collate", "collate_pretokenized_json", "collate_nearMatch", "collate_pretokenized_json_nearMatch"]
-
+__all__ = ["Collation", "collate"]
 
