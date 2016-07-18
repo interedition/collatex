6e1a574c3f91c0992b584876c1821b7875ae40a0
David J Birnbaum <djbpitt@gmail.com>
2016-07-18 07:26:18 -0400

    Revise test_tokenized_json.py
    Replace references to old collate_pretokenized_json() method with collate()
    Compare JSON output to expected output as dictionaries (using json_loads())

--- collatex-pythonport/tests/test_tokenized_json.py
+++ collatex-pythonport/tests/test_tokenized_json.py
@@ -3,8 +3,10 @@ Created on Jun 11, 2014
 
 @author: Ronald Haentjens Dekker
 '''
+import json
 import unittest
-from collatex.core_functions import collate_pretokenized_json
+
+from collatex import collate
 from collatex.exceptions import *
     
 
@@ -32,9 +34,9 @@ class Test(unittest.TestCase):
       ]
     }
 
-        expected_json = '{"table": [[[{"ref": 123, "t": "A"}], [{"adj": true, "t": "black"}], [{"id": "xyz", "t": "cat"}]], [[{"t": "A"}], [{"adj": true, "t": "white"}], [{"n": "cat", "t": "kitten."}]]], "witnesses": ["A", "B"]}'
-        json_out = collate_pretokenized_json(json_in, output="json")
-        self.assertEqual(expected_json, json_out)
+        expected_json = {"table": [[[{"ref": 123, "t": "A"}], [{"adj": True, "t": "black"}], [{"id": "xyz", "t": "cat"}]], [[{"t": "A"}], [{"adj": True, "t": "white"}], [{"n": "cat", "t": "kitten."}]]], "witnesses": ["A", "B"]}
+        json_out = collate(json_in, output="json")
+        self.assertEqual(expected_json, json.loads(json_out))
 
     def testJSONOutput_empty_cells_in_output(self):
         json_in = {
@@ -56,9 +58,9 @@ class Test(unittest.TestCase):
         }
       ]
     }
-        expected_json = '{"table": [[[{"ref": 123, "t": "A"}], [{"adj": true, "t": "black"}], [{"id": "xyz", "t": "cat"}]], [[{"t": "A"}], [null], [{"n": "cat", "t": "kitten."}]]], "witnesses": ["A", "B"]}'
-        json_out = collate_pretokenized_json(json_in, output="json")
-        self.assertEqual(expected_json, json_out)
+        expected_json = {"table": [[[{"ref": 123, "t": "A"}], [{"adj": True, "t": "black"}], [{"id": "xyz", "t": "cat"}]], [[{"t": "A"}], None, [{"n": "cat", "t": "kitten."}]]], "witnesses": ["A", "B"]}
+        json_out = collate(json_in, output="json")
+        self.assertEqual(expected_json, json.loads(json_out))
 
     def testHTMLOutputPretokenizedJSON(self):
         json_in = {
@@ -86,7 +88,7 @@ class Test(unittest.TestCase):
 | A | A | black | cat     |
 | B | A | white | kitten. |
 +---+---+-------+---------+"""
-        plain_table = str(collate_pretokenized_json(json_in, output="table"))
+        plain_table = str(collate(json_in, output="table"))
         self.assertEqual(expected_plain_table, plain_table)
 
     def testHTMLOutputVerticalLayoutPretokenizedJSON(self):
@@ -120,7 +122,7 @@ class Test(unittest.TestCase):
 +-------+---------+
 |  cat  | kitten. |
 +-------+---------+"""
-        plain_text_output = str(collate_pretokenized_json(json_in, layout="vertical"))
+        plain_text_output = str(collate(json_in, layout="vertical"))
         self.assertEquals(expected_output, plain_text_output)
 
     def testSegmentationPretokenizedJSON(self):
@@ -145,8 +147,10 @@ class Test(unittest.TestCase):
         }
       ]
     }
-        bad_func = lambda x: collate_pretokenized_json(x, segmentation=True)
-        self.assertRaises(UnsupportedError, bad_func, json_in )
+        json_expected = {"table": [[[{"ref": 123, "t": "A"}], [{"adj": True, "t": "black"}], [{"id": "xyz", "t": "cat"}]], [[{"t": "A"}], [{"adj": True, "t": "white"}, {"adj": True, "t": "stripy"}], [{"n": "cat", "t": "kitten."}]]], "witnesses": ["A", "B"]}
+
+        json_out = collate(json_in, output="json", segmentation=True)
+        self.assertEquals(json_expected, json.loads(json_out))
 
 if __name__ == '__main__':
     unittest.main()
