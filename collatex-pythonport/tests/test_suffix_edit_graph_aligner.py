2ddf15015dbf4aeba544375e51ec30f5d604d916
David J Birnbaum <djbpitt@gmail.com>
2016-07-18 09:20:53 -0400

    Revise test_suffix_edit_graph_aligner.py
    Add spurious white space before punctuation in comparison strings to mimic tokenization behavior

--- collatex-pythonport/tests/test_suffix_edit_graph_aligner.py
+++ collatex-pythonport/tests/test_suffix_edit_graph_aligner.py
@@ -14,8 +14,8 @@ class Test(unittest.TestCase):
         collation.add_plain_witness("A", "a b c d F g h i ! K ! q r s t")
         collation.add_plain_witness("B", "a b c d F g h i ! q r s t")
         alignment_table = collate(collation)
-        self.assertEquals(["a b c d F g h i!", "K!", "q r s t"], alignment_table.rows[0].to_list_of_strings())
-        self.assertEquals(["a b c d F g h i!", None, "q r s t"], alignment_table.rows[1].to_list_of_strings())
+        self.assertEquals(["a b c d F g h i !", "K !", "q r s t"], alignment_table.rows[0].to_list_of_strings())
+        self.assertEquals(["a b c d F g h i !", None, "q r s t"], alignment_table.rows[1].to_list_of_strings())
 
     def test_hermans_witness_order_independence_case_three_witnesses(self):
         collation = Collation()
