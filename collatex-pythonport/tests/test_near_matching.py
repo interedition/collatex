93ba6e92fb8db1619111b152656829eba63e410a
David J Birnbaum <djbpitt@gmail.com>
2016-07-18 08:57:41 -0400

    Revise test_near_matching.py
    Compare row.to_list_of_strings() to expected output
    TODO: We had to add spurious white space before punctuation in expected output

--- collatex-pythonport/tests/test_near_matching.py
+++ collatex-pythonport/tests/test_near_matching.py
@@ -11,21 +11,21 @@ class Test(unittest.TestCase):
 
     def test_exact_matching(self):
         collation = Collation()
-        collation.add_plain_witness("A", "I bought this glass, because it matches those dinner plates")
+        collation.add_plain_witness("A", "I bought this glass , because it matches those dinner plates")
         collation.add_plain_witness("B", "I bought those glasses")
         alignment_table = collate(collation)
-        self.assertEquals(["I bought", "this glass, because it matches", "those", "dinner plates"],
-                          alignment_table.rows[0].to_list())
-        self.assertEquals(["I bought", None, "those", "glasses"], alignment_table.rows[1].to_list())
+        self.assertEquals(["I bought", "this glass , because it matches", "those", "dinner plates"],
+                          alignment_table.rows[0].to_list_of_strings())
+        self.assertEquals(["I bought", None, "those", "glasses"], alignment_table.rows[1].to_list_of_strings())
 
     def test_near_matching(self):
         collation = Collation()
-        collation.add_plain_witness("A", "I bought this glass, because it matches those dinner plates")
+        collation.add_plain_witness("A", "I bought this glass , because it matches those dinner plates")
         collation.add_plain_witness("B", "I bought those glasses")
         alignment_table = collate(collation, near_match=True)
-        self.assertEquals(["I bought", "this glass, because it matches those dinner plates"],
-                          alignment_table.rows[0].to_list())
-        self.assertEquals(["I bought", "those glasses"], alignment_table.rows[1].to_list())
+        self.assertEquals(["I bought", "this glass , because it matches those dinner plates"],
+                          alignment_table.rows[0].to_list_of_strings())
+        self.assertEquals(["I bought", "those glasses"], alignment_table.rows[1].to_list_of_strings())
 
 
 if __name__ == "__main__":
