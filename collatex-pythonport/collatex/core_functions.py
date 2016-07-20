c11b862214f6140789ee54904297da291f9c75cc
David J Birnbaum <djbpitt@gmail.com>
2016-07-20 05:28:41 -0400

    Added tei output option to core_function
    TODO: Currently always pretty prints; make this user-configurable

--- collatex-pythonport/collatex/core_functions.py
+++ collatex-pythonport/collatex/core_functions.py
@@ -52,6 +52,8 @@ def collate(collation, output="table", layout="horizontal", segmentation=True, n
         return table
     if output == "xml":
         return export_alignment_table_as_xml(table, indent)
+        if output == "tei":
+            return export_alignment_table_as_tei(table, indent)
     else:
         raise Exception("Unknown output type: "+output)
 
@@ -83,3 +85,39 @@ def export_alignment_table_as_xml(table, indent=None):
         result = etree.tostring(app, encoding="unicode", pretty_print=indent)
         readings.append(result)
     return "<root>" + "".join(readings) + "</root>"
+
+def export_alignment_table_as_tei(table, indent=None):
+    p = etree.Element('p')
+    app = None
+    for column in table.columns:
+        if not column.variant:
+            text_node = " ".join(item.token_data["t"] for item in next(iter(column.tokens_per_witness.values())))
+            if not (len(p)):
+                p.text = text_node + "\n"
+            else:
+                app.tail = "\n" + text_node + "\n"
+        else:
+            app = etree.Element('app')
+            preceding = None  # If preceding is None, we're processing the first <rdg> child
+            app.text = "\n  "  # Indent first <rdg>
+            value_dict = {}  # keys are readings, values are an unsorted lists of sigla
+            for key, value in column.tokens_per_witness.items():
+                group = value_dict.setdefault(" ".join([item.token_data["t"] for item in value]), [])
+                group.append(key)
+            rdg_dict = {}  # keys are sorted lists of sigla, with "#" prepended; values are readings
+            for key, value in value_dict.items():
+                rdg_dict[" ".join("#" + item for item in sorted(value))] = key
+            for key, value in sorted(rdg_dict.items()):  # sort <rdg> elements by @wit values
+                if preceding is not None:  # Change tail of preceding <rdg> to indent current one
+                    preceding.tail = "\n  "
+                child = etree.Element('rdg')
+                child.attrib['wit'] = key
+                child.text = value
+                app.append(child)
+                child.tail = "\n"
+                # If preceding is not None on an iteration, use its tail indent non-initial current <rdg>
+                preceding = child
+            p.append(app)
+    # Without the encoding specification, outputs bytes instead of a string
+    result = etree.tostring(p, encoding="unicode")
+    return result
