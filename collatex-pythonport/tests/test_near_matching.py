e4c765586c9262135d8270e85834038e0ed68a50
David J Birnbaum <djbpitt@gmail.com>
2016-07-21 11:38:04 -0400

    Fixed broken expectations in near-matching test

--- collatex-pythonport/tests/test_near_matching.py
+++ collatex-pythonport/tests/test_near_matching.py
@@ -23,9 +23,9 @@ class Test(unittest.TestCase):
         collation.add_plain_witness("A", "I bought this glass , because it matches those dinner plates")
         collation.add_plain_witness("B", "I bought those glasses")
         alignment_table = collate(collation, near_match=True)
-        self.assertEquals(["I bought", "this glass , because it matches those dinner plates"],
+        self.assertEquals(["I bought ", "this glass , because it matches those dinner plates"],
                           alignment_table.rows[0].to_list_of_strings())
-        self.assertEquals(["I bought", "those glasses"], alignment_table.rows[1].to_list_of_strings())
+        self.assertEquals(["I bought ", "those glasses"], alignment_table.rows[1].to_list_of_strings())
 
 
 if __name__ == "__main__":
