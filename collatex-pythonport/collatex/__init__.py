a554821a9f2d5efe5ed2fff939fcffcf332b2e58
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2016-10-30 16:57:07 +0100

    Prepare bug fix release: 2.0.1

--- collatex-pythonport/collatex/__init__.py
+++ collatex-pythonport/collatex/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Ronald Haentjens Dekker'
 __email__ = 'ronald.dekker@huygens.knaw.nl'
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 
 from collatex.core_classes import Collation
 from collatex.core_functions import collate
