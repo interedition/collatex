32251264758146bd56523be335ee08a1605b225e
David J Birnbaum <djbpitt@gmail.com>
2018-08-16 16:05:35 +0200

    rewrite test_export_alignment_table_as_tei.py (pretty print) for new TEI output

--- collatex-pythonport/tests/test_export_alignment_table_as_tei.py
+++ collatex-pythonport/tests/test_export_alignment_table_as_tei.py
@@ -13,26 +13,33 @@ class Test(unittest.TestCase):
         self.assertEqual(output_expected, output)
 
     def test_export_alignment_table_as_tei_prettyprint(self):
-        output_expected = """<p>The
-<app>
-  <rdg wit="#A">quick </rdg>
-</app>
-brown
-<app>
-  <rdg wit="#A">wombat </rdg>
-  <rdg wit="#B #C">koala </rdg>
-</app>
-jumps over the
-<app>
-  <rdg wit="#A #C">industrious </rdg>
-  <rdg wit="#B">lazy </rdg>
-</app>
-<app>
-  <rdg wit="#A">brown </rdg>
-  <rdg wit="#B #C">yellow </rdg>
-</app>
-dog.
-</p>"""
+        output_expected = """<?xml version="1.0" ?>
+<cx:apparatus xmlns="http://www.tei-c.org/ns/1.0" xmlns:cx="http://interedition.eu/collatex/ns/1.0">
+	The 
+	<app>
+		<rdg wit="#A">quick</rdg>
+	</app>
+	 
+	brown 
+	<app>
+		<rdg wit="#A">wombat</rdg>
+		<rdg wit="#B #C">koala</rdg>
+	</app>
+	 
+	jumps over the 
+	<app>
+		<rdg wit="#A #C">industrious</rdg>
+		<rdg wit="#B">lazy</rdg>
+	</app>
+	 
+	<app>
+		<rdg wit="#A">brown</rdg>
+		<rdg wit="#B #C">yellow</rdg>
+	</app>
+	 
+	dog.
+</cx:apparatus>
+"""
         collation = Collation()
         collation.add_plain_witness("A", "The quick brown wombat jumps over the industrious brown dog.")
         collation.add_plain_witness("B", "The brown koala jumps over the lazy yellow dog.")
