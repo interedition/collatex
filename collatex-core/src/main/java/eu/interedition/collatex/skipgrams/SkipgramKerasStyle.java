92b84fe2eedee9b6a7e53a127d400e4f18f54853
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2018-10-18 17:18:02 +0200

    Some early skipgram stuff.

---
+++ collatex-core/src/main/java/eu/interedition/collatex/skipgrams/SkipgramKerasStyle.java
@@ -0,0 +1,27 @@
+package eu.interedition.collatex.skipgrams;
+
+import eu.interedition.collatex.Token;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * author: Ronald Haentjens Dekker
+ * Date: 18-10-2018
+ */
+public class SkipgramKerasStyle {
+    // walk over the squence form left to right
+    // from every position in the index we choose a second position from a limited window.
+
+    public List<List<Token>> create(List<Token> sequence, int windowSize) {
+        List<List<Token>> skipgrams = new ArrayList<>();
+        // ik kan hier gebruik maken van de sublist methode.
+        for (int start = 0; start < sequence.size(); start++) {
+            for (int skip = start+1; skip < Math.min(start+1+windowSize, sequence.size()); skip++) {
+                System.out.println(start+";"+skip);
+            }
+        }
+
+        return skipgrams;
+    }
+}
--- collatex-core/src/test/java/eu/interedition/collatex/skipgrams/SkipGramMWATest.java
+++ collatex-core/src/test/java/eu/interedition/collatex/skipgrams/SkipGramMWATest.java
@@ -2,6 +2,7 @@ package eu.interedition.collatex.skipgrams;
 
 import eu.interedition.collatex.Token;
 import eu.interedition.collatex.simple.SimpleWitness;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -20,6 +21,7 @@ public class SkipGramMWATest {
      * We tokenize one witness
      * Then we create b-grams out of them
      */
+    @Ignore
     @Test
     public void testNGramsOneWitness() {
         String firstWitness = "a b c d e ";
@@ -30,4 +32,15 @@ public class SkipGramMWATest {
         ArrayList<List<Token>> ngrams = c.create(tokens, 2);
         System.out.println(ngrams);
     }
+
+    @Test
+    public void testSkipGramsOneWitness() {
+        SimpleWitness w1 = new SimpleWitness("w1", "a b c d e");
+        List<Token> tokens = w1.getTokens();
+        System.out.println(tokens);
+        SkipgramKerasStyle c = new SkipgramKerasStyle();
+        List<List<Token>> skipgrams = c.create(tokens, 2);
+        System.out.println(skipgrams);
+
+}
 }
