3b1d958788a946576324644fa1b180e12f77e1a8
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2018-10-30 10:47:04 +0100

    SkipgramBasedAligner: deal with the fact that not all skipgrams occur in all witnesses. Removed unnecessary parameters.

--- collatex-core/src/main/java/eu/interedition/collatex/skipgrams/SkipgramBasedAligner.java
+++ collatex-core/src/main/java/eu/interedition/collatex/skipgrams/SkipgramBasedAligner.java
@@ -34,11 +34,11 @@ public class SkipgramBasedAligner {
         // we do a fixed number of iteratiros
         // aligning the highest priority each time.
         for (int i=0; i<2; i++) {
-            alignTheHighestPriority(witness1, witness2, witness3, vocabulary);
+            alignTheHighestPriority(vocabulary);
         }
     }
 
-    private void alignTheHighestPriority(List<Token> witness1, List<Token> witness2, List<Token> witness3, SkipgramVocabulary vocabulary) {
+    private void alignTheHighestPriority(SkipgramVocabulary vocabulary) {
         NormalizedSkipgram normalizedSkipgram = vocabulary.selectHighestCount();
         System.out.println("We start/continue with: "+normalizedSkipgram);
         select(normalizedSkipgram);
@@ -61,8 +61,10 @@ public class SkipgramBasedAligner {
             List<Token> witness1 = witnessSet.get(witnessid);
             // nu create ik die skipgrams opnieuw van een witness
             // en dan pak ik de normalized versoin
-            Skipgram skipgram = findSkipgramForNormalizedFormInWitness(xxx, witness1);
-            variantGraphCreator.selectSkipgram(skipgram);
+            Optional<Skipgram> skipgram = findSkipgramForNormalizedFormInWitness(xxx, witness1);
+            if (skipgram.isPresent()) {
+                variantGraphCreator.selectSkipgram(skipgram.get());
+            }
             // nu gaan we door de naar de volgende witness en zo verder
             // in de toekomst kunnen hierbij transpostions op treden.
             // dan moet we dus iets van een score gaan bijhouden van hoe elk vna deze variant graphs in wording is.
@@ -71,22 +73,21 @@ public class SkipgramBasedAligner {
 
     }
 
-    private Skipgram findSkipgramForNormalizedFormInWitness(NormalizedSkipgram xxx, List<Token> witness1) {
+    // Not every witness contains every normalized skipgram.
+    // So we return an optional
+    private Optional<Skipgram> findSkipgramForNormalizedFormInWitness(NormalizedSkipgram xxx, List<Token> witness1) {
         SkipgramCreator skc = new SkipgramCreator();
         List<Skipgram> skipgrams = skc.create(witness1, 3);
         // we gaan alle skipgrams af op zoek naar een skipgram die voldoet aan die normalzed version
-        Skipgram result = null;
+        Optional<Skipgram> result = Optional.empty();
         for (Skipgram skipgram : skipgrams) {
             SimpleToken sthead = (SimpleToken) skipgram.head;
             SimpleToken sttail = (SimpleToken) skipgram.tail;
             NormalizedSkipgram normalizedSkipgram = new NormalizedSkipgram(sthead.getNormalized(), sttail.getNormalized());
             if (normalizedSkipgram.equals(xxx)) {
-                result = skipgram;
+                result = Optional.of(skipgram);
                 break;
             }
-        }
-        if (result==null) {
-            throw new RuntimeException("Expected skipgram not found in witness!");
         }
         return result;
     }
