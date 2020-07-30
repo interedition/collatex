f27e33c5e5e38b9ac6d8a3ddc3cf73f70be921c5
rhdekker <ronald.dekker@di.huc.knaw.nl>
2020-07-30 21:47:18 +0200

    Converted Island.java to Kotlin.

--- collatex-core/src/main/java/eu/interedition/collatex/dekker/island/Island.kt
+++ collatex-core/src/main/java/eu/interedition/collatex/dekker/island/Island.kt
@@ -16,139 +16,125 @@
  * You should have received a copy of the GNU General Public License
  * along with CollateX.  If not, see <http://www.gnu.org/licenses/>.
  */
+package eu.interedition.collatex.dekker.island
 
-package eu.interedition.collatex.dekker.island;
+import eu.interedition.collatex.dekker.token_index.Block
+import eu.interedition.collatex.simple.SimpleToken
+import java.util.*
 
-import eu.interedition.collatex.Token;
-import eu.interedition.collatex.dekker.token_index.Block;
-import eu.interedition.collatex.simple.SimpleToken;
+class Island : Iterable<Coordinate?> {
+    private val islandCoordinates: MutableList<Coordinate> = ArrayList()
+    private val blockInstance: Block.Instance?
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-
-public class Island implements Iterable<Coordinate> {
-
-    private final List<Coordinate> islandCoordinates = new ArrayList<>();
-    private final Block.Instance blockInstance;
-
-    public Island(Block.Instance blockInstance) {
-        this.blockInstance = blockInstance;
+    constructor(blockInstance: Block.Instance?) {
+        this.blockInstance = blockInstance
     }
 
     // for legacy code
-    public Island() {
-        blockInstance = null;
+    constructor() {
+        blockInstance = null
     }
 
     // for legacy code
-    public Island(Coordinate first, Coordinate last) {
-        blockInstance = null;
-        add(first);
-        Coordinate newCoordinate = first;
-        while (!newCoordinate.equals(last)) {
-            newCoordinate = new Coordinate(newCoordinate.getRow() + 1, newCoordinate.getColumn() + 1);
+    constructor(first: Coordinate, last: Coordinate) {
+        blockInstance = null
+        add(first)
+        var newCoordinate = first
+        while (newCoordinate != last) {
+            newCoordinate = Coordinate(newCoordinate.getRow() + 1, newCoordinate.getColumn() + 1)
             //        LOG.debug("{}", newCoordinate);
-            add(newCoordinate);
+            add(newCoordinate)
         }
     }
 
-    public void add(Coordinate coordinate) {
-        islandCoordinates.add(coordinate);
+    fun add(coordinate: Coordinate) {
+        islandCoordinates.add(coordinate)
     }
 
-    public void removeCoordinate(Coordinate c) {
-        islandCoordinates.remove(c);
+    fun removeCoordinate(c: Coordinate) {
+        islandCoordinates.remove(c)
     }
 
     /**
      * Two islands are competitors if there is a horizontal or
      * vertical line which goes through both islands
      */
-    public boolean isCompetitor(Island isl) {
-        for (Coordinate c : isl) {
-            for (Coordinate d : islandCoordinates) {
-                if (c.sameColumn(d) || c.sameRow(d)) return true;
+    fun isCompetitor(isl: Island): Boolean {
+        for (c in isl) {
+            for (d in islandCoordinates) {
+                if (c.sameColumn(d) || c.sameRow(d)) return true
             }
         }
-        return false;
+        return false
     }
 
-    public boolean contains(Coordinate c) {
-        return islandCoordinates.contains(c);
+    operator fun contains(c: Coordinate): Boolean {
+        return islandCoordinates.contains(c)
     }
 
-    public Coordinate getLeftEnd() {
-        Coordinate coor = islandCoordinates.get(0);
-        for (Coordinate c : islandCoordinates) {
-            if (c.column < coor.column) coor = c;
+    val leftEnd: Coordinate
+        get() {
+            var coor = islandCoordinates[0]
+            for (c in islandCoordinates) {
+                if (c.column < coor.column) coor = c
+            }
+            return coor
         }
-        return coor;
-    }
-
-    public Coordinate getRightEnd() {
-        Coordinate coor = islandCoordinates.get(0);
-        for (Coordinate c : islandCoordinates) {
-            if (c.column > coor.column) coor = c;
+    val rightEnd: Coordinate
+        get() {
+            var coor = islandCoordinates[0]
+            for (c in islandCoordinates) {
+                if (c.column > coor.column) coor = c
+            }
+            return coor
         }
-        return coor;
+
+    fun size(): Int {
+        return islandCoordinates.size
     }
 
-    public int size() {
-        return islandCoordinates.size();
+    override fun iterator(): MutableIterator<Coordinate> {
+        return Collections.unmodifiableList(islandCoordinates).iterator()
     }
 
-    @Override
-    public Iterator<Coordinate> iterator() {
-        return Collections.unmodifiableList(islandCoordinates).iterator();
+    override fun hashCode(): Int {
+        return islandCoordinates.hashCode()
     }
 
-    @Override
-    public int hashCode() {
-        return islandCoordinates.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) return false;
-
-        if (!obj.getClass().equals(Island.class)) return false;
-
-        Island isl = (Island) obj;
-        if (isl.size() != size()) return false;
-
-        boolean result = true;
-        for (Coordinate c : isl) {
-            result &= this.contains(c);
+    override fun equals(obj: Any?): Boolean {
+        if (obj == null) return false
+        if (obj.javaClass != Island::class.java) return false
+        val isl = obj as Island
+        if (isl.size() != size()) return false
+        var result = true
+        for (c in isl) {
+            result = result and this.contains(c)
         }
-        return result;
+        return result
     }
 
-    public Block.Instance getBlockInstance() {
+    fun getBlockInstance(): Block.Instance {
         if (blockInstance == null) {
-            throw new RuntimeException("Block instance is not set on this island! It is probably constructed with legacy code!");
+            throw RuntimeException("Block instance is not set on this island! It is probably constructed with legacy code!")
         }
-        return blockInstance;
+        return blockInstance
     }
 
-    public int getDepth() {
-        return getBlockInstance().getBlock().getDepth();
-    }
+    val depth: Int
+        get() = getBlockInstance().block.getDepth()
 
-    @Override
-    public String toString() {
-        List<Token> instance = getBlockInstance().getTokens();
-        StringBuilder normalized = new StringBuilder();
-        for (int i = 0; i < this.size(); i++) {
-            Token t = instance.get(i);
-            SimpleToken st = (SimpleToken) t;
-            if (normalized.length() > 0) {
-                normalized.append(" ");
+    override fun toString(): String {
+        val instance = getBlockInstance().tokens
+        val normalized = StringBuilder()
+        for (i in 0 until size()) {
+            val t = instance[i]
+            val st = t as SimpleToken
+            if (normalized.length > 0) {
+                normalized.append(" ")
             }
-            normalized.append(st.getNormalized());
+            normalized.append(st.normalized)
         }
-        return normalized.toString();
+        return normalized.toString()
 
 //        if (islandCoordinates.isEmpty()) {
 //            //throw new RuntimeException("Unexpected situation: island coordinates empty!");
