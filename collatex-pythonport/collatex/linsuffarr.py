5e063b15784ed1be3463f9787a3f1b40ee06a6ee
djbpitt <djbpitt@gmail.com>
2023-06-12 10:01:45 -0400

    Bug fix in linsuffarr.py for Python 3.10

--- collatex-pythonport/collatex/linsuffarr.py
+++ collatex-pythonport/collatex/linsuffarr.py
@@ -31,7 +31,7 @@ from pickle import HIGHEST_PROTOCOL as _HIGHEST_PROTOCOL
 from pickle import dumps  as _dumps
 from pickle import loads  as _loads
 from gzip import GzipFile
-from inspect import getargspec
+from inspect import getfullargspec as getargspec # Not yet in official release
 from optparse import OptionParser
 from os.path import getsize
 from sys import argv   as _argv
